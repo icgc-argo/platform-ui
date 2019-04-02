4487231467cd7311d472b2e35953a8c3c96dbd65
Minh <hlminh2000@gmail.com>
2019-04-02 18:23:46 -0400

    adds some docs

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # argo-platform
+
+This repo contains the ICGC-Argo server-rendered front-end and its [GraphQl](https://graphql.org/learn/) BFF (Backend For Frontend) API gateway.
+
+## Repo structure
+
+```
+argo-platform
+|__ client (server-rendered React app)
+|   |__ pages (Next.js routing wrappers)
+|   |__ components (layout & stateful components)
+|   |__ uikits (pure presentational components)
+|   |__ utils (utility functions)
+|   |__ stories (Storybook stories, pulling from uikits)
+|__ server (GraphQl API)
+    |__ schemas (individual domain schemas,
+        remote and locally defined)
+```
+
+Refer to [./client/README.md](./client/README.md) and [./server/README.md](./server/README.md) for each module's documentations.
--- client/app.js
+++ client/app.js
@@ -1,9 +1,7 @@
 import express from "express";
 import NextJS from "next";
-import fetch from "isomorphic-fetch";
-import urlJoin from "url-join";
 
-import { PORT, API_ROOT } from "./config";
+import { PORT } from "./config";
 
 const dev = process.env.NODE_ENV !== "production";
 const app = NextJS({
@@ -13,20 +11,7 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(async () => {
   const server = express();
-  server.get("*", (req, res) => {
-    req.runQuery = ({ query, variables }) =>
-      fetch(urlJoin(API_ROOT, "graphql"), {
-        method: "POST",
-        headers: {
-          "content-type": "application/json"
-        },
-        body: JSON.stringify({
-          query,
-          variables
-        })
-      }).then(res => res.json());
-    return handle(req, res);
-  });
+  server.get("*", (req, res) => handle(req, res));
   server.listen(PORT, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${PORT}`);
