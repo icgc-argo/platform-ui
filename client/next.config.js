38ae9c25ae33292c8d1f20e9d4fc39e7677d2117
Minh <hlminh2000@gmail.com>
2019-04-06 16:58:41 -0400

    handle graphql import

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,20 @@
+const path = require("path");
+
 module.exports = {
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: 'empty'
-    }
+      fs: "empty"
+    };
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: "gql-loader"
+      }
+    ];
 
-    return config
+    return config;
   }
-}
+};
