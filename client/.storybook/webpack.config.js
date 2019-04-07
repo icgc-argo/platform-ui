29df63ddcd901464d6cbcb151cb2a4d8db4db6cf
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-04-07 10:25:34 -0400

    add custom storybook webpack

---
+++ client/.storybook/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config, mode }) => {
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  config.node = {
+    __dirname: true,
+    __filename: true
+  };
+
+  // Return the altered config
+  return config;
+};
