1c4166f83e613212a2b36dffa5898f8ed8cbaddf
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-04-10 14:18:25 -0400

    add prop-types

--- client/uikit/Button/index.js
+++ client/uikit/Button/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Button = ({ children }) => <button>{children}</button>;
+
+Button.propTypes = {
+  // Button text
+  children: PropTypes.node.required
+};
 
 export default Button;
