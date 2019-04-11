23a926b3db5cb9c2ecc93024777b4051bc3fcf32
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-04-11 10:13:46 -0400

    add jsxDecorator to show jsx source

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,11 +1,13 @@
 import { configure, addDecorator } from "@storybook/react";
 import { addReadme } from "storybook-readme";
+import { jsxDecorator } from "storybook-addon-jsx";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+addDecorator(jsxDecorator);
 
 addDecorator(addReadme);
 configure(loadStories, module);
