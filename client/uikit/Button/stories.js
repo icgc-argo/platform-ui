127397629b1b4a8b21f93861ae2ff7870f38416f
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-04-11 15:23:09 -0400

    add top level component doc and cleanup

--- client/uikit/Button/stories.js
+++ client/uikit/Button/stories.js
@@ -1,11 +1,11 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import Button from ".";
-import README from "./readme.md";
+import ReadMe from "./readme.md";
 
-const ButtonStories = storiesOf(`${__dirname}`, module).add(
-  "A basic button",
-  () => <Button>Cognito Argo Sum</Button>
-);
-
+const ButtonStories = storiesOf(__dirname, module)
+  .add("Intro", () => <div />, {
+    info: { text: `${ReadMe}`, inline: true, header: false, source: false }
+  })
+  .add("A basic button", () => <Button>Cognito Argo Sum</Button>);
 export default ButtonStories;
