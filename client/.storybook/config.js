b4306a6097b6365da289a1523246308238099b89
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-04-11 10:15:39 -0400

    note open issue regarding styling warning

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -7,7 +7,7 @@ const req = require.context("../stories", true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
-addDecorator(jsxDecorator);
 
+addDecorator(jsxDecorator); // Fix unique key warning: https://github.com/storybooks/addon-jsx/issues/65
 addDecorator(addReadme);
 configure(loadStories, module);
