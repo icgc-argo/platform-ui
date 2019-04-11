b9f8cf853f2626713abc8395112378448cbe7fd4
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-04-11 12:32:07 -0400

    replace readme with info-addon so we have story book source as documentation

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,5 +1,5 @@
 import { configure, addDecorator } from "@storybook/react";
-import { addReadme } from "storybook-readme";
+import { withInfo } from "@storybook/addon-info";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /\.stories\.js$/);
@@ -7,5 +7,5 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-addDecorator(addReadme);
+addDecorator(withInfo);
 configure(loadStories, module);
--- client/uikit/Button/stories.js
+++ client/uikit/Button/stories.js
@@ -3,8 +3,9 @@ import React from "react";
 import Button from ".";
 import README from "./readme.md";
 
-const ButtonStories = storiesOf(`${__dirname}`, module)
-  .addParameters({ readme: { content: `${README}`, sidebar: `${README}` } })
-  .add("A basic button", () => <Button>Cognito Argo Sum</Button>);
+const ButtonStories = storiesOf(`${__dirname}`, module).add(
+  "A basic button",
+  () => <Button>Cognito Argo Sum</Button>
+);
 
 export default ButtonStories;
