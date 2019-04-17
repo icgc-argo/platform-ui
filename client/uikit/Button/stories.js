cd8138fa06d6d87fb537496436fd49d12d4bcc67
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-04-17 14:40:40 -0400

    add Button stories

--- client/uikit/Button/stories.js
+++ client/uikit/Button/stories.js
@@ -2,8 +2,53 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 import Button from ".";
 
-const ButtonStories = storiesOf(`${__dirname}`, module).add("Basic", () => (
-  <Button onClick={() => console.log("Button clicked")}>Skeleton</Button>
-));
+const dummyClick = () => console.log("..button..clickity click click..");
+
+const ButtonStories = storiesOf(`${__dirname}`, module)
+  .add("Basic", () => <Button onClick={dummyClick}>Skeleton</Button>)
+  .add("Disabled Button", () => (
+    <Button onClick={dummyClick} disabled>
+      Disabled
+    </Button>
+  ))
+  .add("Primary Button", () => (
+    <Button onClick={dummyClick} variant="primary">
+      Primary Button
+    </Button>
+  ))
+  .add("Secondary Button", () => (
+    <Button onClick={dummyClick} variant="secondary">
+      Secondary Button
+    </Button>
+  ))
+  .add("Warning Button", () => (
+    <Button onClick={dummyClick} variant="warning">
+      Warning Button
+    </Button>
+  ))
+  .add("Sized Button", () => (
+    <div>
+      <div style={{ marginBottom: "20px" }}>
+        <Button onClick={dummyClick} variant="primary" size="sm">
+          Small Primary Button
+        </Button>
+      </div>
+      <div style={{ marginBottom: "20px" }}>
+        <Button onClick={dummyClick} variant="primary">
+          Medium Primary Button
+        </Button>
+      </div>
+      <div style={{ marginBottom: "20px" }}>
+        <Button onClick={dummyClick} variant="primary" size="lg">
+          Large Primary Button
+        </Button>
+      </div>
+    </div>
+  ))
+  .add("Button with loading", () => (
+    <Button onClick={dummyClick} loading={true}>
+      Loading Button
+    </Button>
+  ));
 
 export default ButtonStories;
