8523e7b9776035acc0acb2f5a47cb84e76567c0b
Minh <hlminh2000@gmail.com>
2019-04-26 17:01:32 -0400

    adds gateway CI/CD

---
+++ Jenkinsfile.gateway.groovy
@@ -0,0 +1,74 @@
+def commit = "UNKNOWN"
+
+pipeline {
+    agent {
+        kubernetes {
+            label 'gateway-executor'
+            yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: node
+    image: mhart/alpine-node:latest
+    tty: true
+  - name: helm
+    image: alpine/helm:2.12.3
+    tty: true
+    command:
+    - cat
+  - name: docker
+    image: docker:18-git
+    tty: true
+    volumeMounts:
+    - mountPath: /var/run/docker.sock
+      name: docker-sock
+  volumes:
+  - name: docker-sock
+    hostPath:
+      path: /var/run/docker.sock
+      type: File
+"""
+        }
+    }
+    stages {
+        stage('Test') {
+            steps {
+                container('node') {
+                    git url: 'https://github.com/icgc-argo/argo-platform', branch: 'master'
+                    sh "cd ./server && npm ci"
+                    sh "cd ./server && npm run test"
+                }
+            }
+        }
+        stage('Build image') {
+            steps {
+                container('docker') {
+                    withCredentials([usernamePassword(credentialsId:'8d0aaceb-2a19-4f92-ae37-5b61e4c0feb8', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        sh 'docker login -u $USERNAME -p $PASSWORD'
+                    }
+                    git url: 'https://github.com/icgc-argo/argo-platform', branch: 'master'
+                    // DNS error if --network is default
+                    script {
+                        commit = sh(returnStdout: true, script: 'git describe --always').trim()
+                    }
+                    sh "cd ./server && docker build --network=host -f Dockerfile . -t icgcargo/argo-gateway:${commit}-alpine"
+                    sh "docker push icgcargo/argo-gateway:${commit}-alpine"
+                }
+            }
+        }
+        stage('Deploy to QA') {
+            steps {
+                container('helm') {
+                    withCredentials([file(credentialsId:'4ed1e45c-b552-466b-8f86-729402993e3b', variable: 'KUBECONFIG')]) {
+                        sh 'helm init --client-only'
+                        sh 'helm ls --kubeconfig $KUBECONFIG'
+                        sh 'helm repo add icgcargo https://icgc-argo.github.io/argo-charts/'
+                        sh 'helm repo update'
+                        sh "helm upgrade argo-gateway icgcargo/argo-gateway --reuse-values --set image.tag=${commit}-alpine"
+                    }
+                }
+            }
+        }
+    }
+}
