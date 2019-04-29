02f1eca3bd57dea3c91ff223a69cd38ac129dfe5
Minh <hlminh2000@gmail.com>
2019-04-29 15:10:34 -0400

    moves token decoding to hook

--- client/pages/logged-in.js
+++ client/pages/logged-in.js
@@ -1,12 +1,10 @@
 import React from "react";
-import jwtDecode from "jwt-decode";
 import { get } from "lodash";
 
 import useEgoToken from "global/hooks/useEgoToken";
 
 const LoggedIn = () => {
-  const { token: egoToken } = useEgoToken();
-  const data = egoToken ? jwtDecode(egoToken) : null;
+  const { data } = useEgoToken();
   const firstName = get(data, "context.user.firstName", "");
   const lastName = get(data, "context.user.lastName", "");
   return (
