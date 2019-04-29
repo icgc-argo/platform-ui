75261c61091fd7df7b23e1fc31ffbc9c63e8f8b7
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-04-29 10:38:14 -0400

    removed isomorphic-unfetch

--- client/pages/logged-in.js
+++ client/pages/logged-in.js
@@ -1,5 +1,5 @@
 import React from "react";
-import fetch from "isomorphic-unfetch";
+import fetch from "isomorphic-fetch";
 import jwtDecode from "jwt-decode";
 
 class LoggedIn extends React.Component {
