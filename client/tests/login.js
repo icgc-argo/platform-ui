3d1602d01eada24b95faf9c7b4f488027d0ce6bf
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-05-01 13:02:51 -0400

    add nightwatch

--- client/tests/login.js
+++ client/tests/login.js
@@ -1,21 +1,47 @@
-var webdriver = require("selenium-webdriver"),
-  By = webdriver.By,
-  until = webdriver.until;
+/*module.exports = {
+  "Demo test Google": function(browser) {
+    browser
+      .url("http://www.google.com")
+      .waitForElementVisible("body")
+      .setValue("input[type=text]", "nightwatch")
+      .waitForElementVisible("input[name=btnK]")
+      .click("input[name=btnK]")
+      .pause(1000)
+      .assert.containsText("#main", "Night Watch")
+      .end();
+  }
+};
+*/
 
-var driver = new webdriver.Builder().forBrowser("chrome").build();
+module.exports = {
+  "Demo test homepage": function(browser) {
+    browser
+      .url("http://localhost:8080")
+      .waitForElementVisible("body")
+      .click(".card:nth-of-type(2)")
+      .pause(1000)
+      .assert.containsText("h1", "Minh")
+      .end();
+  },
 
-driver.get("http://www.duckduckgo.com");
-
-driver.findElement(By.id("search_form_input_homepage")).sendKeys("webdriver");
-driver.findElement(By.id("search_button_homepage")).click();
-
-driver.sleep(2000).then(function() {
-  driver.getTitle().then(function(title) {
-    if (title === "webdriver - Google Search") {
-      console.log("Test passed", title);
-    } else {
-      console.log("Test failed", title);
-    }
-    driver.quit();
-  });
-});
+  "Demo local storage": function(browser) {
+    browser
+      .url("http://localhost:8080")
+      .waitForElementVisible("body")
+      .execute(
+        function() {
+          console.log(
+            "executing function but were in a browser so none of these console logs will be show"
+          );
+          const token = window.localStorage.getItem("test");
+          console.log("token", token);
+          return token;
+        },
+        [],
+        function(result) {
+          console.log("Our execute script finished", result);
+        }
+      )
+      .end();
+  }
+};
