88771658155d74a7b1100ff8ae9a23a5a60f4a78
Minh <hlminh2000@gmail.com>
2019-05-03 21:02:49 -0400

    moves precomputeApolloCache to utils

--- client/global/utils/graphql.js
+++ client/global/utils/graphql.js
@@ -11,19 +11,26 @@ import { createHttpLink } from "apollo-link-http";
 import { createPage } from "global/utils/pages";
 import { decodeToken } from "global/utils/egoJwt";
 import { API_ROOT } from "global/config";
-import { precomputeApolloCache } from "global/utils/graphql";
 import { UserQuery } from "components/UserPage/index.queries.js";
 import UserPage from "components/UserPage";
 
-export default createPage({
-  getInitialProps: async ({ egoJwt, asPath, query, res }) => {
-    const data = decodeToken(egoJwt);
-    const firstName = get(data, "context.user.firstName", "");
-    const lastName = get(data, "context.user.lastName", "");
-    const extractedData = await precomputeApolloCache({
-      query: UserQuery,
-      variables: {}
-    });
-    return { firstName, lastName, extractedData };
-  }
-})(UserPage);
+export const precomputeApolloCache: ({
+  query: any,
+  variables: { [k: string]: any }
+}) => Promise<any> = async ({ query, variables }) => {
+  const link = (createHttpLink({
+    uri: urlJoin(API_ROOT, "graphql")
+  }): any);
+  const client = new ApolloClient({
+    ssrMode: true,
+    link: link,
+    cache: new InMemoryCache()
+  });
+  const x = await client.query({
+    query,
+    variables: {
+      userId: 2
+    }
+  });
+  return client.extract();
+};
