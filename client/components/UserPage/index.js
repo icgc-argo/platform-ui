12287b3387a119b261b1acfc5a449c8c97a4ab57
Minh <hlminh2000@gmail.com>
2019-05-03 23:25:06 -0400

    creates useQuery hook

--- client/components/UserPage/index.js
+++ client/components/UserPage/index.js
@@ -4,7 +4,7 @@ import { get } from "lodash";
 import urlJoin from "url-join";
 import fetch from "isomorphic-fetch";
 
-import { gql, InMemoryCache } from "apollo-boost";
+import { gql, InMemoryCache, type GraphQLError } from "apollo-boost";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 
@@ -13,27 +13,55 @@ import { decodeToken } from "global/utils/egoJwt";
 import { API_ROOT } from "global/config";
 import { UserQuery } from "./index.queries.js";
 
-export default ({ egoJwt, firstName, lastName, extractedData }: any) => {
-  const [data, setData] = React.useState(null);
-  const [id, setId] = React.useState(2);
+const useQuery = (input: {
+  query: any,
+  variables?: { [k: string]: any },
+  client?: ApolloClient<any>,
+  cache?: any,
+  observe?: Array<any>
+}) => {
   const link = (createHttpLink({
     uri: urlJoin(API_ROOT, "graphql")
   }): any);
-  const client = new ApolloClient({
-    cache: new InMemoryCache().restore(extractedData),
-    link,
-    connectToDevTools: true
-  });
+  const client =
+    input.client ||
+    new ApolloClient<any>({
+      cache: new InMemoryCache().restore(input.cache),
+      link,
+      connectToDevTools: true
+    });
+  const initialState: {
+    data: any,
+    errors: void | Array<GraphQLError>,
+    loading: boolean
+  } = {
+    data: undefined,
+    loading: true,
+    errors: []
+  };
+  const [state, setState] = React.useState(initialState);
   React.useEffect(() => {
     client
       .query({
-        query: UserQuery,
-        variables: {
-          userId: id
-        }
+        query: input.query,
+        variables: input.variables
       })
-      .then(({ data }) => setData(data));
-  }, [id]);
+      .then(({ data, errors, loading }) => setState({ data, errors, loading }));
+  }, input.observe || []);
+  return state;
+};
+
+export default ({ egoJwt, firstName, lastName, extractedData }: any) => {
+  const [id, setId] = React.useState(2);
+  const { data, loading, errors } = useQuery({
+    cache: extractedData,
+    query: UserQuery,
+    variables: {
+      userId: id
+    },
+    observe: [id]
+  });
+
   return (
     <div>
       Logged in user: {firstName} {lastName}
