40146894d6703c1eccac0534a9a933ea5eb52c5f
Minh <hlminh2000@gmail.com>
2019-05-06 15:14:48 -0400

    adds status endpoint and updates Dockerfile

--- client/app.js
+++ client/app.js
@@ -2,7 +2,10 @@ import express from "express";
 import NextJS from "next";
 import bodyParser from "body-parser";
 
+import config from "./package.json";
 import { PORT } from "./global/config";
+
+const { version } = config;
 
 const dev = process.env.NODE_ENV !== "production";
 const Next = NextJS({
@@ -16,11 +19,14 @@ const applyUrlMasks = server => {
   });
 };
 
-Next.prepare().then(async () => {
-  const server = express();
-  server.use(bodyParser.urlencoded({ extended: false }));
-  server.use(bodyParser.json());
+const server = express();
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use("/status", (req, res) => {
+  res.json(version);
+});
 
+Next.prepare().then(async () => {
   applyUrlMasks(server);
 
   server.get("/", (req, res) => {
