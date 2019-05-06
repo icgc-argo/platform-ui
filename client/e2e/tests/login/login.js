84aa35bdaa13cdcc0c005774963f66d3e971e0d3
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-05-06 17:51:14 -0400

    add id for link

--- client/e2e/tests/login/login.js
+++ client/e2e/tests/login/login.js
@@ -19,6 +19,8 @@ module.exports = {
     browser
       .url("http://localhost:8080")
       .waitForElementVisible("body")
+      .click("#link-login")
+      .waitForElementVisible("#google-log-in")
       .click("#google-log-in");
   },
 
