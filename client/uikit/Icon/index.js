bcb4b673dc1a8706e5666a95cd3356e6feeffbbf
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-05-10 16:35:37 -0400

    add icon component

---
+++ client/uikit/Icon/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Icons from "./icons";
+import { css, jsx } from "@emotion/core";
+
+const aCSSObj = css`
+  background-color: green;
+`;
+console.log("css", aCSSObj);
+
+const Icon = ({ name, width, height, fill, className }) => {
+  const svg = Icons[name];
+  return (
+    <svg
+      css={css`
+        height: 100px;
+      `}
+      width={width}
+      height={height}
+      viewBox={svg.viewBox}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title lang="en">{svg.title}</title>
+      <g>
+        <path fill={fill} fillRule={svg.fillRule || "nonezero"} d={svg.path} />
+      </g>
+    </svg>
+  );
+};
+
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  fill: PropTypes.string
+};
+
+export default Icon;
