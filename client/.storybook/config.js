d156e30d0951f6f00a20392ba200d7b4c1b4928e
Minh <hlminh2000@gmail.com>
2019-05-12 15:12:19 -0400

    code format

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,7 +1,7 @@
 import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
-import ReactTable from 'react-table';
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+import ReactTable from "react-table";
 import "react-table/react-table.css";
 
 // automatically import all files ending in *.stories.js
@@ -10,36 +10,56 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-addParameters({ options: { 
-  addonPanelInRight: true,
-}})
-addDecorator((()=> {
-  const TableComponent=({propDefinitions}) => (
-    <ReactTable
-      minRows={0}
-      showPagination={false}
-      data={propDefinitions}
-      columns={[
-        {Header: "property", accessor: "property"},
-        {Header: "type", accessor: "propType", Cell: ({ original }) => 
-          <React.Fragment>
-            {original.propType.name}
-            {original.propType.name === "enum" && 
-              <div>{original.propType.value.map(({value}) => <div>- {value}</div>)} </div>
-            }
-          </React.Fragment>},
-        {Header: "required", accessor: "required", Cell: ({ original }) => (
-          original.required 
-            ? <React.Fragment>{String(original.required)}</React.Fragment> 
-            : null)
-        },
-        {Header: "description", accessor: "description", style:{ 'white-space': 'unset'}},
-        {Header: "default value", accessor: "defaultValue"}
-      ]}
-    />
-  )
-  return withInfo({inline: true, header: false, TableComponent})
-})());
+addParameters({
+  options: {
+    addonPanelInRight: true
+  }
+});
+addDecorator(
+  (() => {
+    const TableComponent = ({ propDefinitions }) => (
+      <ReactTable
+        minRows={0}
+        showPagination={false}
+        data={propDefinitions}
+        columns={[
+          { Header: "property", accessor: "property" },
+          {
+            Header: "type",
+            accessor: "propType",
+            Cell: ({ original }) => (
+              <React.Fragment>
+                {original.propType.name}
+                {original.propType.name === "enum" && (
+                  <div>
+                    {original.propType.value.map(({ value }) => (
+                      <div>- {value}</div>
+                    ))}{" "}
+                  </div>
+                )}
+              </React.Fragment>
+            )
+          },
+          {
+            Header: "required",
+            accessor: "required",
+            Cell: ({ original }) =>
+              original.required ? (
+                <React.Fragment>{String(original.required)}</React.Fragment>
+              ) : null
+          },
+          {
+            Header: "description",
+            accessor: "description",
+            style: { "white-space": "unset" }
+          },
+          { Header: "default value", accessor: "defaultValue" }
+        ]}
+      />
+    );
+    return withInfo({ inline: true, header: false, TableComponent });
+  })()
+);
 addDecorator(withKnobs);
 
 configure(loadStories, module);
