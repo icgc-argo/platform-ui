422ca15b283bda7796865d4f1ba7f21480550312
Minh Ha <hlminh2000@gmail.com>
2019-05-13 08:29:13 -0400

    add knobs to storybook (#29)
    * add knobs to storybook
    
    * updates storybook version
    
    * adds react-table to storybook info
    
    * updates husky
    
    * code format
    
    * code format
    
    * use knobs for "Button with multiple child nodes"

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,5 +1,8 @@
-import { configure, addDecorator } from "@storybook/react";
+import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /\.stories\.js$/);
@@ -7,6 +10,56 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-addDecorator(withInfo);
+addParameters({
+  options: {
+    addonPanelInRight: true
+  }
+});
+addDecorator(
+  (() => {
+    const TableComponent = ({ propDefinitions }) => (
+      <ReactTable
+        minRows={0}
+        showPagination={false}
+        data={propDefinitions}
+        columns={[
+          { Header: "property", accessor: "property" },
+          {
+            Header: "type",
+            accessor: "propType",
+            Cell: ({ original }) => (
+              <React.Fragment>
+                {original.propType.name}
+                {original.propType.name === "enum" && (
+                  <div>
+                    {original.propType.value.map(({ value }) => (
+                      <div>- {value}</div>
+                    ))}{" "}
+                  </div>
+                )}
+              </React.Fragment>
+            )
+          },
+          {
+            Header: "required",
+            accessor: "required",
+            Cell: ({ original }) =>
+              original.required ? (
+                <React.Fragment>{String(original.required)}</React.Fragment>
+              ) : null
+          },
+          {
+            Header: "description",
+            accessor: "description",
+            style: { "white-space": "unset" }
+          },
+          { Header: "default value", accessor: "defaultValue" }
+        ]}
+      />
+    );
+    return withInfo({ inline: true, header: false, TableComponent });
+  })()
+);
+addDecorator(withKnobs);
 
 configure(loadStories, module);
