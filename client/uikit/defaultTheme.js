e067ba6f66120b102f875c42a1718402136d524a
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-05-16 11:18:19 -0400

    add input styling

---
+++ client/uikit/defaultTheme.js
@@ -0,0 +1,23 @@
+import { css } from "@emotion/core";
+
+const colors = {
+  grey: "#525767",
+  grey_1: "#babcc2",
+  grey_2: "#dcdde1",
+  light_blue: "#88b9e9",
+  red: "#e00548",
+  pale_grey: "#f6f6f7",
+  light_blue_grey: "#d0d1d8"
+};
+
+const button = {
+  sizes: { sm: "10px", md: "16px", lg: "24px" },
+  primary: { color: "#90278e", hover: "#7C227A" },
+  secondary: { color: "#2b388f", hover: "#25307B" },
+  warning: { color: "#ff9427", hover: "#FF870E" }
+};
+
+export default {
+  button,
+  colors
+};
