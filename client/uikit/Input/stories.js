9b6a1989d860cba391664ab5f7beee0a9f86fdfc
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-05-16 11:17:45 -0400

    add story

--- client/uikit/Input/stories.js
+++ client/uikit/Input/stories.js
@@ -1,9 +1,28 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import Input from ".";
+import { withKnobs, text, boolean, radios } from "@storybook/addon-knobs";
 
-const InputStories = storiesOf(`${__dirname}`, module).add("Basic", () => (
-  <Input>Skeleton</Input>
-));
+const createKnobs = () => {
+  const disabled = boolean("disabled", false);
+  const value = text("Value", "I entered some input...");
+  const placeholder = text("Placeholder", "State typing here..");
+  const size = radios("size", {
+    sm: "sm",
+    lg: "lg"
+  });
+
+  return {
+    disabled,
+    value,
+    placeholder,
+    size
+  };
+};
+
+const InputStories = storiesOf(`${__dirname}`, module).add("Basic", () => {
+  const props = createKnobs();
+  return <Input {...props} />;
+});
 
 export default InputStories;
