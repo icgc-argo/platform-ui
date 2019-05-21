5e6d8688cd94327bf7660e7155c4ae747cf3731c
Minh <hlminh2000@gmail.com>
2019-05-21 17:56:27 -0400

    typography model in theme

---
+++ client/uikit/theme/defaultTheme/typography.js
@@ -0,0 +1,101 @@
+export default {
+  hero: {
+    fontFamily: "WorkSans",
+    fontSize: "44px",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "normal",
+    letterSpacing: "normal"
+  },
+  title: {
+    fontFamily: "WorkSans",
+    fontSize: "30px",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "normal",
+    letterSpacing: "normal"
+  },
+  subtitle: {
+    fontFamily: "WorkSans",
+    fontSize: "22px",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "normal",
+    letterSpacing: "normal"
+  },
+  subtitle2: {
+    fontFamily: "WorkSans",
+    fontSize: "18px",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "normal",
+    letterSpacing: "normal"
+  },
+  navigation: {
+    fontFamily: "WorkSans",
+    fontSize: "15px",
+    fontWeight: "600",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "normal",
+    letterSpacing: "normal"
+  },
+  paragraph: {
+    fontFamily: "WorkSans",
+    fontSize: "14px",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "1.57",
+    letterSpacing: "normal"
+  },
+  paragraphBold: {
+    fontFamily: "WorkSans",
+    fontSize: "14px",
+    fontWeight: "600",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "1.57",
+    letterSpacing: "normal"
+  },
+  data: {
+    fontFamily: "WorkSans",
+    fontSize: "12px",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "1.17",
+    letterSpacing: "normal"
+  },
+  dataBold: {
+    fontFamily: "WorkSans",
+    fontSize: "13px",
+    fontWeight: "600",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "1.08",
+    letterSpacing: "normal"
+  },
+  caption: {
+    fontFamily: "WorkSans",
+    fontSize: "11px",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "1.27",
+    letterSpacing: "normal"
+  },
+  captionBold: {
+    fontFamily: "WorkSans",
+    fontSize: "11px",
+    fontWeight: "600",
+    fontStyle: "normal",
+    fontStretch: "normal",
+    lineHeight: "1.27",
+    letterSpacing: "normal"
+  }
+};
