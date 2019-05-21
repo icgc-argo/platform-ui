33fc2d6b07bb4c1c66f96970123194575405187e
Minh <hlminh2000@gmail.com>
2019-05-21 17:56:27 -0400

    typography model in theme

--- client/uikit/theme/defaultTheme/index.js
+++ client/uikit/theme/defaultTheme/index.js
@@ -1,9 +1,11 @@
 import colors from "./colors";
 import button from "./button";
 import appBar from "./appBar";
+import typography from "./typography";
 
 export default {
   colors,
   button,
-  appBar
+  appBar,
+  typography
 };
