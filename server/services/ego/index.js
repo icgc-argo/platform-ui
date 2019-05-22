cf5d6f7b82ad09fa7d2b5b04c057ba79d9959df9
Jon Eubank <joneubank@gmail.com>
2019-05-22 16:04:07 -0400

    :sparkles: Ego service provides user data from ego via grpc

---
+++ server/services/ego/index.js
@@ -0,0 +1,53 @@
+/*
+ * This file dynamically generates a gRPC client from Ego.proto.
+ * The content of Ego.proto is copied directly from:
+ */
+import grpc from 'grpc';
+import * as loader from '@grpc/proto-loader';
+import { EGO_ROOT_GRPC } from '../../config';
+
+const PROTO_PATH = __dirname + '/Ego.proto';
+const packageDefinition = loader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const proto = grpc.loadPackageDefinition(packageDefinition).bio.overture.ego.grpc;
+
+const userService = new proto.UserService(EGO_ROOT_GRPC, grpc.credentials.createInsecure());
+
+const getUser = async (id, jwt = null) => {
+  return await new Promise((resolve, reject) => {
+    const meta = new grpc.Metadata();
+
+    if (jwt) {
+      meta.add('jwt', jwt);
+    }
+
+    userService.getUser({ id }, meta, (err, response) => (err ? reject(err) : resolve(response)));
+  });
+};
+
+const listUsers = async ({ pageNum, limit, sort, groups, query } = {}, jwt = null) => {
+  const payload = {
+    page: { page_number: pageNum, page_size: limit, sort },
+    group_ids: groups,
+    query: { value: query },
+  };
+
+  const meta = new grpc.Metadata();
+  if (jwt) {
+    meta.add('jwt', jwt);
+  }
+
+  return await new Promise((resolve, reject) =>
+    userService.listUsers(payload, meta, (err, response) =>
+      err ? reject(err) : resolve(response),
+    ),
+  );
+};
+
+export default { getUser, listUsers };
