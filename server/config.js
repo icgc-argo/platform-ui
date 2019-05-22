043c9341ea7cfc3b3bc91324d2389a1d5cb8d23c
Jon Eubank <joneubank@gmail.com>
2019-05-22 15:58:52 -0400

    :wrench: Add ego paths to env and config

--- server/config.js
+++ server/config.js
@@ -5,6 +5,8 @@ export const ARRANGER_ROOT = process.env.ARRANGER_ROOT || 'http://localhost:5050
 export const ARRANGER_PROJECT_ID = process.env.ARRANGER_PROJECT_ID || 'test';
 
 // Ego config
+export const EGO_ROOT_REST = process.env.EGO_ROOT_REST || 'http://localhost:8081';
+export const EGO_ROOT_GRPC = process.env.EGO_ROOT_GRPC || 'localhost:50051';
 export const EGO_JWT_SECRET = process.env.EGO_JWT_SECRET;
 export const EGO_APPLICATION_ID = process.env.EGO_APPLICATION_ID;
 export const EGO_APPLICATION_SECRET = process.env.EGO_APPLICATION_SECRET;
