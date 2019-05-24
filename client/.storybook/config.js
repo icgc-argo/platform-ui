5ee40b2cf371d2de2a085524500b31afb45a29f7
Minh <hlminh2000@gmail.com>
2019-05-24 16:03:32 -0400

    brings the react-table style into a scoped container for storybook

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -2,8 +2,8 @@ import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 import ReactTable from "react-table";
-import "react-table/react-table.css";
 import React from "react";
+import ReactTableContainer from "./ReactTableContainer";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /\.stories\.js$/);
@@ -19,56 +19,60 @@ addParameters({
 addDecorator(
   (() => {
     const TableComponent = ({ propDefinitions }) => (
-      <ReactTable
-        minRows={0}
-        showPagination={false}
-        data={propDefinitions}
-        columns={[
-          { Header: "property", accessor: "property" },
-          {
-            Header: "type",
-            accessor: "propType",
-            Cell: ({ original }) =>
-              !original.propType ? null : (
-                <React.Fragment>
-                  {original.propType.name}
-                  {original.propType.name === "enum" && (
-                    <div>
-                      {Array.isArray(original.propType.value)
-                        ? original.propType.value.map(({ value }, i) => (
-                            <div key={i}>- {value}</div>
-                          ))
-                        : original.propType.value}{" "}
-                    </div>
-                  )}
-                </React.Fragment>
-              )
-          },
-          {
-            Header: "required",
-            accessor: "required",
-            Cell: ({ original }) =>
-              original.required ? (
-                <React.Fragment>{String(original.required)}</React.Fragment>
-              ) : null
-          },
-          {
-            Header: "description",
-            accessor: "description",
-            style: { whiteSpace: "unset" }
-          },
-          {
-            Header: "default value",
-            accessor: "defaultValue",
-            style: { whiteSpace: "unset" },
-            Cell: ({ original }) => {
-              return (
-                <React.Fragment>{String(original.defaultValue)}</React.Fragment>
-              );
+      <ReactTableContainer>
+        <ReactTable
+          minRows={0}
+          showPagination={false}
+          data={propDefinitions}
+          columns={[
+            { Header: "property", accessor: "property" },
+            {
+              Header: "type",
+              accessor: "propType",
+              Cell: ({ original }) =>
+                !original.propType ? null : (
+                  <React.Fragment>
+                    {original.propType.name}
+                    {original.propType.name === "enum" && (
+                      <div>
+                        {Array.isArray(original.propType.value)
+                          ? original.propType.value.map(({ value }, i) => (
+                              <div key={i}>- {value}</div>
+                            ))
+                          : original.propType.value}{" "}
+                      </div>
+                    )}
+                  </React.Fragment>
+                )
+            },
+            {
+              Header: "required",
+              accessor: "required",
+              Cell: ({ original }) =>
+                original.required ? (
+                  <React.Fragment>{String(original.required)}</React.Fragment>
+                ) : null
+            },
+            {
+              Header: "description",
+              accessor: "description",
+              style: { whiteSpace: "unset" }
+            },
+            {
+              Header: "default value",
+              accessor: "defaultValue",
+              style: { whiteSpace: "unset" },
+              Cell: ({ original }) => {
+                return (
+                  <React.Fragment>
+                    {String(original.defaultValue)}
+                  </React.Fragment>
+                );
+              }
             }
-          }
-        ]}
-      />
+          ]}
+        />
+      </ReactTableContainer>
     );
     return withInfo({ inline: true, header: false, TableComponent });
   })()
