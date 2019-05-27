d1c26947b2dee76d66cf0a25946f68755ecda149
Minh <hlminh2000@gmail.com>
2019-05-27 16:19:47 -0400

    updates comment

--- server/services/ego/index.js
+++ server/services/ego/index.js
@@ -1,6 +1,6 @@
 /*
  * This file dynamically generates a gRPC client from Ego.proto.
- * The content of Ego.proto is copied directly from:
+ * The content of Ego.proto is copied directly from: https://github.com/overture-stack/ego/blob/develop/src/main/proto/Ego.proto
  */
 import grpc from 'grpc';
 import * as loader from '@grpc/proto-loader';
