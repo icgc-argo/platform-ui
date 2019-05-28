9c98a0a74032a1e2cd23fe7b268e1ccb3bd990fa
Minh <hlminh2000@gmail.com>
2019-05-28 14:16:56 -0400

    pagination component placeholder

--- client/uikit/Table/TablePagination/index.js
+++ client/uikit/Table/TablePagination/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+import Typography from "../../Typography";
+
+const PaginationContainer = styled("div")`
+  min-height: 40px;
+  display: flex;
+  align-items: center;
+  background: ${({ theme }) => theme.colors.white};
+  border-top: solid 1px ${({ theme }) => theme.colors.grey_2};
+  border-bottom: solid 1px ${({ theme }) => theme.colors.grey_2};
+`;
 
 /*
  * Please edit me!
  */
-const TablePagination = () => <h1>TablePagination</h1>;
+const TablePagination = () => (
+  <PaginationContainer>
+    <Typography variant="caption">TablePagination coming soon</Typography>
+  </PaginationContainer>
+);
 
 TablePagination.propTypes = {
   /*
