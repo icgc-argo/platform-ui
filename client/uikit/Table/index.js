db3ff9d269b4743f4c3d3b6d7057285059ef4343
Minh <hlminh2000@gmail.com>
2019-05-28 12:11:35 -0400

    styles header and some interaction state

--- client/uikit/Table/index.js
+++ client/uikit/Table/index.js
@@ -6,31 +6,104 @@ import ReactTable from "react-table";
 
 import reactTableDefaultStyle from "./reactTableDefaultStyle";
 import Typography from "../Typography";
+import ascending from "../assets/table/ascending.svg";
+import descending from "../assets/table/descending.svg";
 
 const StyledTable = styled(ReactTable)`
   ${reactTableDefaultStyle}
-  & .rt-thead.-header {
-    box-shadow: none;
+  &.ReactTable {
+    border: none;
   }
-  & .rt-th {
+
+  &.ReactTable .rt-th {
     ${({ theme }) => css(theme.typography.data)};
     font-weight: bold;
     background: ${({ theme }) => theme.table.th.background};
   }
-  & .rt-td {
+  &.ReactTable .rt-td {
     ${({ theme }) => css(theme.typography.data)}
+    padding: 10px 16px;
+  }
+
+  /* overrides stripped rows style */
+  &.ReactTable.-striped .rt-tr:not(.-odd) {
+    background: ${({ theme }) => theme.table.th.background};
+  }
+  &.ReactTable.-striped .rt-tr.-odd {
+    background: ${({ theme }) => theme.colors.white};
+  }
+
+  /* overrides hover highlight rows style */
+  &.ReactTable.-highlight .rt-tbody .rt-tr:not(.-padRow):hover {
+    background: ${({ theme }) => theme.colors.grey_3};
+  }
+
+  &.ReactTable .rt-thead.-header {
+    box-shadow: none;
+    border-top: solid 1px ${({ theme }) => theme.colors.grey_2};
+    border-bottom: solid 2px ${({ theme }) => theme.colors.grey_2};
+    & .rt-th {
+      padding: 10px 16px;
+      border-left: none;
+      border-right: none;
+      text-align: left;
+      display: flex;
+      justify-content: space-between;
+      ${({ sortable }) =>
+        sortable
+          ? css`
+              &.-sort-asc {
+                box-shadow: none;
+                &:after {
+                  content: url(${ascending});
+                }
+              }
+            `
+          : css``}
+      ${({ sortable }) =>
+        sortable
+          ? css`
+              &.-sort-desc {
+                box-shadow: none;
+                &:after {
+                  content: url(${descending});
+                }
+              }
+            `
+          : css``}
+    }
   }
 `;
 
-const Table = props => (
-  <StyledTable minRows={0} showPagination={false} {...props} />
+const Table = ({
+  className = "",
+  stripped = true,
+  highlight = true,
+  ...rest
+}) => (
+  <StyledTable
+    className={`${className} ${stripped ? "-striped" : ""} ${
+      highlight ? "-highlight" : ""
+    }`}
+    minRows={0}
+    showPagination={false}
+    {...rest}
+  />
 );
 
 Table.propTypes = {
-  ...ReactTable.propTypes
   /**
    * This component effectively exposes the full react-table API
    */
+  ...ReactTable.propTypes,
+  /**
+   * Whether to strip odd rows
+   */
+  stripped: PropTypes.bool,
+  /**
+   * Whether to highlight hovered row
+   */
+  highlight: PropTypes.bool
 };
 
 export default Table;
