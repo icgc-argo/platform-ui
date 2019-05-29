1c3ce2df622cf8aa59958aae70363741db1882eb
Minh <hlminh2000@gmail.com>
2019-05-29 11:37:59 -0400

    improved comment

--- client/uikit/Table/reactTableDefaultStyle.js
+++ client/uikit/Table/reactTableDefaultStyle.js
@@ -1,12 +1,15 @@
 import { css } from '@emotion/core';
 
 /**
- * This is the default react table styling with minor tweeks:
+ * This is the default react table styling copied
+ * from react-table/react-table.css with minor tweeks:
  * - removed vendor prefixes for readability
+ * - wrapping in css tag for scoping and usage in Emotion.
  *
  * This file should not be modified, any modification should be applied as
  * overrides on top. The cleanup was done for ease of exploration
  */
+
 export default css`
   &.ReactTable {
     position: relative;
