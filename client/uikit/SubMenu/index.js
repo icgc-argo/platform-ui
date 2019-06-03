adb1a20d88e4c67642a7bc2297659a98d463961e
Minh <hlminh2000@gmail.com>
2019-06-03 15:18:22 -0400

    implements icons to the submenu

--- client/uikit/SubMenu/index.js
+++ client/uikit/SubMenu/index.js
@@ -5,77 +5,7 @@ import css from '@emotion/css';
 
 import Icon from '../Icon';
 import useTheme from '../utils/useTheme';
-
-const defaultLabelStyle = ({ selected, theme }) => css`
-  & > .MenuItemContent {
-    cursor: pointer;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    &:hover {
-      background: ${theme.colors.grey_3};
-    }
-  }
-`;
-
-const level1Style = ({ selected, theme }) => css`
-  ${css(theme.typography.navigation)}
-  ${defaultLabelStyle({ theme, selected })}
-  background: ${theme.colors.white};
-  color: ${selected ? theme.colors.secondary_dark : 'black'};
-  animation: all 1s;
-  & > .MenuItemContent {
-    padding: 12px;
-  }
-`;
-
-const level2Style = ({ selected, theme }) => css`
-  ${css(theme.typography.navigation)}
-  ${defaultLabelStyle({ theme, selected })}
-  border: none;
-  border-left: solid 2px;
-  font-weight: normal;
-  border-left-color: ${selected ? theme.colors.secondary : theme.colors.white};
-  background: ${selected ? theme.colors.grey_4 : 'none'};
-  & > .MenuItemContent {
-    padding-left: 40px;
-    padding-right: 18px;
-  }
-`;
-
-const level3Style = ({ selected, theme }) => css`
-  ${css(theme.typography.navigation)}
-  ${defaultLabelStyle({ theme, selected })}
-  border: none;
-  font-weight: normal;
-  background: ${selected ? theme.colors.secondary_4 : 'none'};
-  & > .MenuItemContent {
-    padding-left: 52px;
-  }
-`;
-
-const defaultStyle = props => css`
-  ${level1Style(props)}
-  ${defaultLabelStyle(props)}
-  .MenuItemContainer > & {
-    ${level2Style(props)}
-    .MenuItemContainer > & {
-      ${level3Style(props)}
-    }
-  }
-`;
-
-const MenuItemContainer = styled('div')`
-  ${({ level, ...rest }) =>
-    level === 1
-      ? level1Style(rest)
-      : level === 2
-      ? level2Style(rest)
-      : level === 3
-      ? level3Style(rest)
-      : defaultStyle(rest)}
-`;
+import { MenuItemContainer, IconContainer, ContentContainer } from './styledComponents';
 
 export const MenuItem = ({
   className = '',
@@ -84,6 +14,7 @@ export const MenuItem = ({
   children,
   content,
   onClick = e => {},
+  icon,
 }) => {
   const [localSelectedState, setLocalSelectedState] = React.useState(controlledSelectedState);
   const isSelected =
@@ -101,7 +32,16 @@ export const MenuItem = ({
       className={`${className} MenuItemContainer`}
     >
       <div className="MenuItemContent">
-        <span>{content}</span>
+        <ContentContainer>
+          {icon && (
+            <IconContainer>
+              {React.cloneElement(icon, {
+                fill: isSelected ? theme.colors.secondary_dark : theme.colors.black,
+              })}
+            </IconContainer>
+          )}
+          {content}
+        </ContentContainer>
         {children && content && (
           <Icon
             name={isSelected ? 'chevron_down' : 'chevron_right'}
---
+++ client/uikit/SubMenu/styledComponents.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import css from '@emotion/css';
+
+const defaultLabelStyle = ({ selected, theme }) => css`
+  & > .MenuItemContent {
+    cursor: pointer;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    &:hover {
+      background: ${theme.colors.grey_3};
+    }
+  }
+`;
+
+const level1Style = ({ selected, theme }) => css`
+  ${css(theme.typography.navigation)}
+  ${defaultLabelStyle({ theme, selected })}
+  background: ${theme.colors.white};
+  color: ${selected ? theme.colors.secondary_dark : 'black'};
+  animation: all 1s;
+  & > .MenuItemContent {
+    padding: 12px;
+  }
+`;
+
+const level2Style = ({ selected, theme }) => css`
+  ${css(theme.typography.navigation)}
+  ${defaultLabelStyle({ theme, selected })}
+  border: none;
+  border-left: solid 2px;
+  font-weight: normal;
+  border-left-color: ${selected ? theme.colors.secondary : theme.colors.white};
+  background: ${selected ? theme.colors.grey_4 : 'none'};
+  & > .MenuItemContent {
+    padding-left: 40px;
+    padding-right: 18px;
+  }
+`;
+
+const level3Style = ({ selected, theme }) => css`
+  ${css(theme.typography.navigation)}
+  ${defaultLabelStyle({ theme, selected })}
+  border: none;
+  font-weight: normal;
+  background: ${selected ? theme.colors.secondary_4 : 'none'};
+  & > .MenuItemContent {
+    padding-left: 52px;
+  }
+`;
+
+const defaultStyle = props => css`
+  ${level1Style(props)}
+  ${defaultLabelStyle(props)}
+  .MenuItemContainer > & {
+    ${level2Style(props)}
+    .MenuItemContainer > & {
+      ${level3Style(props)}
+    }
+  }
+`;
+
+export const MenuItemContainer = styled('div')`
+  ${({ level, ...rest }) =>
+    level === 1
+      ? level1Style(rest)
+      : level === 2
+      ? level2Style(rest)
+      : level === 3
+      ? level3Style(rest)
+      : defaultStyle(rest)}
+`;
+
+export const IconContainer = styled('span')`
+  margin-right: 10px;
+`;
+
+export const ContentContainer = styled('span')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
