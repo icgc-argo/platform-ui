d34ff6174140f9faa968ee9fc1326e46ec492c4f
Minh <hlminh2000@gmail.com>
2019-06-04 17:34:53 -0400

    implements hidden select component for accessibility

--- client/uikit/form/Select/stories.js
+++ client/uikit/form/Select/stories.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Select from '.';
+import { action } from '@storybook/addon-actions';
 import { text, boolean, radios } from '@storybook/addon-knobs';
+
+import Select from '.';
 
 const createKnobs = () => {
   const error = boolean('error', false);
@@ -28,7 +30,16 @@ const createKnobs = () => {
 
 const InputStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
   <div style={{ width: '200px' }}>
-    <Select />
+    <Select
+      aria-label="demo-select"
+      options={[
+        { content: 'Value 1', value: 'v1' },
+        { content: 'Value 2', value: 'v2' },
+        { content: 'Value 3', value: 'v3' },
+        { content: 'Value 4', value: 'v4' },
+      ]}
+      onChange={action('onChange')}
+    />
   </div>
 ));
 
