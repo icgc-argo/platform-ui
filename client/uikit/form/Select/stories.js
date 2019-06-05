219b4fb14232fe2ae8c04c36890a9067e06e5293
Minh <hlminh2000@gmail.com>
2019-06-05 18:10:20 -0400

    bumps optionslist up

--- client/uikit/form/Select/stories.js
+++ client/uikit/form/Select/stories.js
@@ -40,6 +40,17 @@ const InputStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
       ]}
       onChange={action('onChange')}
     />
+    <Select
+      aria-label="demo-select"
+      value="v1"
+      options={[
+        { content: 'Value 1', value: 'v1' },
+        { content: 'Value 2', value: 'v2' },
+        { content: 'Value 3', value: 'v3' },
+        { content: 'Value 4', value: 'v4' },
+      ]}
+      onChange={action('onChange')}
+    />
   </div>
 ));
 
