5cb8588c16cb76be41b99afe92373f13f1a3629e
Xu Deng <d8660091@gmail.com>
2019-06-06 13:41:13 -0400

    Move multiselect to form

--- client/uikit/form/MultiSelect/index.js
+++ client/uikit/form/MultiSelect/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import Icon from '../Icon';
+import Icon from '../../Icon';
 import Option from './Option';
 import css from '@emotion/css';
 
