fb850888c55b2d1e1bc81f8f3062b153002b131f
Xu Deng <d8660091@gmail.com>
2019-06-06 11:01:37 -0400

    Add Container

---
+++ client/uikit/Container/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
+const Container = styled('div')`
+  border-radius: 8px;
+  box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.1), 0 1px 5px 0 rgba(0, 0, 0, 0.08);
+  background-color: white;
+`;
+
+Container.propTypes = {};
+
+export default Container;
