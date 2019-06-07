df70c28e596690bd4af8e39a5f735ecf3ee6a0a8
Minh <hlminh2000@gmail.com>
2019-06-07 11:08:01 -0400

    move proptypes table out of the storybook config file

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -5,7 +5,7 @@ import { withA11y } from '@storybook/addon-a11y';
 
 import ReactTable from 'react-table';
 import React from 'react';
-import ReactTableContainer from './ReactTableContainer';
+import PropTypesTable from './PropTypesTable';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
@@ -19,63 +19,7 @@ addParameters({
   },
 });
 addDecorator(withA11y);
-addDecorator(
-  (() => {
-    const TableComponent = ({ propDefinitions }) => (
-      <ReactTableContainer>
-        <ReactTable
-          minRows={0}
-          showPagination={false}
-          data={propDefinitions}
-          columns={[
-            { Header: 'property', accessor: 'property' },
-            {
-              Header: 'type',
-              accessor: 'propType',
-              Cell: ({ original }) =>
-                !original.propType ? null : (
-                  <React.Fragment>
-                    {original.propType.name}
-                    {original.propType.name === 'enum' && (
-                      <div>
-                        {Array.isArray(original.propType.value)
-                          ? original.propType.value.map(({ value }, i) => (
-                              <div key={i}>- {value}</div>
-                            ))
-                          : original.propType.value}{' '}
-                      </div>
-                    )}
-                  </React.Fragment>
-                ),
-            },
-            {
-              Header: 'required',
-              accessor: 'required',
-              Cell: ({ original }) =>
-                original.required ? (
-                  <React.Fragment>{String(original.required)}</React.Fragment>
-                ) : null,
-            },
-            {
-              Header: 'description',
-              accessor: 'description',
-              style: { whiteSpace: 'unset' },
-            },
-            {
-              Header: 'default value',
-              accessor: 'defaultValue',
-              style: { whiteSpace: 'unset' },
-              Cell: ({ original }) => {
-                return <React.Fragment>{String(original.defaultValue)}</React.Fragment>;
-              },
-            },
-          ]}
-        />
-      </ReactTableContainer>
-    );
-    return withInfo({ inline: true, header: false, TableComponent });
-  })(),
-);
+addDecorator(withInfo({ inline: true, header: false, TableComponent: PropTypesTable }));
 addDecorator(withKnobs);
 
 configure(loadStories, module);
