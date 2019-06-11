b4c0c7a5b7d60cd5ebbb46db715772d8d458756f
Minh <hlminh2000@gmail.com>
2019-06-11 17:02:03 -0400

    creates PercentageBar

---
+++ client/uikit/PercentageBar/stories.js
@@ -0,0 +1,18 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import css from '@emotion/css';
+import { number, select } from '@storybook/addon-knobs';
+
+import defaultTheme from '../theme/defaultTheme';
+import PercentageBar from '.';
+
+const PercentageBarStories = storiesOf(`${__dirname}`, module).add('Basic', () => {
+  const color = select('fill', [null, '#00f', ...Object.keys(defaultTheme.colors)], null);
+  return (
+    <div style={{ width: 200, background: 'white' }}>
+      <PercentageBar nom={number('nom', 500)} denom={number('denom', 2000)} color={color} />
+    </div>
+  );
+});
+
+export default PercentageBarStories;
