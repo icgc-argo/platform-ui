1fdbf10ddd65113965e7152ff9466ac13f6d8005
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-06-11 12:36:33 -0400

    fix colors

--- client/uikit/theme/defaultTheme/radiocheckbox.js
+++ client/uikit/theme/defaultTheme/radiocheckbox.js
@@ -1,23 +1,23 @@
-import colors from "./colors";
+import colors from './colors';
 export default {
   fontSizes: {
-    default: "14px"
+    default: '14px',
   },
   textColors: {
     default: colors.black,
-    disabled: colors.light_blue_grey
+    disabled: '#d0d1d8',
   },
   borderColors: {
     default: colors.grey_2,
-    disabled: colors.light_blue_grey
+    disabled: '#d0d1d8',
   },
   radio: {
     default: colors.grey_1,
     checked: colors.secondary,
-    disabled: colors.grey_2
+    disabled: colors.grey_2,
   },
   colors: {
     disabled: colors.grey_3,
-    default: colors.white
-  }
+    default: colors.white,
+  },
 };
