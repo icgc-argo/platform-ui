bf04934d34bf9be41d3f034c760082e772b97d5f
Minh <hlminh2000@gmail.com>
2019-06-15 20:24:11 -0400

    internalizes TooltipContainer in ToolTip component to access theme and completes tooltip component story
    stuff

--- client/uikit/ToolTip/index.js
+++ client/uikit/ToolTip/index.js
@@ -1,22 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Tooltip as ReactTippy } from 'react-tippy';
+
 import { css, styled } from '..';
-
-// import { styled } from '..';
+import useTheme from 'uikit/utils/useTheme';
 import Typography from '../Typography';
-import { withProps } from 'recompose';
 
-const TooltipContainer = styled('div')`
-  ${({ theme }) => css(theme.typography.label)}
-  background: green;
-  color: white;
-`;
+const ToolTip = ({ html, ...rest }) => {
+  const theme = useTheme();
+  const TooltipContainer = styled('div')`
+    ${css(theme.typography.label)}
+    background: ${theme.colors.primary_1};
+    border-radius: 2px;
+    padding: 2px 4px;
+    color: white;
+    font-weight: normal;
+  `;
+  return <ReactTippy html={<TooltipContainer id="tooltip">{html}</TooltipContainer>} {...rest} />;
+};
 
-const ToolTip = ({ html, ...rest }) => (
-  <ReactTippy html={<TooltipContainer>{html}</TooltipContainer>} {...rest} />
-);
-
-ToolTip.propTypes = {};
+// exposing full react-tippy API based on https://github.com/tvkhoa/react-tippy, leaving out some style specific stuff
+ToolTip.propTypes = {
+  disabled: PropTypes.bool,
+  open: PropTypes.bool,
+  useContext: PropTypes.bool,
+  onRequestClose: PropTypes.func,
+  position: PropTypes.oneOf('top', 'bottom', 'left', 'right'),
+  trigger: PropTypes.oneOf('mouseenter', 'focus', 'click', 'manual'),
+  tabIndex: PropTypes.number,
+  interactive: PropTypes.bool,
+  interactiveBorder: PropTypes.number,
+  delay: PropTypes.number,
+  hideDelay: PropTypes.number,
+  animation: PropTypes.oneOf('shift', 'perspective', 'fade', 'scale', 'none'),
+  animateFill: PropTypes.bool,
+  duration: PropTypes.number,
+  distance: PropTypes.number,
+  offset: PropTypes.number,
+  hideOnClick: PropTypes.oneOf(true, false, 'persistent'),
+  multiple: PropTypes.bool,
+  followCursor: PropTypes.bool,
+  inertia: PropTypes.bool,
+  transitionFlip: PropTypes.bool,
+  popperOptions: PropTypes.object,
+  html: PropTypes.any,
+  unmountHTMLWhenHide: PropTypes.bool,
+  sticky: PropTypes.bool,
+  stickyDuration: PropTypes.number,
+  touchHold: PropTypes.bool,
+  onShow: PropTypes.func,
+  onShown: PropTypes.func,
+  onHide: PropTypes.func,
+  onHidden: PropTypes.func,
+  className: PropTypes.string,
+  style: PropTypes.PropTypes.object,
+};
 
 export default ToolTip;
--- client/uikit/ToolTip/stories.js
+++ client/uikit/ToolTip/stories.js
@@ -1,13 +1,58 @@
-import { storiesOf } from '@storybook/react';
 import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { radios, select, boolean, number } from '@storybook/addon-knobs';
+
 import ToolTip from '.';
 import Icon from '../Icon';
 import Typography from 'uikit/Typography';
 
-const ToolTipStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
-  <ToolTip html={<span>yo</span>}>
-    <img src="https://i.pinimg.com/originals/fa/0c/05/fa0c05778206cb2b2dddf89267b7a31c.jpg" />
-  </ToolTip>
-));
+const ToolTipStories = storiesOf(`${__dirname}`, module).add('Basic', () => {
+  const knobs = {
+    disabled: boolean('disabled', false),
+    open: boolean('open', undefined),
+    useContext: boolean('useContext', undefined),
+    position: select('position', ['top', 'bottom', 'left', 'right'], 'bottom'),
+    trigger: select('trigger', ['mouseenter', 'focus', 'click', 'manual'], undefined),
+    tabIndex: number('tabIndex', undefined),
+    interactive: boolean('interactive', undefined),
+    interactiveBorder: number('interactiveBorder', undefined),
+    delay: number('delay', undefined),
+    hideDelay: number('hideDelay', undefined),
+    animation: select('animation', ['shift', 'perspective', 'fade', 'scale', 'none'], undefined),
+    animateFill: boolean('animateFill', undefined),
+    duration: number('duration', undefined),
+    distance: number('distance', undefined),
+    offset: number('offset', undefined),
+    hideOnClick: select('hideOnClick', [true, false, 'persistent'], undefined),
+    multiple: boolean('multiple', undefined),
+    followCursor: boolean('followCursor', undefined),
+    inertia: boolean('inertia', undefined),
+    transitionFlip: boolean('transitionFlip', undefined),
+    unmountHTMLWhenHide: boolean('unmountHTMLWhenHide', undefined),
+    sticky: boolean('sticky', undefined),
+    stickyDuration: number('stickyDuration', undefined),
+    touchHold: boolean('touchHold', undefined),
+  };
+  return (
+    <div style={{ width: 200 }}>
+      <div>Hover me!!!</div>
+      <ToolTip
+        html={<span>Doge~~~!!!!</span>}
+        onRequestClose={action('onRequestClose')}
+        onShow={action('onShow')}
+        onShown={action('onShown')}
+        onHide={action('onHide')}
+        onHidden={action('onHidden')}
+        {...knobs}
+      >
+        <img
+          style={{ width: 50, borderRadius: 100 }}
+          src="https://i.pinimg.com/originals/fa/0c/05/fa0c05778206cb2b2dddf89267b7a31c.jpg"
+        />
+      </ToolTip>
+    </div>
+  );
+});
 
 export default ToolTipStories;
