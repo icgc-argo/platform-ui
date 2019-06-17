62e7c599a4e038553b90212e17fd9d6cc57b42d5
ballabadi <bashar.allabadi@oicr.on.ca>
2019-06-17 12:16:48 -0400

    remove deploy qa from gateway ci

--- Jenkinsfile.gateway.groovy
+++ Jenkinsfile.gateway.groovy
@@ -57,25 +57,7 @@ spec:
                 }
             }
         }
-        stage('Deploy to QA') {
-            when {
-                expression {
-                    GIT_BRANCH = 'origin/' + sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
-                    return GIT_BRANCH == 'origin/master' || params.FORCE_FULL_BUILD
-                }
-            }
-            steps {
-                container('helm') {
-                    withCredentials([file(credentialsId:'4ed1e45c-b552-466b-8f86-729402993e3b', variable: 'KUBECONFIG')]) {
-                        sh 'helm init --client-only'
-                        sh 'helm ls --kubeconfig $KUBECONFIG'
-                        sh 'helm repo add icgcargo https://icgc-argo.github.io/charts/'
-                        sh 'helm repo update'
-                        sh "helm upgrade argo-gateway icgcargo/argo-gateway --set image.tag=${commit},appConfig.EGO_ROOT_GRPC=ego-qa:50051,appConfig.PROGRAM_SERVICE_ROOT=program-service-qa:50051"
-                    }
-                }
-            }
-        }
+
         stage('Deploy to argo-qa') {
             when {
                 expression {
