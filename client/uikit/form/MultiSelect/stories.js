748040cc373458d7e3db8475a536d285dcfedd7d
Xu Deng <d8660091@gmail.com>
2019-06-18 15:36:28 -0400

    Merge allow new to basic

--- client/uikit/form/MultiSelect/stories.js
+++ client/uikit/form/MultiSelect/stories.js
@@ -16,58 +16,37 @@ function WithState({ children }) {
   });
 }
 
-const MultiSelectStories = storiesOf(`${__dirname}`, module)
-  .add('Basic', () => (
-    <WithState>
-      <MultiSelect
-        value="[parent state]"
-        onChange={() => '[parent func]'}
-        placeholder="Add one or more..."
-        disabled={boolean('disabled', false)}
-        error={boolean('error', false)}
-        single={boolean('single', false)}
-      >
-        <Option value="Australia">Australia</Option>
-        <Option value="Cambodia">Cambodia</Option>
-        <Option value="Cameroon">Cameroon</Option>
-        <Option value="Canada">Canada</Option>
-        <Option value="Afghanistan">Afghanistan</Option>
-        <Option value="Albania">Albania</Option>
-        <Option value="Algeria">Algeria</Option>
-        <Option value="Andorra">Andorra</Option>
-        <Option value="Angola">Angola</Option>
-        <Option value="Antigua and Barbuda">Antigua and Barbuda</Option>
-        <Option value="Argentina">Argentina</Option>
-        <Option value="Armenia">Armenia</Option>
-        <Option value="Austria">Austria</Option>
-        <Option value="Azerbaijan">Azerbaijan</Option>
-        <Option value="Bahamas">Bahamas</Option>
-        <Option value="Bahrain">Bahrain</Option>
-        <Option value="Bangladesh">Bangladesh</Option>
-        <Option value="Barbados">Barbados</Option>
-        <Option value="Belarus">Belarus</Option>
-      </MultiSelect>
-    </WithState>
-  ))
-  .add('Allow new value', () => (
-    <WithState>
-      <MultiSelect
-        value="[parent state]"
-        onChange={() => '[parent func]'}
-        placeholder="Add one or more..."
-        single={boolean('single', false)}
-        allowNew
-      >
-        <Option value="Australia">Australia</Option>
-        <Option value="Cambodia">Cambodia</Option>
-        <Option value="Cameroon">Cameroon</Option>
-        <Option value="Canada">Canada</Option>
-        <Option value="Afghanistan">Afghanistan</Option>
-        <Option value="Albania">Albania</Option>
-        <Option value="Algeria">Algeria</Option>
-        <Option value="Andorra">Andorra</Option>
-        <Option value="Angola">Angola</Option>
-      </MultiSelect>
-    </WithState>
-  ));
+const MultiSelectStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
+  <WithState>
+    <MultiSelect
+      value="[parent state]"
+      onChange={() => '[parent func]'}
+      placeholder="Add one or more..."
+      disabled={boolean('disabled', false)}
+      error={boolean('error', false)}
+      single={boolean('single', false)}
+      allowNew={boolean('allowNew', false)}
+    >
+      <Option value="Australia">Australia</Option>
+      <Option value="Cambodia">Cambodia</Option>
+      <Option value="Cameroon">Cameroon</Option>
+      <Option value="Canada">Canada</Option>
+      <Option value="Afghanistan">Afghanistan</Option>
+      <Option value="Albania">Albania</Option>
+      <Option value="Algeria">Algeria</Option>
+      <Option value="Andorra">Andorra</Option>
+      <Option value="Angola">Angola</Option>
+      <Option value="Antigua and Barbuda">Antigua and Barbuda</Option>
+      <Option value="Argentina">Argentina</Option>
+      <Option value="Armenia">Armenia</Option>
+      <Option value="Austria">Austria</Option>
+      <Option value="Azerbaijan">Azerbaijan</Option>
+      <Option value="Bahamas">Bahamas</Option>
+      <Option value="Bahrain">Bahrain</Option>
+      <Option value="Bangladesh">Bangladesh</Option>
+      <Option value="Barbados">Barbados</Option>
+      <Option value="Belarus">Belarus</Option>
+    </MultiSelect>
+  </WithState>
+));
 export default MultiSelectStories;
