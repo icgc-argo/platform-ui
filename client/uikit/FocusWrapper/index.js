f5146e44facdd0144528c326b5c6572032c466e5
Minh <hlminh2000@gmail.com>
2019-06-21 15:12:27 -0400

    implements some dynamic configs

--- client/uikit/FocusWrapper/index.js
+++ client/uikit/FocusWrapper/index.js
@@ -10,7 +10,6 @@ const FocusWrapper = styled('button')`
   box-shadow: none;
   outline: none;
   transition: box-shadow 0.2s ease-in;
-  border-radius: 2px;
   line-height: 0px;
   &:focus {
     box-shadow: 0px 0px 4px 0px ${({ theme }) => theme.colors.secondary_1};
--- client/uikit/Toast/stories.js
+++ client/uikit/Toast/stories.js
@@ -1,10 +1,40 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import Toast from '.';
+import Toast, { TOAST_VARIANTS, TOAST_INTERACTION_EVENTS, TOAST_INTERACTION } from '.';
 import { action } from '@storybook/addon-actions';
+import { select, text } from '@storybook/addon-knobs';
 
-const ToastStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
-  <Toast onInteraction={action('RECEIVED EVENT')} />
-));
+const ToastStories = storiesOf(`${__dirname}`, module).add('Basic', () => {
+  const variant = select('variant', [undefined, ...Object.values(TOAST_VARIANTS)], undefined);
+  const interactionType = select(
+    'interactionType',
+    [undefined, ...Object.values(TOAST_INTERACTION)],
+    undefined,
+  );
+  const title = text('title', 'Hipster Ipsum');
+  const content = text(
+    'content',
+    'Lorem ipsum dolor amet helvetica post-ironic fingerstache trust fund pitchfork tofu venmo live-edge',
+  );
+  return (
+    <>
+      <Toast
+        variant={variant}
+        interactionType={interactionType}
+        title={title}
+        content={content}
+        onInteraction={action('RECEIVED EVENT')}
+      />
+      <Toast
+        variant={variant}
+        interactionType={interactionType}
+        title={title}
+        content={content}
+        onInteraction={action('RECEIVED EVENT')}
+        icon={null}
+      />
+    </>
+  );
+});
 
 export default ToastStories;
