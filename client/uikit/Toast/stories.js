fc2f7d7863f09a497b68364d33a2befe4ff84c09
Minh <hlminh2000@gmail.com>
2019-06-21 13:36:53 -0400

    initial implementation of Toast component with Info style

--- client/uikit/Toast/stories.js
+++ client/uikit/Toast/stories.js
@@ -1,7 +1,10 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import Toast from '.';
+import { action } from '@storybook/addon-actions';
 
-const ToastStories = storiesOf(`${__dirname}`, module).add('Basic', () => <Toast>Skeleton</Toast>);
+const ToastStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
+  <Toast onInteraction={action('RECEIVED EVENT')} />
+));
 
 export default ToastStories;
