90b024ade66b2ca35cb75e22a41efd0ff950fc21
Minh <hlminh2000@gmail.com>
2019-06-24 16:06:29 -0400

    implements ToastStack animation without react-anime

--- client/uikit/Toast/stories.js
+++ client/uikit/Toast/stories.js
@@ -30,11 +30,6 @@ const ToastStories = storiesOf(`${__dirname}`, module).add('Basic', () => {
         title={title}
         content={content}
         onInteraction={action('RECEIVED EVENT')}
-        animation={{
-          opacity: [0, 1],
-          translateX: [100, 0],
-          easing: 'easeOutSine',
-        }}
       />
       <Toast
         variant={variant}
@@ -45,11 +40,6 @@ const ToastStories = storiesOf(`${__dirname}`, module).add('Basic', () => {
         title={title}
         content={content}
         onInteraction={action('RECEIVED EVENT')}
-        animation={{
-          opacity: [0, 1],
-          translateX: [100, 0],
-          easing: 'easeOutSine',
-        }}
         icon={null}
       />
     </>
--- client/uikit/ToastStack/stories.js
+++ client/uikit/ToastStack/stories.js
@@ -4,35 +4,57 @@ import { action } from '@storybook/addon-actions';
 
 import ToastStack from '.';
 import { TOAST_VARIANTS } from '../Toast';
+import Button from 'uikit/Button';
 
-const ToastStackStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
-  <ToastStack
-    onInteraction={action('onInteraction')}
-    toastConfigs={[
+const Story = () => {
+  const [stack, setStack] = React.useState([
+    {
+      id: String(Math.random()),
+      title: 'Hipster Ipsum',
+      content:
+        'Lorem ipsum dolor amet iPhone pabst celiac church-key chia intelligentsia meh tousled freegan',
+    },
+    {
+      id: String(Math.random()),
+      variant: TOAST_VARIANTS.SUCCESS,
+      title: 'Hipster Ipsum',
+      content:
+        'Tilde gentrify single-origin coffee lo-fi roof party twee. Chillwave stumptown street art four dollar toast literally cred artisan',
+    },
+    {
+      id: String(Math.random()),
+      variant: TOAST_VARIANTS.WARNING,
+      title: 'Hipster Ipsum',
+      content: 'Cold-pressed beard narwhal ennui',
+    },
+    {
+      id: String(Math.random()),
+      variant: TOAST_VARIANTS.ERROR,
+      title: 'Hipster Ipsum',
+      content:
+        'Thundercats la croix microdosing shoreditch, green juice VHS YOLO taxidermy adaptogen literally',
+    },
+  ]);
+  const push = () =>
+    setStack(stack => [
+      ...stack,
       {
+        id: String(Math.random()),
         title: 'Hipster Ipsum',
         content:
           'Lorem ipsum dolor amet iPhone pabst celiac church-key chia intelligentsia meh tousled freegan',
       },
-      {
-        variant: TOAST_VARIANTS.SUCCESS,
-        title: 'Hipster Ipsum',
-        content:
-          'Tilde gentrify single-origin coffee lo-fi roof party twee. Chillwave stumptown street art four dollar toast literally cred artisan',
-      },
-      {
-        variant: TOAST_VARIANTS.WARNING,
-        title: 'Hipster Ipsum',
-        content: 'Cold-pressed beard narwhal ennui',
-      },
-      {
-        variant: TOAST_VARIANTS.ERROR,
-        title: 'Hipster Ipsum',
-        content:
-          'Thundercats la croix microdosing shoreditch, green juice VHS YOLO taxidermy adaptogen literally',
-      },
-    ]}
-  />
-));
+    ]);
+  const pop = () => setStack(stack => stack.slice(0, stack.length - 1));
+  return (
+    <div>
+      <ToastStack onInteraction={action('onInteraction')} toastConfigs={stack} />
+      <Button onClick={push}>Push</Button>
+      <Button onClick={pop}>Pop</Button>
+    </div>
+  );
+};
+
+const ToastStackStories = storiesOf(`${__dirname}`, module).add('Basic', () => <Story />);
 
 export default ToastStackStories;
