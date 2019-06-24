43c372223ceddd8cdf6d1aded7465472c7426df9
Minh <hlminh2000@gmail.com>
2019-06-24 10:24:47 -0400

    adds basic ToastStack component

---
+++ client/uikit/ToastStack/stories.js
@@ -0,0 +1,35 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import ToastStack from '.';
+import { TOAST_VARIANTS } from 'uikit/Toast';
+
+const ToastStackStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
+  <ToastStack
+    toastConfigs={[
+      {
+        title: 'Hipster Ipsum',
+        content:
+          'Lorem ipsum dolor amet iPhone pabst celiac church-key chia intelligentsia meh tousled freegan',
+      },
+      {
+        variant: TOAST_VARIANTS.SUCCESS,
+        title: 'Hipster Ipsum',
+        content:
+          'Tilde gentrify single-origin coffee lo-fi roof party twee. Chillwave stumptown street art four dollar toast literally cred artisan',
+      },
+      {
+        variant: TOAST_VARIANTS.WARNING,
+        title: 'Hipster Ipsum',
+        content: 'Cold-pressed beard narwhal ennui',
+      },
+      {
+        variant: TOAST_VARIANTS.ERROR,
+        title: 'Hipster Ipsum',
+        content:
+          'Thundercats la croix microdosing shoreditch, green juice VHS YOLO taxidermy adaptogen literally',
+      },
+    ]}
+  />
+));
+
+export default ToastStackStories;
