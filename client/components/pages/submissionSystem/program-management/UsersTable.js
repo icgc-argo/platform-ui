a97fbc3bfd6020d00f81ae770a522f3fe62a6141
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-06-25 10:50:43 -0400

    add column types

--- client/components/pages/submissionSystem/program-management/UsersTable.js
+++ client/components/pages/submissionSystem/program-management/UsersTable.js
@@ -17,6 +17,8 @@ type UsersTableUser = {
   joinDate: string | null,
 };
 
+type CellProps = { original: UsersTableUser };
+
 const RoleDisplayName: { [key: RoleKey]: string } = {
   ADMINISTRATOR: 'Administrator',
   DATA_SUBMITTER: 'Data Submitter',
@@ -28,6 +30,48 @@ const StatusDisplayName: { [key: StatusKey]: string } = {
   PENDING_INVITATION: 'Pending Invitation',
 };
 
-const UsersTable = (tableProps: {}) => {};
+const UsersTable = (tableProps: {
+  users: Array<UsersTableUser>,
+  onUsersEditClick: ({ user: UsersTableUser }) => void,
+  onUsersDeleteClick: ({ user: UsersTableUser }) => void,
+  onUsersResendInviteClick: ({ user: UsersTableUser }) => void,
+}) => {
+  const columns: Array<{
+    Header: string,
+    accessor?: $Keys<UsersTableUser>,
+    Cell?: CellProps => any,
+    sortable?: boolean,
+    width?: number,
+    headerStyle?: {},
+  }> = [
+    {
+      Header: 'Name',
+      accessor: 'name',
+    },
+    {
+      Header: 'Email',
+      accessor: 'email',
+    },
+    {
+      Header: 'Role',
+      accessor: 'role',
+      Cell: ({ original }) => (original.role ? RoleDisplayName[original.role] : ''),
+    },
+    {
+      Header: 'Daco Approved',
+      accessor: 'isDacoApproved',
+    },
+    {
+      Header: 'Status',
+      accessor: 'status',
+      Cell: ({ original }) => (original.status ? StatusDisplayName[original.status] : ''),
+    },
+    {
+      Header: 'Joined On',
+      accessor: 'joinDate',
+    },
+  ];
+  return <Table data={[]} columns={columns} />;
+};
 
 export default UsersTable;
