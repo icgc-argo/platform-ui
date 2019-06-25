730d87abe79958043835c93a38c7d4e74f46638f
Minh <hlminh2000@gmail.com>
2019-06-25 15:20:45 -0400

    adds modal ui component

---
+++ client/uikit/Modal/stories.js
@@ -0,0 +1,28 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import Modal from '.';
+import { action } from '@storybook/addon-actions';
+import { text } from '@storybook/addon-knobs';
+
+const ModalStories = storiesOf(`${__dirname}`, module).add('Basic', () => {
+  const title = text('title', 'Hipster');
+  const children = text(
+    'children',
+    `Lorem ipsum dolor amet hoodie iPhone palo santo freegan bitters chicharrones fingerstache taiyaki authentic fam skateboard next level helvetica forage. Squid typewriter mustache, chia shoreditch retro gluten-free tbh bicycle rights kickstarter pickled pork belly post-ironic. Authentic green juice tofu kickstarter. Yuccie shoreditch mustache, asymmetrical flexitarian four loko austin edison bulb 3 wolf moon stumptown church-key poutine everyday carry affogato prism. Cardigan church-key coloring book microdosing pabst listicle. Try-hard salvia paleo cloud bread, tbh tattooed craft beer chartreuse meh kinfolk squid austin. Fixie blog craft beer tacos, brunch shabby chic artisan bespoke DIY air plant four dollar toast art party vegan franzen.`,
+  );
+  const actionButtonText = text('actionButtonText', 'Beer Me');
+  const cancelText = text('cancelText', 'nevermind');
+  return (
+    <Modal
+      title={title}
+      actionButtonText={actionButtonText}
+      cancelText={cancelText}
+      onActionClick={action('onActionClick')}
+      onCancelClick={action('onCancelClick')}
+    >
+      {children}
+    </Modal>
+  );
+});
+
+export default ModalStories;
