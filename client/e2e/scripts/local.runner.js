c90423f998f1dc44988df47b86b95858f079feb1
Minh <hlminh2000@gmail.com>
2019-06-27 16:53:17 -0400

    make local test script check if theres already a running BS client

--- client/e2e/scripts/local.runner.js
+++ client/e2e/scripts/local.runner.js
@@ -2,37 +2,46 @@
 
 require('dotenv').config();
 
-var Nightwatch = require('nightwatch');
-var browserstack = require('browserstack-local');
-var bs_local;
+const fetch = require('isomorphic-fetch');
+const Nightwatch = require('nightwatch');
+const browserstack = require('browserstack-local');
+
+// Code to start browserstack local before start of test
+let bs_local;
+const onBsStart = function(error) {
+  if (error) {
+    throw error;
+  }
+
+  console.log('Connected. Now testing...');
+  Nightwatch.cli(function(argv) {
+    Nightwatch.CliRunner(argv)
+      .setup(null, function() {
+        // Code to stop browserstack local after end of parallel test
+        bs_local.stop(function() {});
+      })
+      .runTests(function() {
+        // Code to stop browserstack local after end of single test
+        bs_local.stop(function() {});
+      });
+  });
+};
 
 try {
   process.mainModule.filename = './node_modules/.bin/nightwatch';
 
-  // Code to start browserstack local before start of test
   console.log('Connecting local');
   Nightwatch.bs_local = bs_local = new browserstack.Local();
-  bs_local.start(
-    { key: process.env.BROWSERSTACK_ACCESS_KEY, localIdentifier: String(Math.random()) },
-    function(error) {
-      if (error) {
-        throw error;
-      }
-
-      console.log('Connected. Now testing...');
-      Nightwatch.cli(function(argv) {
-        Nightwatch.CliRunner(argv)
-          .setup(null, function() {
-            // Code to stop browserstack local after end of parallel test
-            bs_local.stop(function() {});
-          })
-          .runTests(function() {
-            // Code to stop browserstack local after end of single test
-            bs_local.stop(function() {});
-          });
-      });
-    },
-  );
+  fetch('http://localhost:45691') // dirty test to check if there's already a local client
+    .then(() => {
+      bs_local.start(
+        { key: process.env.BROWSERSTACK_ACCESS_KEY, localIdentifier: String(Math.random()) },
+        onBsStart,
+      );
+    })
+    .catch(err => {
+      bs_local.start({ key: process.env.BROWSERSTACK_ACCESS_KEY }, onBsStart);
+    });
 } catch (ex) {
   console.log('There was an error while starting the test runner:\n\n');
   process.stderr.write(ex.stack + '\n');
