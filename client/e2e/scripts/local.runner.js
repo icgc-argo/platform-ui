ec29075586a483ab68291cb9f7295f4ff39b25d8
Minh <hlminh2000@gmail.com>
2019-06-27 12:10:39 -0400

    adds defensive stopping of browserstack-local

--- client/e2e/scripts/local.runner.js
+++ client/e2e/scripts/local.runner.js
@@ -12,24 +12,36 @@ try {
   // Code to start browserstack local before start of test
   console.log('Connecting local');
   Nightwatch.bs_local = bs_local = new browserstack.Local();
-  bs_local.start({ key: process.env.BROWSERSTACK_ACCESS_KEY }, function(error) {
-    if (error) throw error;
+  bs_local.stop(function(error) {
+    if (error) {
+      throw error;
+    }
+    bs_local.start({ key: process.env.BROWSERSTACK_ACCESS_KEY }, function(error) {
+      if (error) {
+        throw error;
+      }
 
-    console.log('Connected. Now testing...');
-    Nightwatch.cli(function(argv) {
-      Nightwatch.CliRunner(argv)
-        .setup(null, function() {
-          // Code to stop browserstack local after end of parallel test
-          bs_local.stop(function() {});
-        })
-        .runTests(function() {
-          // Code to stop browserstack local after end of single test
-          bs_local.stop(function() {});
-        });
+      console.log('Connected. Now testing...');
+      Nightwatch.cli(function(argv) {
+        Nightwatch.CliRunner(argv)
+          .setup(null, function() {
+            // Code to stop browserstack local after end of parallel test
+            bs_local.stop(function() {});
+          })
+          .runTests(function() {
+            // Code to stop browserstack local after end of single test
+            bs_local.stop(function() {});
+          });
+      });
     });
   });
 } catch (ex) {
-  console.log('There was an error while starting the test runner:\n\n');
-  process.stderr.write(ex.stack + '\n');
-  process.exit(2);
+  bs_local.stop(function(error) {
+    if (error) {
+      console.log(`error stopping bs_local: ${error}\n`);
+    }
+    console.log('There was an error while starting the test runner:\n\n');
+    process.stderr.write(ex.stack + '\n');
+    process.exit(2);
+  });
 }
