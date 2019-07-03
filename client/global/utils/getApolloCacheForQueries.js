94c4f1fc2c225e95354b115cc3a2b8b2187a0155
Minh <hlminh2000@gmail.com>
2019-07-03 14:16:18 -0400

    server-side prefetch must send user egoJwt

--- client/global/utils/getApolloCacheForQueries.js
+++ client/global/utils/getApolloCacheForQueries.js
@@ -9,13 +9,18 @@ import fetch from 'isomorphic-fetch';
 import { GATEWAY_API_ROOT } from 'global/config';
 import createInMemoryCache from './createInMemoryCache';
 
-export default async (queries: Array<{ query: any, variables?: { [key: string]: any } }>) => {
+export default (queries: Array<{ query: any, variables?: { [key: string]: any } }>) => async (
+  egoJwt: string,
+) => {
   const apolloClient = new ApolloClient({
     ssrMode: typeof window === 'undefined',
     // $FlowFixMe apollo-client and apollo-link-http have a type conflict in their typing
     link: createHttpLink({
       uri: urlJoin(GATEWAY_API_ROOT, '/graphql'),
       fetch: fetch,
+      headers: {
+        authorization: egoJwt,
+      },
     }),
     cache: createInMemoryCache(),
   });
--- client/pages/program-management.js
+++ client/pages/program-management.js
@@ -4,7 +4,6 @@ import React from 'react';
 import { isRdpcMember } from 'global/utils/egoJwt';
 import { createPage } from 'global/utils/pages';
 import ProgramManagement from 'components/pages/submissionSystem/program-management';
-import getApolloCacheForQueries from 'global/utils/getApolloCacheForQueries';
 
 // $FlowFixMe .gql file not supported
 import { programsListQuery } from 'components/pages/submissionSystem/programs/queries.gql';
