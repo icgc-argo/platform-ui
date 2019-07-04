f7cb0c6283d3cebc228249e35551adc10423d87c
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-07-04 12:39:44 -0400

    add components storybook section

---
+++ client/stories/comp.stories.js
@@ -0,0 +1,14 @@
+import { configure, addDecorator } from '@storybook/react';
+import React from 'react';
+
+import { ThemeProvider } from '../uikit';
+
+const req = require.context('../components', true, /.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+addDecorator(story => <ThemeProvider>{story()}</ThemeProvider>);
+
+configure(loadStories, module);
