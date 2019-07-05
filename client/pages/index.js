aa6a98fc4e8c119054d62cd339d4147a31d515f4
Minh <hlminh2000@gmail.com>
2019-07-05 15:02:21 -0400

    renames landing page component to login

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,9 @@
 // @flow
 import React from 'react';
-import LandingPage from 'components/pages/landing';
+import LoginPage from 'components/pages/login';
 import { createPage } from 'global/utils/pages';
 
 export default createPage({
   isPublic: true,
-  getInitialProps: LandingPage.getInitialProps,
-})(props => <LandingPage {...props} />);
+  getInitialProps: async () => ({}),
+})(props => <LoginPage {...props} />);
