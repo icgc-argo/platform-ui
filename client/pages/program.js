d8e7abd1c811a0543153d00dc660bda3cc7b026c
Minh <hlminh2000@gmail.com>
2019-07-05 16:05:26 -0400

    change /program-management path to /program

---
+++ client/pages/program.js
@@ -0,0 +1,25 @@
+// @flow
+import React from 'react';
+import { get } from 'lodash';
+
+import { decodeToken } from 'global/utils/egoJwt';
+import { isRdpcMember, hasAccessToProgram } from 'global/utils/egoJwt';
+import { createPage } from 'global/utils/pages';
+import ProgramManagement from 'components/pages/submissionSystem/program-management';
+
+export default createPage({
+  isPublic: false,
+  isAccessible: async ({ egoJwt, ctx }) => {
+    const {
+      query: { id },
+    } = ctx;
+    if (id) {
+      return !isRdpcMember(egoJwt) && hasAccessToProgram({ egoJwt, programId: id });
+    } else {
+      return true;
+    }
+  },
+  getInitialProps: async ({ query }) => ({
+    programId: query.id,
+  }),
+})(ProgramManagement);
