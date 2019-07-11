11800b97626b642ffd28eca7f02059a6af1b92ba
Minh <hlminh2000@gmail.com>
2019-07-11 18:17:45 -0400

    standardize program pages access control
    defines autho logic per page because next.js cannot build non-component files

--- client/pages/program/clinical-submission/[shortName].js
+++ client/pages/program/clinical-submission/[shortName].js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import { createPage } from 'global/utils/pages';
 import programClinicalSubmission from 'components/pages/submission-system/program-clinical-submission';
+import { isRdpcMember, isProgramAdmin } from 'global/utils/egoJwt';
 
 export default createPage({
-  isPublic: true,
-  isAccessible: async ({ egoJwt, ctx }) => {
-    return true;
+  isPublic: false,
+  isAccessible: ({ egoJwt, ctx }) => {
+    const {
+      query: { shortName },
+    } = ctx;
+    return !isRdpcMember(egoJwt) && isProgramAdmin({ egoJwt, programId: shortName });
   },
 })(programClinicalSubmission);
--- client/pages/program/manage/[shortName].js
+++ client/pages/program/manage/[shortName].js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { isRdpcMember, isProgramAdmin } from 'global/utils/egoJwt';
 import { createPage } from 'global/utils/pages';
 import ProgramManagement from 'components/pages/submission-system/program-management';
+import { isRdpcMember, isProgramAdmin } from 'global/utils/egoJwt';
 
 export default createPage({
-  isPublic: true,
-  isAccessible: async ({ egoJwt, ctx }) => {
+  isPublic: false,
+  isAccessible: ({ egoJwt, ctx }) => {
     const {
       query: { shortName },
     } = ctx;
