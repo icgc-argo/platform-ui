812d25bfed2ae23753ce21708c3acaccd935153f
Minh <hlminh2000@gmail.com>
2019-07-11 18:43:47 -0400

    puts submission system paths under /submission

--- client/global/constants/pages.js
+++ client/global/constants/pages.js
@@ -1,10 +1,12 @@
 export const CREATE_PROGRAM_PAGE_PATH = '/create-program';
 export const LOGIN_PAGE_PATH = '/login';
-export const PROGRAMS_LIST_PATH = '/programs';
-export const PROGRAM_ENTITY_PATH = '/program';
-export const DCC_OVERVIEW_PATH = '/submission';
 export const USER_PAGE_PATH = '/user';
-export const PROGRAM_MANAGE_PATH = '/program/manage/[shortName]';
-export const PROGRAM_DASHBOARD_PATH = '/program/dashboard/[shortName]';
-export const PROGRAM_ID_REGISTRATION_PATH = '/program/id-registration/[shortName]';
-export const PROGRAM_CLINICAL_SUBMISSION_PATH = '/program/clinical-submission/[shortName]';
+
+export const DCC_OVERVIEW_PATH = '/submission/submission';
+export const PROGRAMS_LIST_PATH = '/submission/programs';
+export const PROGRAM_ENTITY_PATH = '/submission/program';
+export const PROGRAM_MANAGE_PATH = '/submission/program/manage/[shortName]';
+export const PROGRAM_DASHBOARD_PATH = '/submission/program/dashboard/[shortName]';
+export const PROGRAM_ID_REGISTRATION_PATH = '/submission/program/id-registration/[shortName]';
+export const PROGRAM_CLINICAL_SUBMISSION_PATH =
+  '/submission/program/clinical-submission/[shortName]';
