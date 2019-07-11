3441313f21e46b89d29d5de29d798924230cc0e3
Minh <hlminh2000@gmail.com>
2019-07-11 14:44:20 -0400

    removes error pages to speed up build

--- client/pages/500.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from 'react';
-import { createPage } from 'global/utils/pages';
-import Error500Page from 'components/pages/500';
-
-export default createPage({
-  isPublic: true,
-  getInitialProps: async () => ({}),
-})(Error500Page);
