f2ad1cab9595118275ecfc9ab76a1e51966baf66
Minh <hlminh2000@gmail.com>
2019-07-11 11:31:08 -0400

    lets _error handle errors

--- client/pages/_error.js
+++ client/pages/_error.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import get from 'lodash/get';
 import Page404 from 'components/pages/404';
+import Page401 from 'components/pages/401';
 import Page500 from 'components/pages/500';
 
 class Error extends React.Component {
   static getInitialProps({ res, err }) {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
-    return { statusCode };
+    const STATUS_KEY = 'statusCode';
+    if (err[STATUS_KEY] === 401) {
+      res.status(401);
+    }
+    return { [STATUS_KEY]: get(res, STATUS_KEY) || get(err, STATUS_KEY) || null };
   }
 
   render() {
@@ -14,6 +19,9 @@ class Error extends React.Component {
     }
     if (this.props.statusCode === 500) {
       return <Page500 />;
+    }
+    if (this.props.statusCode === 401) {
+      return <Page401 />;
     }
     return (
       <p>
