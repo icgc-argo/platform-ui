deb70cf6ffc3cb72824e78ef09ec98f96c6c2513
Minh <hlminh2000@gmail.com>
2019-07-11 18:17:45 -0400

    standardize program pages access control
    defines autho logic per page because next.js cannot build non-component files

--- client/pages/program/dashboard/[shortName].js
+++ client/pages/program/dashboard/[shortName].js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import { createPage } from 'global/utils/pages';
 import ProgramDashboard from 'components/pages/submission-system/program-dashboard';
+import { isRdpcMember, isProgramAdmin } from 'global/utils/egoJwt';
 
 export default createPage({
-  isPublic: true,
-  isAccessible: async ({ egoJwt, ctx }) => {
-    return true;
+  isPublic: false,
+  isAccessible: ({ egoJwt, ctx }) => {
+    const {
+      query: { shortName },
+    } = ctx;
+    return !isRdpcMember(egoJwt) && isProgramAdmin({ egoJwt, programId: shortName });
   },
 })(ProgramDashboard);
