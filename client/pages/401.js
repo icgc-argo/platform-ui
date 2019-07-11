2c7b4cf4c07b21ca87068820ceb254d29cce136b
Minh <hlminh2000@gmail.com>
2019-07-11 14:44:20 -0400

    removes error pages to speed up build

--- client/pages/401.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from 'react';
-import { createPage } from 'global/utils/pages';
-import Error401Page from 'components/pages/401';
-
-export default createPage({
-  isPublic: true,
-  getInitialProps: async () => ({}),
-})(Error401Page);
--- client/pages/404.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from 'react';
-import { createPage } from 'global/utils/pages';
-import Error404Page from 'components/pages/404';
-
-export default createPage({
-  isPublic: true,
-  getInitialProps: async () => ({}),
-})(Error404Page);
