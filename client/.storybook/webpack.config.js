5c9232a143e8c8c30f368b5fa50fa9ed3f282644
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-07-12 12:41:07 -0400

    add graphql loader to storybook webpack

--- client/.storybook/webpack.config.js
+++ client/.storybook/webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
 
-// Export a function. Accept the base config as the only param.
-module.exports = async ({ config, mode }) => {
-  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
-
-  // Make whatever fine-grained changes you need
+module.exports = async ({ config }) => {
   config.node = {
     __dirname: true,
     __filename: true,
   };
 
   config.resolve.modules.push(path.resolve(__dirname, '../'));
+
+  config.module.rules.push({
+    test: /\.(graphql|gql)$/,
+    exclude: /node_modules/,
+    loader: 'graphql-tag/loader',
+  });
 
   // Return the altered config
   return config;
