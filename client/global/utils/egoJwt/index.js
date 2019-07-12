8b7c14c7b0aae8088b9768bb94f037d236fcebc0
Minh <hlminh2000@gmail.com>
2019-07-12 16:33:04 -0400

    converts client/global/utils/egoJwt/index.js to use @icgc-argo/ego-token-utils
    fixes import for javascript for now

--- client/global/utils/egoJwt/index.js
+++ client/global/utils/egoJwt/index.js
@@ -2,23 +2,8 @@
 import jwtDecode from 'jwt-decode';
 import { get, memoize } from 'lodash';
 
-import { asEnum } from '../common';
-
-const PERMISSIONS: {
-  READ: string,
-  WRITE: string,
-  ADMIN: string,
-  DENY: string,
-} = {
-  READ: 'READ',
-  WRITE: 'WRITE',
-  ADMIN: 'ADMIN',
-  DENY: 'DENY',
-};
-const DCC_PREFIX = 'program-service.WRITE';
-const RDPC_PREFIX = 'RDPC-';
-const PROGRAM_PREFIX = 'PROGRAM-';
-const PROGRAM_DATA_PREFIX = 'PROGRAMDATA-';
+// $FlowFixMe
+import TokenUtils from '@icgc-argo/ego-token-utils/dist/lib/ego-token-utils';
 
 type EgoJwtData = {
   iat: number,
@@ -47,107 +32,30 @@ type EgoJwtData = {
 
 type PermissionScopeObj = {
   policy: string,
-  permission: $Values<typeof PERMISSIONS>,
+  permission: 'READ' | 'WRITE' | 'ADMIN' | 'DENY',
 };
 
-export const decodeToken = memoize<[string], EgoJwtData>(egoJwt => {
-  return jwtDecode(egoJwt);
-});
+export const decodeToken = memoize<[string], EgoJwtData>(egoJwt => TokenUtils.decodeToken(egoJwt));
 
-export const isValidJwt = (egoJwt: string) => {
-  try {
-    const { exp } = decodeToken(egoJwt);
-    return exp * 1000 > Date.now();
-  } catch (err) {
-    return false;
-  }
-};
+export const isValidJwt = (egoJwt: string): boolean => TokenUtils.isValidJwt(egoJwt);
 
-export const isDccMember = (egoJwt: string) => {
-  try {
-    const data = decodeToken(egoJwt);
-    const permissions = data.context.user.permissions;
-    return permissions.some(p => p.includes(DCC_PREFIX));
-  } catch (err) {
-    return false;
-  }
-};
+export const isDccMember = (egoJwt: string): boolean => TokenUtils.isDccMember(egoJwt);
 
-export const isRdpcMember = (egoJwt: string) => {
-  try {
-    const data = decodeToken(egoJwt);
-    const permissions = data.context.user.permissions;
-    const rdpcPermissions = permissions.filter(p => {
-      const policy = p.split('.')[0];
-      return policy.indexOf(RDPC_PREFIX) === 0;
-    });
-    const isMember =
-      rdpcPermissions.some(p =>
-        [PERMISSIONS.READ, PERMISSIONS.WRITE, PERMISSIONS.ADMIN].includes(p.split('.')[1]),
-      ) && !rdpcPermissions.some(p => [PERMISSIONS.DENY].includes(p.split('.')[1]));
-    return isMember;
-  } catch (err) {
-    return false;
-  }
-};
+export const isRdpcMember = (egoJwt: string): boolean => TokenUtils.isRdpcMember(egoJwt);
 
-export const parseScope = (scope: string): PermissionScopeObj => {
-  const permission = scope.split('.')[1];
-  if (Object.values(PERMISSIONS).includes(permission)) {
-    return {
-      policy: scope.split('.')[0],
-      permission,
-    };
-  } else {
-    throw new Error(`invalid scope: ${scope}`);
-  }
-};
+export const parseScope = (scope: string): PermissionScopeObj => TokenUtils.parseScope(scope);
 
 export const serializeScope = (scopeObj: PermissionScopeObj): string =>
-  `${scopeObj.policy}.${scopeObj.permission}`;
+  TokenUtils.serializeScope(scopeObj);
 
-export const getAuthorizedProgramScopes = (egoJwt: string): Array<PermissionScopeObj> => {
-  const data = decodeToken(egoJwt);
-  const permissions = data.context.user.permissions;
-  const programPermissions = permissions.filter(p => {
-    const policy = p.split('.')[0];
-    const output =
-      policy.indexOf(PROGRAM_PREFIX) === 0 && policy.indexOf(PROGRAM_DATA_PREFIX) !== 0;
-    return output;
-  });
-  return programPermissions.reduce((acc, p) => {
-    const scopeObj = parseScope(p);
-    if (
-      [PERMISSIONS.READ, PERMISSIONS.WRITE, PERMISSIONS.ADMIN].includes(scopeObj.permission) &&
-      ![PERMISSIONS.DENY].includes(scopeObj.permission)
-    ) {
-      acc.push(scopeObj);
-    }
-    return acc;
-  }, []);
-};
+export const getAuthorizedProgramScopes = (egoJwt: string): Array<PermissionScopeObj> =>
+  TokenUtils.getReadableProgramScopes(egoJwt);
 
-export const canReadProgram = (args: { egoJwt: string, programId: string }): boolean => {
-  const authorizedProgramScopes = getAuthorizedProgramScopes(args.egoJwt);
-  return authorizedProgramScopes.some(({ policy }) => policy.includes(args.programId));
-};
+export const canReadProgram = (args: { egoJwt: string, programId: string }): boolean =>
+  TokenUtils.canReadProgram(args);
 
-export const canWriteProgram = (args: { egoJwt: string, programId: string }): boolean => {
-  const authorizedProgramScopes = getAuthorizedProgramScopes(args.egoJwt);
-  return authorizedProgramScopes.some(
-    ({ policy, permission }) =>
-      policy.includes(args.programId) &&
-      [PERMISSIONS.WRITE, PERMISSIONS.ADMIN].includes(permission),
-  );
-};
+export const canWriteProgram = (args: { egoJwt: string, programId: string }): boolean =>
+  TokenUtils.canWriteProgram(args);
 
-export const isProgramAdmin = (args: { egoJwt: string, programId: string }): boolean => {
-  return canWriteProgram(args);
-
-  /** TODO: switch to below logic when .ADMIN scope is available */
-
-  // const authorizedProgramScopes = getAuthorizedProgramScopes(args.egoJwt);
-  // return authorizedProgramScopes.some(
-  //   ({ policy, permission }) => policy.includes(args.programId) && permission === PERMISSIONS.ADMIN,
-  // );
-};
+export const isProgramAdmin = (args: { egoJwt: string, programId: string }): boolean =>
+  TokenUtils.isProgramAdmin(args);
