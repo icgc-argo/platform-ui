d7f6256deb8cbbf0799d3c33dbd9d29e3eb404bb
Xu Deng <d8660091@gmail.com>
2019-07-12 15:44:51 -0400

    Add layout to several pages

--- client/components/pages/submission-system/program-clinical-submission/index.js
+++ client/components/pages/submission-system/program-clinical-submission/index.js
@@ -1,5 +1,35 @@
 import * as React from 'react';
+import SubmissionLayout from '../layout';
+import { css } from 'uikit';
+import TitleBar from 'uikit/TitleBar';
+import { ContentBox } from 'uikit/PageLayout';
 
-export default function ProgramClinicalSubmission() {
-  return <div>Program Clinical Submission</div>;
+export default function ProgramClinicalSubmission({ logOut, pathname }) {
+  return (
+    <SubmissionLayout
+      pathname={pathname}
+      logOut={logOut}
+      contentHeader={
+        <div
+          css={css`
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+          `}
+        >
+          <TitleBar>
+            <>Clinical Submission</>
+          </TitleBar>
+        </div>
+      }
+    >
+      <ContentBox
+        css={css`
+          padding-top: 0px;
+        `}
+      >
+        Clinical Submission
+      </ContentBox>
+    </SubmissionLayout>
+  );
 }
