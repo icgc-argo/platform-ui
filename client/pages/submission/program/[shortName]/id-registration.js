05d5e1dc09097db7c4f8c085325cd9540c7af5a1
Minh <hlminh2000@gmail.com>
2019-07-15 14:21:32 -0400

    makes program dashboard, clinical submission and id registration only accessible by those with program read scope

--- client/pages/submission/program/[shortName]/id-registration.js
+++ client/pages/submission/program/[shortName]/id-registration.js
@@ -1,15 +1,16 @@
+//@flow
 import React from 'react';
 
 import { createPage } from 'global/utils/pages';
 import programIDRegistration from 'components/pages/submission-system/program-id-registration';
-import { isRdpcMember, isProgramAdmin } from 'global/utils/egoJwt';
+import { isRdpcMember, canReadProgram } from 'global/utils/egoJwt';
 
 export default createPage({
   isPublic: false,
-  isAccessible: ({ egoJwt, ctx }) => {
+  isAccessible: async ({ egoJwt, ctx }) => {
     const {
       query: { shortName },
     } = ctx;
-    return !isRdpcMember(egoJwt);
+    return !isRdpcMember(egoJwt) && canReadProgram({ egoJwt, programId: shortName });
   },
 })(programIDRegistration);
