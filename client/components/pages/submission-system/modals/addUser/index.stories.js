f3fd937ddfc208565aad5f1c65770050cf4e43af
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-07-19 13:05:18 -0400

    remoive unused import

--- client/components/pages/submission-system/modals/addUser/index.stories.js
+++ client/components/pages/submission-system/modals/addUser/index.stories.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import UserSection from './userSection';
 import AddUserModal from '.';
 
-const UserSectionStory = storiesOf(`${__dirname}`, module).add('Basic', () => <AddUserModal />);
+const AddUserModalStory = storiesOf(`${__dirname}`, module).add('Basic', () => <AddUserModal />);
 
-export default UserSectionStory;
+export default AddUserModalStory;
