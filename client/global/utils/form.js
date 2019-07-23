ad0d7cfd56a530bff581fe2e16b5aa71d11d26d8
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-07-23 14:26:01 -0400

    add form util for error msgs

---
+++ client/global/utils/form.js
@@ -0,0 +1,3 @@
+// error messages
+export const requiredError = fieldName => `Please fill in ${fieldName}.`;
+export const multiSelectError = fieldName => `${fieldName} field must have at least 1 item.`;
