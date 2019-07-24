bc75cfe7649130539063734d6730c0dd62e36645
ciaran schutte <ciaranschutte@gmail.com>
2019-07-24 14:01:07 -0400

    add storybook actions and proptypes

--- client/components/pages/submission-system/modals/deleteUser/index.js
+++ client/components/pages/submission-system/modals/deleteUser/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'uikit/Modal';
+import PropTypes from 'prop-types';
 
 const DeleteUserModal = ({ user, dismissModal, onSubmit }) => {
   return (
@@ -15,6 +16,12 @@ const DeleteUserModal = ({ user, dismissModal, onSubmit }) => {
       Are you sure you want to remove <strong>{user ? user.name : ''}</strong> from the program?
     </Modal>
   );
+};
+
+DeleteUserModal.propTypes = {
+  user: PropTypes.object.isRequired,
+  dismissModal: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
 
 export default DeleteUserModal;
