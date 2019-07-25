98f567cf8fe53f454cf8065624862327f555f917
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2019-07-25 10:14:28 -0400

    Feat/#251 standardise form error (#328)
    * add form util for error msgs
    
    * update error messages in validations
    
    * change multi error name and update

---
+++ client/global/utils/form.js
@@ -0,0 +1,4 @@
+// error messages
+export const requiredError = fieldName => `Please fill in ${fieldName}.`;
+export const mustHaveMoreThanZeroError = fieldName =>
+  `${fieldName} field must have at least 1 item.`;
