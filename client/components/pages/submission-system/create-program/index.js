a2c438a53b8d99e42057a864f233265de5465dc7
Minh <hlminh2000@gmail.com>
2019-08-01 12:49:16 -0400

    bold program name in toast

--- client/components/pages/submission-system/create-program/index.js
+++ client/components/pages/submission-system/create-program/index.js
@@ -29,7 +29,7 @@ export default () => {
     toaster.addToast({
       title: '',
       variant: TOAST_VARIANTS.SUCCESS,
-      content: `The program: ${programName} has been created`,
+      content: <>The program: {<strong>{programName}</strong>} has been created</>,
       onInteraction: event => {
         if (event.type === NOTIFICATION_INTERACTION_EVENTS.ACTION) {
           router.push(PROGRAM_DASHBOARD_PATH.replace(PROGRAM_SHORT_NAME_PATH, programShortName));
