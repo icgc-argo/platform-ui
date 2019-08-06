89faffcebb31512dc152a136958ac64eb93f89c9
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-08-06 17:37:10 -0400

    edit user add flow

--- client/components/pages/submission-system/modals/editUser/index.stories.js
+++ client/components/pages/submission-system/modals/editUser/index.stories.js
@@ -1,7 +1,15 @@
+// @flow
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import EditUserModal from '.';
+import { action } from '@storybook/addon-actions';
 
-const EditUserModalStory = storiesOf(`${__dirname}`, module).add('Basic', () => <EditUserModal />);
+const EditUserModalStory = storiesOf(`${__dirname}`, module).add('Basic', () => (
+  <EditUserModal
+    user={{ firstName: 'Joe', lastName: 'Blogs', email: 'jblogs@example.com', role: 'Full' }}
+    dismissModal={action('dismiss modal')}
+    onSubmit={data => action('submit')(data)}
+  />
+));
 
 export default EditUserModalStory;
