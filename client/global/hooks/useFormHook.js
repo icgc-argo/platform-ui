a5cf0437b661df6e5007600e16a4ba49aabae42a
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-08-07 10:53:19 -0400

    remove section functionality for useFormHook

--- client/global/hooks/useFormHook.js
+++ client/global/hooks/useFormHook.js
@@ -1,131 +1,105 @@
+// @flow
 import React, { useState } from 'react';
 import { get, isArray, flattenDeep } from 'lodash';
 import * as yup from 'yup';
+type FormData = {
+  key: string,
+  val: mixed,
+};
 
 /**
- * Each form is broken down into sections and fields
- * eg. [{name: 'John', age: 45}, {name: 'Pat', age: 54}]
+ * Supports single forms only, {} not [{},{}]
  */
+const useFormHook = ({
+  initialFields,
+  schema: formSchema,
+  disabledFields = [],
+}: {
+  initialFields: {},
+  schema: {
+    validate: typeof yup.object,
+  },
+  disabledFields: [],
+}) => {
+  let initErrors = {};
 
-// assumes passing single init values and not array
-const useFormHook = ({ initialFields, schema: formSchema, disabledFields }) => {
-  const initErrors = [initialFields].map(section => {
-    const field = {};
-    for (let [key, value] of Object.entries(section)) {
-      field[key] = '';
-    }
-    return field;
-  });
+  for (let [key, value] of Object.entries(initialFields)) {
+    initErrors[key] = '';
+  }
 
-  const [form, setForm] = useState({ errors: initErrors, data: [initialFields] });
+  const [form, setForm] = useState({ errors: initErrors, data: initialFields });
   const [touched, setTouched] = useState(false);
   const { errors, data } = form;
 
-  const hasErrors = flattenDeep(errors.map(section => Object.values(section))).some(x => x);
+  const hasErrors = Object.values(errors).some(x => x);
 
   // set form data
-  const setData = ({ key, val, index = 0 }) => {
+  const setData = ({ key, val }: FormData) => {
     if (!touched) setTouched(true);
 
     setForm({
       ...form,
-      data: data.map((field, i) => (i === index ? { ...field, [key]: val } : field)),
-    });
-  };
-
-  // set single error
-  const setError = ({ key, val, index = 0 }) => {
-    setForm({
-      ...form,
-      errors: errors.map((field, i) => (i === index ? { ...field, [key]: val } : field)),
+      data: { ...data, [key]: val },
     });
   };
 
   // set all errors
-  const setErrors = ({ validationErrors, index }) =>
+  const setErrors = ({ validationErrors }) =>
     setForm({
       ...form,
-      errors: errors.map((field, i) => (i === index ? validationErrors : field)),
+      errors: { ...errors },
     });
 
-  // delete a block of values
-  const deleteSection = deletedIndex => {
-    const filterDeleted = (item, index) => index !== deletedIndex;
+  // set single error
+  const setError = ({ key, val }: FormData) => {
     setForm({
-      errors: errors.filter(filterDeleted),
-      data: data.filter(filterDeleted),
-    });
-  };
-
-  // create a block of values
-  const createSection = sectionFields => {
-    setForm({
-      errors: errors.concat(sectionFields),
-      data: data.concat(sectionFields),
+      ...form,
+      errors: { ...errors, [key]: val },
     });
   };
 
   // validate a single field
-  const validateField = async ({ key, index = 0 }) => {
+  const validateField = async ({ key }: { key: string }) => {
     try {
-      const value = await yup.reach(formSchema, key).validate(data[index][key]);
-      setError({ key, val: '', index });
+      const value = await yup.reach(formSchema, key).validate(data[key]);
+      setError({ key, val: '' });
     } catch (fieldError) {
       const message =
         isArray(fieldError.inner) && fieldError.inner.length > 1
           ? fieldError.inner[fieldError.inner.length - 1].message
           : fieldError.message;
 
-      setError({ key, val: message, index });
+      setError({ key, val: message });
     }
   };
 
-  // validate a section
-  const validateSection = async ({ index }) =>
+  // validates entire form
+  const validateForm = (): Promise<any> =>
     new Promise(async (resolve, reject) => {
-      const section = data[index];
-
+      console.log('VALIDATE FORM', formSchema, data);
       try {
-        const validData = await formSchema.validate(section, {
-          abortEarly: false,
+        const validData = await formSchema.validate(data, {
+          //abortEarly: false,
           stripUnknown: true,
         });
+        console.log('valid data', validData);
         resolve(validData);
       } catch (formErrors) {
         const validationErrors = get(formErrors, 'inner', []).reduce((output, error) => {
           output[error.path] = error.message;
           return output;
         }, {});
-
-        setErrors({ validationErrors, index });
+        console.log('errors', formErrors, validationErrors);
+        setErrors({ validationErrors });
         reject(validationErrors);
       }
     });
-
-  // validates entire form
-  const validateForm = () =>
-    Promise.all(
-      data.map(
-        (section, index) =>
-          new Promise(async (resolve, reject) => {
-            try {
-              const validData = await validateSection({ index });
-              resolve(validData);
-            } catch (e) {
-              reject();
-            }
-          }),
-      ),
-    );
 
   return {
     errors,
     data,
     setData,
-    deleteSection,
-    createSection,
     validateField,
-    validateSection,
     validateForm,
     touched,
     hasErrors,
