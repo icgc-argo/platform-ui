e2de0550e425cfc3cba5fcfb37c3787235108eb0
Minh <hlminh2000@gmail.com>
2019-08-07 22:11:21 -0400

    updates @icgc-argo/ego-token-utils

--- client/global/utils/egoJwt/index.js
+++ client/global/utils/egoJwt/index.js
@@ -3,7 +3,7 @@ import jwtDecode from 'jwt-decode';
 import { get, memoize } from 'lodash';
 
 // $FlowFixMe
-import createEgoUtils from '@icgc-argo/ego-token-utils/dist/lib/ego-token-utils';
+import createEgoUtils from '@icgc-argo/ego-token-utils';
 
 const TokenUtils = createEgoUtils();
 type EgoJwtData = {
