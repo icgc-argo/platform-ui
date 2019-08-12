51ee3531218919dcc9858386cd4ae31ab85c8693
Minh <hlminh2000@gmail.com>
2019-08-12 16:26:22 -0400

    implements ClipboardCopyField component

--- client/uikit/ClipboardCopyField/index.js
+++ client/uikit/ClipboardCopyField/index.js
@@ -1,15 +1,112 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// @flow
+import * as React from 'react';
+import { StyledInputWrapper, INPUT_STATES, INPUT_SIZES } from 'uikit/form/common';
+import Button from 'uikit/Button';
+import Typography from 'uikit/Typography';
+import { css, styled } from '..';
+import Tag, { TAG_VARIANTS } from 'uikit/Tag';
+import Icon from 'uikit/Icon';
 
-/*
- * Please edit me!
- */
-const ClipboardCopyField = () => <h1>ClipboardCopyField</h1>;
+const TagWrapper = styled('div')`
+  margin-right: 5px;
+`;
+const PrompMessage = styled('div')`
+  position: absolute;
+  top: 100%;
+  right: 0%;
+  margin-right: 5px;
+`;
 
-ClipboardCopyField.propTypes = {
-  /*
-   * Don't forget about little old prop types!
-   */
+const copyToClipboard = async (data: string) => {
+  await navigator.clipboard.writeText(data);
+};
+
+const ClipboardCopyField = ({
+  value,
+  buttonText = 'copy',
+  disabled = false,
+  tagText,
+  errorText,
+  timeout = 2000,
+}: {
+  buttonText?: string,
+  value?: string,
+  tagText?: string,
+  disabled?: boolean,
+  errorText?: string,
+  timeout?: number,
+}) => {
+  const [promptMsgShown, setPromptMsgShown] = React.useState(false);
+  let currentTimeout: ?TimeoutID = undefined;
+
+  const showPromptMessage = () => {
+    clearTimeout(currentTimeout);
+    setPromptMsgShown(true);
+    currentTimeout = setTimeout(() => {
+      setPromptMsgShown(false);
+    }, timeout);
+  };
+
+  const onCopyClicked = async () => {
+    if (value) {
+      await copyToClipboard(value);
+      showPromptMessage();
+    }
+  };
+
+  return (
+    <div
+      css={css`
+        position: relative;
+      `}
+    >
+      <StyledInputWrapper
+        disabled={disabled}
+        size={INPUT_SIZES.SM}
+        inputState={INPUT_STATES.default}
+        css={css`
+          padding-left: 5px;
+          display: flex;
+          cursor: unset;
+        `}
+      >
+        {(errorText || tagText) && (
+          <TagWrapper>
+            <Tag variant={errorText ? TAG_VARIANTS.ERROR : TAG_VARIANTS.INFO}>
+              {errorText || tagText}
+            </Tag>
+          </TagWrapper>
+        )}
+        <div
+          css={css`
+            flex: 1;
+            text-overflow: ellipsis;
+            overflow: hidden;
+          `}
+        >
+          <Typography variant="default">{value}</Typography>
+        </div>
+        <Button
+          async
+          disabled={disabled}
+          onClick={onCopyClicked}
+          css={css`
+            border-radius: 0px;
+            position: relative;
+          `}
+        >
+          {promptMsgShown ? <Icon name="checkmark" fill="white" height="13px" /> : buttonText}
+        </Button>
+      </StyledInputWrapper>
+      {promptMsgShown && (
+        <PrompMessage>
+          <Typography variant="label" component="div" color="accent2_dark">
+            Copied!
+          </Typography>
+        </PrompMessage>
+      )}
+    </div>
+  );
 };
 
 export default ClipboardCopyField;
--- client/uikit/ClipboardCopyField/stories.js
+++ client/uikit/ClipboardCopyField/stories.js
@@ -1,9 +1,18 @@
+// @flow
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import ClipboardCopyField from '.';
+import { text, boolean } from '@storybook/addon-knobs';
 
-const ClipboardCopyFieldStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
-  <ClipboardCopyField>Skeleton</ClipboardCopyField>
-));
+const ClipboardCopyFieldStories = storiesOf(`${__dirname}`, module).add('Basic', () => {
+  const knobs = {
+    buttonText: text('buttonText', 'copy'),
+    value: text('value', 'bf551c13e8bbf551csd42df343wrfwd3w4dfe43wrfe34d03'),
+    tagText: text('tagText', 'Expires in: 5 days'),
+    disabled: boolean('disabled', false),
+    errorText: text('errorText', undefined),
+  };
+  return <ClipboardCopyField {...knobs} />;
+});
 
 export default ClipboardCopyFieldStories;
