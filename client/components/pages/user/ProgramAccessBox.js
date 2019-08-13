ef686da5f06a199ec293846fa8e80b2ad14e87d1
Minh <hlminh2000@gmail.com>
2019-08-13 16:26:51 -0400

    implements data for DCC and gets ready for other user types

--- client/components/pages/user/ProgramAccessBox.js
+++ client/components/pages/user/ProgramAccessBox.js
@@ -2,21 +2,28 @@
 import * as React from 'react';
 import Typography from 'uikit/Typography';
 import { Row, Col } from 'react-grid-system';
+import { useQuery, useMutation } from 'react-apollo-hooks';
 import { css, styled } from 'uikit';
 import Link from 'uikit/Link';
 import AccessTokenBox from './AccessTokenBox';
 import { Box } from './common';
-import Table from 'uikit/Table';
+import Table, { DefaultLoadingComponent } from 'uikit/Table';
 import useAuthContext from 'global/hooks/useAuthContext';
-import { getAuthorizedProgramScopes } from 'global/utils/egoJwt';
+import { isDccMember, getReadableProgramShortNames, canWriteProgram } from 'global/utils/egoJwt';
 import Icon from 'uikit/Icon';
+import USER_PROGRAM_LIST_QUERY from './USER_PROGRAM_LIST_QUERY.gql';
 
-type T_ProgramTableProgram = { shortName: string, role: string, permissions: Array<string> };
-const ProgramTable = (props: { programs: Array<T_ProgramTableProgram> }) => (
+type T_ProgramTableProgram = {
+  shortName: string,
+  role: string,
+  permissions: Array<string>,
+};
+const ProgramTable = (props: { programs: Array<T_ProgramTableProgram>, loading: boolean }) => (
   <Table
     sortable={false}
     showPagination={false}
     data={props.programs}
+    loading={props.loading}
     columns={
       ([
         { Header: 'Program Name', accessor: 'shortName', maxWidth: 150 },
@@ -27,30 +34,33 @@ const ProgramTable = (props: { programs: Array<T_ProgramTableProgram> }) => (
   />
 );
 
+const PROGRAM_USER_ROLES_DISPLAY = {
+  DCC: 'DCC Admin',
+  PROGRAM_ADMIN: 'Program Admin',
+  SUBMITTER: 'Data Submitter',
+  COLLABORATOR: 'Collaborator',
+};
+
+const PROGRAM_USER_PERMISSIONS_DISPLAY = {
+  DCC: 'Manage Programs, Submit Data, Download Data',
+  PROGRAM_ADMIN: 'Submit Data, Download Data, Manage Users',
+  SUBMITTER: 'Submit Data, Download Data',
+  COLLABORATOR: 'Download Data',
+};
+
 const getProgramTableProgramFromEgoJwt = (egoJwt: string): T_ProgramTableProgram[] => {
-  const readableProgramScopes = getAuthorizedProgramScopes(egoJwt || '');
-  return [
-    {
-      shortName: 'PACA-AU',
-      role: 'Program Administrator',
+  const readableProgramScopes = getReadableProgramShortNames(egoJwt || '');
+  const output = readableProgramScopes.map(shortName => {
+    const role = canWriteProgram({ egoJwt, programId: shortName })
+      ? PROGRAM_USER_ROLES_DISPLAY.PROGRAM_ADMIN
+      : '';
+    return {
+      shortName,
+      role,
       permissions: ['Submit Data, Download Data, Manage Users'],
-    },
-    {
-      shortName: 'PACA-ER',
-      role: 'Program Administrator',
-      permissions: ['Submit Data, Download Data, Manage Users'],
-    },
-    {
-      shortName: 'PACA-GB',
-      role: 'Program Administrator',
-      permissions: ['Submit Data, Download Data, Manage Users'],
-    },
-    {
-      shortName: 'PACA-CA',
-      role: 'Program Administrator',
-      permissions: ['Submit Data, Download Data, Manage Users'],
-    },
-  ];
+    };
+  });
+  return output;
 };
 
 const DacoAccessStatusDisplay = ({ approved }: { approved: boolean }) => {
@@ -127,7 +137,21 @@ const DacoAccessStatusDisplay = ({ approved }: { approved: boolean }) => {
 
 export default function ProgramAccessBox() {
   const { token } = useAuthContext() || {};
-  const programs = getProgramTableProgramFromEgoJwt(token || '');
+  const { data, loading: loadingPrograms, error } = useQuery(USER_PROGRAM_LIST_QUERY);
+  let programs;
+  const isDcc = isDccMember(token);
+  if (isDcc) {
+    programs = loadingPrograms
+      ? []
+      : data.programs.map(({ shortName }) => ({
+          shortName,
+          role: PROGRAM_USER_ROLES_DISPLAY.DCC,
+          permissions: PROGRAM_USER_PERMISSIONS_DISPLAY.DCC,
+        }));
+  } else {
+    programs = getProgramTableProgramFromEgoJwt(token || '');
+  }
+
   return (
     <Box title="Program Access" iconName="programs">
       <div
@@ -143,7 +167,7 @@ export default function ProgramAccessBox() {
         </Typography>
       </div>
       <div>
-        <ProgramTable programs={programs} />
+        <ProgramTable programs={programs} loading={loadingPrograms && isDcc} />
       </div>
     </Box>
   );
