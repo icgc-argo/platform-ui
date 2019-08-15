d30f9d03da6acd66f51cc92c94201120a16ab366
Minh <hlminh2000@gmail.com>
2019-08-15 16:15:01 -0400

    removes git tagging for now

--- Jenkinsfile.gateway.groovy
+++ Jenkinsfile.gateway.groovy
@@ -78,10 +78,6 @@ spec:
             // }
             steps {
                 container('docker') {
-                    withCredentials([usernamePassword(credentialsId: 'argoGithub', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                        sh "git tag ${version}"
-                        sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${githubRepo} --tags"
-                    }
                     withCredentials([usernamePassword(credentialsId:'argoDockerHub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                         sh 'docker login -u $USERNAME -p $PASSWORD'
                     }
