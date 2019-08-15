f038e7404fc6f74f176d26396e3ba89a3c9732f4
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-08-15 18:18:10 -0400

    add storybook knobs

--- client/uikit/Progress/stories.js
+++ client/uikit/Progress/stories.js
@@ -4,9 +4,13 @@ import React from 'react';
 import css from '@emotion/css';
 import ProgressItem from '.';
 import { PROGRESS_STATUS } from '.';
+import { text, select } from '@storybook/addon-knobs';
 
-const ProgressItemStories = storiesOf(`${__dirname}`, module).add('Success', () => (
-  <ProgressItem text="upload" state={PROGRESS_STATUS.SUCCESS} />
+const ProgressItemStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
+  <ProgressItem
+    text={text('Text', 'Upload')}
+    state={select('State', PROGRESS_STATUS, PROGRESS_STATUS.SUCCESS, '')}
+  />
 ));
 
 export default ProgressItemStories;
