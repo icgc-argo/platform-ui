5ef54a787001f092b966523105b1e768539bb95b
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-08-27 11:54:03 -0400

    update storybook more sample errors

--- components/pages/submission-system/program-sample-registration/ErrorTable/index.stories.js
+++ components/pages/submission-system/program-sample-registration/ErrorTable/index.stories.js
@@ -13,6 +13,29 @@ const SAMPLE_ERRORS = [
     errorDescription:
       'Sample is attached to two different specimens in your file. Samples can only be linked to a single specimen.',
   },
+  {
+    row: 4,
+    sampleID: 8013215,
+    field: 'Gender',
+    errorValue: 'Male',
+    errorDescription:
+      'The value does not match the previously registered value for this sample. Please correct your file or contact DCC to update the registered data.',
+  },
+  {
+    row: 5,
+    sampleID: 8013214,
+    field: 'Submitter Specimen ID',
+    errorValue: 'ICGC_0007',
+    errorDescription:
+      'Sample has already been registered with a specimen. Samples can only be linked to a single specimen.  Please correct your file or contact DCC to update the registered data.',
+  },
+  {
+    row: 25,
+    sampleID: 8013213,
+    field: 'Sample Type',
+    errorValue: 'Blood',
+    errorDescription: 'The value is not permissible for this field.',
+  },
 ];
 
 const ErrorTableStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
