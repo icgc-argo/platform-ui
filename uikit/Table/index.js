5e56a615d1fab5a5f3435614c2da803487e03ce1
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-08-29 17:17:33 -0400

    dont show pagination when no data

--- uikit/Table/index.js
+++ uikit/Table/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
 import selectTable from 'react-table/lib/hoc/selectTable';
 import { get } from 'lodash';
+import isEmpty from 'lodash/isEmpty';
 
 import TablePagination from './TablePagination';
 import { StyledTable } from './styledComponent';
@@ -79,6 +80,7 @@ const Table = <Data: { [k: string]: any }>({
   LoadingComponent = DefaultLoadingComponent,
   columns,
   data,
+  showPagination,
   ...rest
 }: TableProps<Data>) => {
   // these are props passed by SelectTable. Defaults are not exposed in props for encapsulation
@@ -105,6 +107,7 @@ const Table = <Data: { [k: string]: any }>({
       minRows={0}
       PaginationComponent={PaginationComponent}
       NoDataComponent={() => null}
+      showPagination={isEmpty(data) ? false : showPagination}
       {...rest}
     />
   );
