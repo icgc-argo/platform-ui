3ca2818d62b8c3f1e888a9d8a14ae29c90dc9c26
Minh <hlminh2000@gmail.com>
2019-09-16 17:20:13 -0400

    🐛 adds global css to support react-tippy unmountHTMLWhenHide prop

--- uikit/Tooltip/index.js
+++ uikit/Tooltip/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
+// @flow
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import { Tooltip as ReactTippy } from 'react-tippy';
+import './index.css';
 
 import { css, styled } from '..';
 import useTheme from '../utils/useTheme';
 import Typography from '../Typography';
 
-const Tooltip = ({ html, ...rest }) => {
+// exposing full react-tippy API based on https://github.com/tvkhoa/react-tippy, leaving out some style specific stuff
+type TooltipProps = {
+  disabled?: boolean,
+  open?: boolean,
+  useContext?: boolean,
+  onRequestClose?: any => any,
+  position?: 'top' | 'bottom' | 'left' | 'right',
+  trigger?: 'mouseenter' | 'focus' | 'click' | 'manual',
+  tabIndex?: number,
+  interactive?: boolean,
+  interactiveBorder?: number,
+  delay?: number,
+  hideDelay?: number,
+  animation?: 'shift' | 'perspective' | 'fade' | 'scale' | 'none',
+  animateFill?: boolean,
+  duration?: number,
+  distance?: number,
+  offset?: number,
+  hideOnClick?: true | false | 'persistent',
+  multiple?: boolean,
+  followCursor?: boolean,
+  inertia?: boolean,
+  transitionFlip?: boolean,
+  popperOptions?: {},
+  html?: React.Node,
+  unmountHTMLWhenHide?: boolean,
+  sticky?: boolean,
+  stickyDuration?: number,
+  touchHold?: boolean,
+  onShow?: any => any,
+  onShown?: any => any,
+  onHide?: any => any,
+  onHidden?: any => any,
+  className?: string,
+  style?: {},
+};
+
+const Tooltip = ({ html, ...rest }: TooltipProps) => {
+  console.log('unmountHTMLWhenHide: ', rest.unmountHTMLWhenHide);
   const theme = useTheme();
   const TooltipContainer = styled('div')`
     ${css(theme.typography.caption)}
@@ -17,43 +57,6 @@ const Tooltip = ({ html, ...rest }) => {
     font-weight: normal;
   `;
   return <ReactTippy html={<TooltipContainer id="tooltip">{html}</TooltipContainer>} {...rest} />;
-};
-
-// exposing full react-tippy API based on https://github.com/tvkhoa/react-tippy, leaving out some style specific stuff
-Tooltip.propTypes = {
-  disabled: PropTypes.bool,
-  open: PropTypes.bool,
-  useContext: PropTypes.bool,
-  onRequestClose: PropTypes.func,
-  position: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
-  trigger: PropTypes.oneOf(['mouseenter', 'focus', 'click', 'manual']),
-  tabIndex: PropTypes.number,
-  interactive: PropTypes.bool,
-  interactiveBorder: PropTypes.number,
-  delay: PropTypes.number,
-  hideDelay: PropTypes.number,
-  animation: PropTypes.oneOf(['shift', 'perspective', 'fade', 'scale', 'none']),
-  animateFill: PropTypes.bool,
-  duration: PropTypes.number,
-  distance: PropTypes.number,
-  offset: PropTypes.number,
-  hideOnClick: PropTypes.oneOf([true, false, 'persistent']),
-  multiple: PropTypes.bool,
-  followCursor: PropTypes.bool,
-  inertia: PropTypes.bool,
-  transitionFlip: PropTypes.bool,
-  popperOptions: PropTypes.object,
-  html: PropTypes.any,
-  unmountHTMLWhenHide: PropTypes.bool,
-  sticky: PropTypes.bool,
-  stickyDuration: PropTypes.number,
-  touchHold: PropTypes.bool,
-  onShow: PropTypes.func,
-  onShown: PropTypes.func,
-  onHide: PropTypes.func,
-  onHidden: PropTypes.func,
-  className: PropTypes.string,
-  style: PropTypes.PropTypes.object,
 };
 
 export default Tooltip;
