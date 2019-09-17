37b66462ef3dc561cd418826a4f48b2a284f63d6
Minh <hlminh2000@gmail.com>
2019-09-17 16:41:37 -0400

    🐛 fixes pages/contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,9 @@
-
 import ContactPage from 'components/pages/contact';
 import { createPage } from 'global/utils/pages';
 import React from 'react';
 
 export default createPage({
   isPublic: true,
-})(({ redirect, egoJwt }) => {
+})(() => {
   return <ContactPage />;
 });
