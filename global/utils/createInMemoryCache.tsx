3fe4df9ca7485d6d65c8e63f67c9829bc7c40d56
Minh <hlminh2000@gmail.com>
2019-09-18 18:33:32 -0400

    🐛 fixes createMemoryCache

--- global/utils/createInMemoryCache.tsx
+++ global/utils/createInMemoryCache.tsx
@@ -1,10 +1,10 @@
-
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export default () =>
   new InMemoryCache({
     dataIdFromObject: obj => {
-      if (obj.__typename === 'Program') return `PROGRAM::${obj.shortName}`;
+      if (obj.__typename === 'Program')
+        return `PROGRAM::${(obj as { shortName: string }).shortName}`;
       else return obj.id;
     },
   });
