682dd6b1f9eee1330284a0cdb728759f9f4e6fc7
Minh <hlminh2000@gmail.com>
2019-09-19 16:54:32 -0400

    🐛 fixes uikit/utils/Hook/index.tsx

--- uikit/utils/Hook/index.tsx
+++ uikit/utils/Hook/index.tsx
@@ -4,26 +4,29 @@ import PropTypes from 'prop-types';
 /*
  * Please edit me!
  */
-const Hook = ({ initialState, render = () => null, effect = () => () => {}, watch = () => [] }) => {
-  const [state, setState] = React.useState(initialState);
-  React.useEffect(effect, watch(state));
-  return render([state, setState]);
-};
-
-Hook.propTypes = {
-  initialState: PropTypes.any.isRequired,
+const Hook: React.ComponentType<{
+  initialState: any;
   /**
    * receives [state, setState] to keep with hooks semantic
    */
-  render: PropTypes.func.isRequired,
+  render: (a: [any, React.Dispatch<any>]) => React.ReactNode | React.ReactNodeArray;
   /**
    * gets passed to React.useEffect, return cleanup function if needed
    */
-  effect: PropTypes.func,
+  effect?: () => void | (() => () => void);
   /**
    * call result gets passed to React.useEffect, receives this component's state
    */
-  watch: PropTypes.func,
+  watch?: (state: any) => any[];
+}> = ({
+  initialState,
+  render = ([]) => null,
+  effect = () => () => {},
+  watch = (state: any) => [],
+}) => {
+  const [state, setState] = React.useState(initialState);
+  React.useEffect(effect, watch(state));
+  return render([state, setState]);
 };
 
 export default Hook;
