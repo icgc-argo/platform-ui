98c03fd72cc17bc9eb9625a14636b452509b6094
Minh <hlminh2000@gmail.com>
2019-09-19 09:34:02 -0400

    Revert "✨ enables loading css file"
    This reverts commit e2e07258aa8ffcede9bbc58d378b65a071d847d3.

--- next.config.js
+++ next.config.js
@@ -5,31 +5,8 @@ const urlJoin = require('url-join');
 const withImages = require('next-images');
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
-const withCSS = require('@zeit/next-css');
 
-const compose = (...funcs) => args => funcs.reduce((prev, func) => func(prev), args);
-
-/**
- * @todo remove this nonsense when possible.
- * refer to this: https://github.com/zeit/next-plugins/issues/392
- */
-function HACK_removeMinimizeOptionFromCssLoaders(config) {
-  console.warn('HACK: Removing `minimize` option from `css-loader` entries in Webpack config');
-  config.module.rules.forEach(rule => {
-    if (Array.isArray(rule.use)) {
-      rule.use.forEach(u => {
-        if (u.loader === 'css-loader' && u.options) {
-          delete u.options.minimize;
-        }
-      });
-    }
-  });
-}
-
-module.exports = compose(
-  withImages,
-  withCSS,
-)({
+module.exports = withImages({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
@@ -41,12 +18,15 @@ module.exports = compose(
       // This asn1 nonsense is to allow the jsonwebtokens dependency `parse-asn1` to get webpacked correctly. It has a dependency called `asn1.js` and a file with the same name that webpack gets confused.
       'asn1.js': urlJoin(__dirname, '/node_modules/asn1.js/lib/asn1.js'),
     };
-    config.module.rules.push({
-      test: /\.(graphql|gql)$/,
-      exclude: /node_modules/,
-      loader: 'graphql-tag/loader',
-    });
-    HACK_removeMinimizeOptionFromCssLoaders(config);
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: 'graphql-tag/loader',
+      },
+    ];
+
     return config;
   },
   publicRuntimeConfig: {
