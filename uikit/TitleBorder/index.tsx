ae9cd92d91b49096696b9c40583aa263495ebae3
Minh <hlminh2000@gmail.com>
2019-09-19 17:30:53 -0400

    🐛 makes TitleBorder width optional

--- uikit/TitleBorder/index.tsx
+++ uikit/TitleBorder/index.tsx
@@ -2,7 +2,7 @@ import { css, styled } from '..';
 import React from 'react';
 import defaultTheme from 'uikit/theme/defaultTheme';
 
-const TitleBorder = styled<'hr', { width: string; color: keyof typeof defaultTheme.colors }>('hr')`
+const TitleBorder = styled<'hr', { width?: string; color: keyof typeof defaultTheme.colors }>('hr')`
   border: 0;
   width: ${({ width }) => (width ? width : 'auto')};
   height: 3px;
