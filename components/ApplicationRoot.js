69051b08964cc1e9c38958efa1950f4bef9c040c
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-09-19 14:13:54 -0400

    replace apollo link with apollo upload link:

--- components/ApplicationRoot.js
+++ components/ApplicationRoot.js
@@ -1,6 +1,5 @@
 //@flow
 import ApolloClient from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
 import { ToasterContext, useToastState } from 'global/hooks/toaster';
 import { AuthProvider } from 'global/hooks/useAuthContext';
 import { PageContext } from 'global/hooks/usePageContext';
@@ -16,6 +15,7 @@ import urljoin from 'url-join';
 import Head from 'components/Head';
 import { ApolloProvider } from '@apollo/react-hooks';
 import get from 'lodash/get';
+import { createUploadLink } from 'apollo-upload-client';
 
 import type { ClientSideGetInitialPropsContext } from 'global/utils/pages/types';
 
@@ -121,7 +121,7 @@ export default function ApplicationRoot({
 
   const apolloClient = new ApolloClient({
     // $FlowFixMe apollo-client and apollo-link-http have a type conflict in their typing
-    link: createHttpLink({
+    link: createUploadLink({
       uri: urljoin(GATEWAY_API_ROOT, '/graphql'),
       fetch: fetch,
       headers: egoJwt
