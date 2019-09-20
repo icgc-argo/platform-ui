0af4c36b935848e028f6ebcbf450d75599e801e8
Minh <hlminh2000@gmail.com>
2019-09-20 13:45:40 -0400

    🐛 fixes EditUserModal story

--- components/pages/submission-system/modals/editUser/index.stories.tsx
+++ components/pages/submission-system/modals/editUser/index.stories.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import EditUserModal from '.';
@@ -6,7 +5,12 @@ import { action } from '@storybook/addon-actions';
 
 const EditUserModalStory = storiesOf(`${__dirname}`, module).add('Basic', () => (
   <EditUserModal
-    user={{ firstName: 'Joe', lastName: 'Blogs', email: 'jblogs@example.com', role: 'Full' }}
+    user={{
+      firstName: 'Joe',
+      lastName: 'Blogs',
+      email: 'jblogs@example.com',
+      role: 'COLLABORATOR',
+    }}
     dismissModal={action('dismiss modal')}
     onSubmit={data => action('submit')(data)}
   />
