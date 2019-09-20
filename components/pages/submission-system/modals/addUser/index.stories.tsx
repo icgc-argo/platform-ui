8d266418087e83ee12211420f21838d8a8cee199
Minh <hlminh2000@gmail.com>
2019-09-20 13:43:19 -0400

    🐛 fixes addUser modal story

--- components/pages/submission-system/modals/addUser/index.stories.tsx
+++ components/pages/submission-system/modals/addUser/index.stories.tsx
@@ -4,7 +4,10 @@ import AddUserModal from '.';
 import { action } from '@storybook/addon-actions';
 
 const AddUserModalStory = storiesOf(`${__dirname}`, module).add('Basic', () => (
-  <AddUserModal onSubmit={data => action('Submit data')(data)} />
+  <AddUserModal
+    onSubmit={data => action('Submit data')(data)}
+    dismissModal={action('dismissModal')}
+  />
 ));
 
 export default AddUserModalStory;
