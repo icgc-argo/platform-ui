cd17634fcccb7e5a5eb16cc0ff412abef863cc0c
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-09-20 12:18:32 -0400

    show error banner on invalid file upload

--- components/pages/submission-system/program-sample-registration/Instructions/index.stories.tsx
+++ components/pages/submission-system/program-sample-registration/Instructions/index.stories.tsx
@@ -8,11 +8,18 @@ const InstructionsStories = storiesOf(`${__dirname}`, module).add('Basic', () =>
   <div style={{ background: 'white' }}>
     <Instructions
       registrationEnabled={boolean('registrationEnabled', false)}
+<<<<<<< HEAD:components/pages/submission-system/program-sample-registration/Instructions/index.stories.tsx
       onUpload={action('upload')}
       setRegisterState={() => ''}
       shortName={'PROGRAM-SHORTNAME'}
       registrationId={'123456'}
     />
+=======
+      showError={action('show error')}
+    >
+      Skeleton
+    </Instructions>
+>>>>>>> show error banner on invalid file upload:components/pages/submission-system/program-sample-registration/Instructions/index.stories.js
   </div>
 ));
 
