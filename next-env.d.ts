be5a3a2ad481dda6cda3c09c08095312fd973974
Minh <hlminh2000@gmail.com>
2019-09-23 15:33:10 -0400

    🐛 fixes .md import type error

--- next-env.d.ts
+++ next-env.d.ts
@@ -5,6 +5,10 @@ declare module '*.svg' {
   const content: any;
   export default content;
 }
+declare module '*.md' {
+  const content: any;
+  export default content;
+}
 declare module '*.gql' {
   const content: string;
   export default content;
