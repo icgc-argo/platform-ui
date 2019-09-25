4bf48e89bbdbe382f0f0da481a030bc9edcd9418
Minh <hlminh2000@gmail.com>
2019-09-25 16:42:28 -0400

    🐛 fixes typescript error
    🐛 fixes type error
    
    🎨 names argument key instead of id

--- global/hooks/usePersistentContext.tsx
+++ global/hooks/usePersistentContext.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 // persist state between page navigations
 
-export const PersistentContext = React.createContext({});
+export const PersistentContext = React.createContext({
+  getItem: (key: string, defaultValue?: any) => null as any,
+  setItem: (key: string, value: any) => {},
+});
 
 export function usePersistentContext() {
   return React.useContext(PersistentContext);
 }
 
 // Just like React.useState, but the state returned by this function is persistent across page refreshes
-export default function usePersistState(key, defaultValue) {
+export default function usePersistState(key: string, defaultValue?: any) {
   const { getItem, setItem } = usePersistentContext();
   const [persistState, setPersistState] = React.useState(getItem(key, defaultValue));
 
