89ad259a19380732088b98faad93b65f042d9359
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-09-26 13:42:10 -0400

    cleanup

--- global/utils/createInMemoryCache.js
+++ global/utils/createInMemoryCache.js
@@ -13,8 +13,6 @@ export default () =>
       if (obj.__typename === 'Program') return `Program::${obj.shortName}`;
       // Treat valid resp and errors resp as the same object, even though only one returns ID
       if (obj.__typename === 'ClinicalRegistrationData') {
-        console.log('clnica', obj);
-
         return `ClinicalRegistrationData::${obj.shortName}`;
       } else return obj.id;
     },
