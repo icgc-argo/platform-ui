2448f3ec84c7c74f475086f81bbd568f0c3c6199
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-09-27 17:09:38 -0400

    use emotion as instead of component prop

--- uikit/NoData/index.tsx
+++ uikit/NoData/index.tsx
@@ -25,7 +25,7 @@ const NoData: React.ComponentType<NoDataProps> = ({ children, title, subtitle })
       `}
       color="grey"
       variant="navigation"
-      component="p"
+      as="p"
       bold
     >
       {title}
@@ -37,7 +37,7 @@ const NoData: React.ComponentType<NoDataProps> = ({ children, title, subtitle })
       `}
       color="grey"
       variant="data"
-      component="p"
+      as="p"
     >
       {subtitle}
     </Typography>
