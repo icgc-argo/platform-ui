1e8c7cb7ca16867cfec0fcd0b266839b8aee14e3
Xu Deng <d8660091@gmail.com>
2019-09-27 11:08:59 -0400

    Do not render nodata component when loading

--- uikit/Table/NoDataComponent.tsx
+++ uikit/Table/NoDataComponent.tsx
@@ -4,7 +4,7 @@ import css from '@emotion/css';
 import Typography from '../Typography';
 import useTheme from '../utils/useTheme';
 
-export default function NoDataComponent() {
+export default function NoDataComponent(props) {
   const theme = useTheme();
   return (
     <div
@@ -16,17 +16,21 @@ export default function NoDataComponent() {
         padding: 32px 0;
       `}
     >
-      <img alt="no data found" src={noDataSvg} />
-      <Typography
-        variant="navigation"
-        css={css`
-          font-weight: 600;
-          color: ${theme.colors.grey};
-          margin: 16px 0;
-        `}
-      >
-        No Data Found
-      </Typography>
+      {props.loading ? null : (
+        <>
+          <img alt="no data found" src={noDataSvg} />
+          <Typography
+            variant="navigation"
+            css={css`
+              font-weight: 600;
+              color: ${theme.colors.grey};
+              margin: 16px 0;
+            `}
+          >
+            No Data Found
+          </Typography>
+        </>
+      )}
     </div>
   );
 }
