27cadd1877d2c2d0423d2a7d24a45c93a39f6554
Xu Deng <d8660091@gmail.com>
2019-10-07 14:59:58 -0400

    Revert count fix

--- components/pages/submission-system/program-sample-registration/index.tsx
+++ components/pages/submission-system/program-sample-registration/index.tsx
@@ -148,8 +148,8 @@ export default function ProgramIDRegistration() {
     } = clinicalRegistration;
     submissionInfo = { createdAt, creator, fileName };
     stats = {
-      newCount: newDonors.count + newSamples.count + newSpecimens.count,
-      existingCount: alreadyRegistered.count,
+      newCount: alreadyRegistered.count,
+      existingCount: newDonors.count + newSamples.count + newSpecimens.count,
     };
   }
 
