f92f76f0c12e2125a329c952a4c45c008b0d1e76
Minh <hlminh2000@gmail.com>
2019-10-08 09:25:01 -0400

    ✨ implements the showClinicalType flag

--- components/pages/submission-system/program-clinical-submission/ErrorNotification.tsx
+++ components/pages/submission-system/program-clinical-submission/ErrorNotification.tsx
@@ -5,6 +5,12 @@ import Notification from 'uikit/notifications/Notification';
 import Table from 'uikit/Table';
 import { ClinicalSubmissionError } from './types';
 
+const insertAt = <T extends any>(arr: T[]) => (i: number) => (element: T) => [
+  ...arr.slice(0, i),
+  element,
+  ...arr.slice(i, arr.length),
+];
+
 export default ({
   title,
   errors,
@@ -14,65 +20,79 @@ export default ({
 }: {
   title: string;
   subtitle: string;
-  errors: ClinicalSubmissionError[];
-  showClinicalType?: boolean;
+  errors: Array<
+    ClinicalSubmissionError & {
+      clinicalType?: string;
+    }
+  >;
   onClearClick: React.ComponentProps<typeof Button>['onClick'];
-}) => (
-  <Notification
-    variant="ERROR"
-    interactionType="NONE"
-    title={
-      <div
-        css={css`
-          display: flex;
-          justify-content: space-between;
-        `}
-      >
-        {title}
+  showClinicalType?: boolean;
+}) => {
+  const defaultColumns: {
+    accessor: keyof typeof errors[0];
+    Header: React.ReactNode;
+  }[] = [
+    {
+      accessor: 'row',
+      Header: 'Row #',
+    },
+    {
+      accessor: 'donorId',
+      Header: 'Submitter Donor ID',
+    },
+    {
+      accessor: 'field',
+      Header: 'Field with Error',
+    },
+    {
+      accessor: 'value',
+      Header: 'Error Value',
+    },
+    {
+      accessor: 'message',
+      Header: 'Error Dscription',
+    },
+  ];
+  const columnsWithClinicalType = insertAt(defaultColumns)(1)({
+    accessor: 'clinicalType',
+    Header: 'Clinical type',
+  });
+  return (
+    <Notification
+      variant="ERROR"
+      interactionType="NONE"
+      title={
         <div
           css={css`
             display: flex;
+            justify-content: space-between;
           `}
         >
-          <Button variant="secondary" size="sm">
-            Error Report
-          </Button>
-          <Button variant="text" size="sm" onClick={onClearClick}>
-            Clear
-          </Button>
+          {title}
+          <div
+            css={css`
+              display: flex;
+            `}
+          >
+            <Button variant="secondary" size="sm">
+              Error Report
+            </Button>
+            <Button variant="text" size="sm" onClick={onClearClick}>
+              Clear
+            </Button>
+          </div>
         </div>
-      </div>
-    }
-    content={
-      <>
-        <div>{subtitle}</div>
-        <Table
-          showPagination={false}
-          columns={[
-            {
-              accessor: 'row',
-              Header: 'Row #',
-            },
-            {
-              accessor: 'donorId',
-              Header: 'Submitter Donor ID',
-            },
-            {
-              accessor: 'field',
-              Header: 'Field with Error',
-            },
-            {
-              accessor: 'value',
-              Header: 'Error Value',
-            },
-            {
-              accessor: 'message',
-              Header: 'Error Dscription',
-            },
-          ]}
-          data={errors}
-        />
-      </>
-    }
-  />
-);
+      }
+      content={
+        <>
+          <div>{subtitle}</div>
+          <Table
+            showPagination={false}
+            columns={showClinicalType ? columnsWithClinicalType : defaultColumns}
+            data={errors}
+          />
+        </>
+      }
+    />
+  );
+};
