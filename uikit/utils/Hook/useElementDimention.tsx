84a8e6156199413f3497150b52a6d23971adf94d
Minh <hlminh2000@gmail.com>
2019-10-31 10:34:54 -0400

    ♻️ merges the height and width hooks into one
    🎨 order code

--- uikit/utils/Hook/useElementDimention.tsx
+++ uikit/utils/Hook/useElementDimention.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import debounce from 'lodash/debounce';
 import getElementResizeListener from 'uikit/utils/getElementResizeListener';
 
-const useElementWidth = (
+export default (
   parentRef: React.RefObject<HTMLElement>,
   _config: { resizeDebounce?: number } = {},
 ) => {
@@ -11,9 +11,11 @@ const useElementWidth = (
     ..._config,
   };
   const [width, setWidthState] = React.useState(null);
+  const [height, setHeightState] = React.useState(null);
   const [resizing, setResizing] = React.useState(false);
   React.useEffect(() => {
     if (!!parentRef.current) setWidthState(parentRef.current.clientWidth);
+    if (!!parentRef.current) setHeightState(parentRef.current.clientHeight);
   }, [!!parentRef.current ? parentRef.current.clientWidth : 0]);
   React.useEffect(() => {
     const currentParent = parentRef.current;
@@ -22,43 +24,15 @@ const useElementWidth = (
         setWidthState(width);
         setResizing(false);
       }, config.resizeDebounce);
-      const onResize = () => {
-        if (currentParent.clientWidth !== width) {
-          setResizing(true);
-          setWidth(currentParent.clientWidth);
-        }
-      };
-      const resizeListener = getElementResizeListener();
-      if (currentParent) resizeListener.listenTo(currentParent, onResize);
-      return () => {
-        if (currentParent) resizeListener.removeListener(currentParent, onResize);
-      };
-    }
-  }, [parentRef.current]);
-  return { width, resizing };
-};
-
-const useElementHeight = (
-  parentRef: React.RefObject<HTMLElement>,
-  _config: { resizeDebounce?: number } = {},
-) => {
-  const config: typeof _config = {
-    resizeDebounce: 200,
-    ..._config,
-  };
-  const [height, setHeightState] = React.useState(null);
-  const [resizing, setResizing] = React.useState(false);
-  React.useEffect(() => {
-    if (!!parentRef.current) setHeightState(parentRef.current.clientHeight);
-  }, [!!parentRef.current ? parentRef.current.clientHeight : 0]);
-  React.useEffect(() => {
-    const currentParent = parentRef.current;
-    if (currentParent) {
       const setHeight = debounce((height: number) => {
         setHeightState(height);
         setResizing(false);
       }, config.resizeDebounce);
       const onResize = () => {
+        if (currentParent.clientWidth !== width) {
+          setResizing(true);
+          setWidth(currentParent.clientWidth);
+        }
         if (currentParent.clientHeight !== height) {
           setResizing(true);
           setHeight(currentParent.clientHeight);
@@ -71,18 +45,5 @@ const useElementHeight = (
       };
     }
   }, [parentRef.current]);
-  return { height, resizing };
-};
-
-export default (
-  parentRef: React.RefObject<HTMLElement>,
-  _config: { resizeDebounce?: number } = {},
-) => {
-  const { width, resizing: resizingWidth } = useElementWidth(parentRef, _config);
-  const { height, resizing: resizingHeight } = useElementHeight(parentRef, _config);
-  return {
-    width,
-    height,
-    resizing: resizingWidth || resizingHeight,
-  };
+  return { width, height, resizing };
 };
