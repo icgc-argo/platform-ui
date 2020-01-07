e0b9865e7eb076d4cbf914b253002a788bb5c367
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-01-07 01:19:41 -0500

    adjust 3 row layout and update pages using it

--- components/pages/DefaultLayout.tsx
+++ components/pages/DefaultLayout.tsx
@@ -1,13 +1,18 @@
 import NavBar from 'components/NavBar';
 import Footer from '../Footer';
 import { PageContainer } from 'uikit/PageLayout';
+import styled from '@emotion/styled';
+
+const ThreeRowPageContainer = styled(PageContainer)`
+  grid-template-rows: 58px 1fr 59px; /* header + content + footer + 1px border */
+`;
 
 export default function DefaultLayout({ children }) {
   return (
-    <PageContainer>
+    <ThreeRowPageContainer>
       <NavBar />
-      {children}
+      <div>{children}</div>
       <Footer />
-    </PageContainer>
+    </ThreeRowPageContainer>
   );
 }
