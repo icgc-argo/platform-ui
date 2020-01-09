9c721b6214d5dbf76afd933b6aae1480791b12a2
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2020-01-09 11:06:25 -0500

    Bug/#888 footer fixes (#975)
    * 💄 change col width
    
    change col width
    
    * adjust 3 row layout and update pages using it
    
    * change col width on footer

--- components/pages/user/index.tsx
+++ components/pages/user/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import DefaultLayout from '../DefaultLayout';
-import { ContentHeader, PageContent, ContentBody, PageBody } from 'uikit/PageLayout';
+import { ContentHeader, ContentBody } from 'uikit/PageLayout';
 import Typography from 'uikit/Typography';
 import { Row, Col } from 'react-grid-system';
 import { css } from 'uikit';
@@ -28,34 +28,30 @@ export function UserPage({ firstName, lastName }: { firstName: string; lastName:
 
   return (
     <DefaultLayout>
-      <PageBody className="noSidebar">
-        <PageContent>
-          <ContentHeader>
-            <Typography variant="title" color="primary" as="h1">
-              Profile & Tokens
-            </Typography>
-          </ContentHeader>
-          <ContentBody
-            css={css`
-              padding: 10px;
-            `}
-          >
-            <Row nogutter>
-              <Column>
-                <ProfileBox />
-              </Column>
-            </Row>
-            <Row nogutter>
-              <Column sm={12} md={6}>
-                <AccessKeyBox accessKey={apiKey} loading={loading} />
-              </Column>
-              <Column sm={12} md={6}>
-                <ProgramAccessBox isDacoApproved={isDacoApproved} loading={loading} />
-              </Column>
-            </Row>
-          </ContentBody>
-        </PageContent>
-      </PageBody>
+      <ContentHeader>
+        <Typography variant="title" color="primary" as="h1">
+          Profile & Tokens
+        </Typography>
+      </ContentHeader>
+      <ContentBody
+        css={css`
+          padding: 10px;
+        `}
+      >
+        <Row nogutter>
+          <Column>
+            <ProfileBox />
+          </Column>
+        </Row>
+        <Row nogutter>
+          <Column sm={12} md={6}>
+            <AccessKeyBox accessKey={apiKey} loading={loading} />
+          </Column>
+          <Column sm={12} md={6}>
+            <ProgramAccessBox isDacoApproved={isDacoApproved} loading={loading} />
+          </Column>
+        </Row>
+      </ContentBody>
     </DefaultLayout>
   );
 }
