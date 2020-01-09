8247e13a77a49283d206cc9f648b6182947254d7
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-01-09 11:05:49 -0500

    💄 chevron variations
    add chevron left and chevron up

---
+++ uikit/Icon/icons/collection/chevron_up.tsx
@@ -0,0 +1,12 @@
+import { css } from '@emotion/core';
+
+export default {
+  title: 'Chevron Up',
+  viewBox: '0 0 12 12',
+  path:
+    'M9.952 3.342a1.224 1.224 0 0 1 1.697 0 1.147 1.147 0 0 1 0 1.65l-4.8 4.666a1.224 1.224 0 0 1-1.697 0l-4.8-4.666a1.143 1.143 0 0 1 0-1.65 1.224 1.224 0 0 1 1.696 0L6 7.184l3.952-3.842z',
+  css: css`
+    height: 12px;
+    transform: rotate(180deg);
+  `,
+};
