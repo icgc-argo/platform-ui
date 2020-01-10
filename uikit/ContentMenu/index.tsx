c68ae04a2a287625a64ca1995e4ab4c2bef84439
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-01-10 13:55:08 -0500

    ✨ uikit add contentmenu
    add content menu component to uikit

---
+++ uikit/ContentMenu/index.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import Typography from 'uikit/Typography';
+import styled from '@emotion/styled';
+import useTheme from '../utils/useTheme';
+import { css } from '@emotion/core';
+
+const Cont = styled('div')`
+  margin-top: 14px;
+  border-left: 2px solid ${({ color, theme }) => (color ? color : theme.colors.secondary)};
+`;
+
+const MenuItem = ({ name, onClick, active }) => {
+  const theme = useTheme();
+  return (
+    <div
+      css={css`
+        padding: 8px 0 8px 16px;
+        background-color: ${active && theme.colors.secondary_4};
+
+        &:hover {
+          cursor: pointer;
+        }
+      `}
+      onClick={onClick}
+    >
+      <Typography variant="data" color={active ? 'primary' : 'secondary_dark'}>
+        {name}
+      </Typography>
+    </div>
+  );
+};
+
+const Menu = ({
+  title,
+  contents,
+  color,
+  scrollYOffset = 0,
+}: {
+  title: string;
+  contents: Array<{ name: string; contentRef?: React.RefObject<HTMLElement> }>;
+  color?: string;
+  // use case: fixed header on page, need extra offset to scroll to top of content
+  scrollYOffset?: number;
+}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <div>
+      <Typography variant="sectionHeader" color="primary">
+        {title}
+      </Typography>
+      <Cont color={color}>
+        {contents.map(({ name, contentRef }, index) => (
+          <MenuItem
+            key={index}
+            name={name}
+            active={activeIndex === index}
+            onClick={() => {
+              setActiveIndex(index);
+              if (contentRef) window.scrollTo(0, contentRef.current.offsetTop - scrollYOffset);
+            }}
+          />
+        ))}
+      </Cont>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ uikit/ContentMenu/stories.tsx
@@ -0,0 +1,29 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { text, color } from '@storybook/addon-knobs';
+import ContentMenu from '.';
+
+const ContentMenuStories = storiesOf(`${__dirname}`, module).add('Basic', () => {
+  const contentNames = ['Sample Registration', 'Specimen', 'Donor', 'Treatment'];
+  const contents = contentNames.map(content => ({ name: content, contentRef: React.createRef() }));
+
+  return (
+    <div style={{ display: 'grid' }}>
+      <p>// NB: Jump to content only works on window level</p>
+      <ContentMenu
+        title={text('Title', 'Clinical Files')}
+        contents={contents}
+        color={color('Border', '#0774d3')}
+      />
+      <div style={{ marginTop: '20px', border: '1px solid blue' }}>
+        {contents.map(({ name, contentRef }, i) => (
+          <div ref={contentRef} key={i} style={{ height: '40px' }}>
+            {name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+});
+
+export default ContentMenuStories;
