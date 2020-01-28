0dae15fb256d78b05656ad39d6f732284c706975
Priyonto Saha <psaha@ml3839-psaha.ad.oicr.on.ca>
2020-01-28 15:21:34 -0500

    ✅ Adding ids for Selenium

--- components/pages/submission-system/program-clinical-submission/PageContent.tsx
+++ components/pages/submission-system/program-clinical-submission/PageContent.tsx
@@ -406,6 +406,7 @@ export default () => {
       ))}
       {hasDataError && (
         <div
+          id="error-submission-sign-off"
           css={css`
             margin-top: 20px;
           `}
