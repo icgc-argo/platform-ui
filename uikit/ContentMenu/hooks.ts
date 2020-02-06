055c4fc6caa20e196d11575c9e6ffdcc308ee9b7
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-02-06 02:20:05 -0500

    change hook to ts file

---
+++ uikit/ContentMenu/hooks.ts
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+
+export const useMenuHighlight = (
+  sectionClassName: string,
+  anchorClassName: string,
+  topOffset: number,
+): void => {
+  const [lastActiveLink, setLastActiveLink] = useState(undefined);
+  const sections = document.getElementsByClassName(sectionClassName);
+  const menuLinks = document.getElementsByClassName(anchorClassName);
+
+  useEffect(() => {
+    const findActiveSection = () => {
+      if (!sections.length) return null;
+
+      for (let i = 0; i < sections.length; i++) {
+        const currentSection = sections[i];
+        const { top } = currentSection.getBoundingClientRect();
+        const offset = topOffset;
+
+        if (top >= 0 && top <= offset) {
+          return currentSection;
+        }
+      }
+    };
+
+    const highlightMenu = () => {
+      const activeSection = findActiveSection();
+      // highlight menu
+      if (activeSection) {
+        for (let i = 0; i < menuLinks.length; i++) {
+          const menuLink = menuLinks[i];
+
+          if (menuLink.id === activeSection.id) {
+            // remove last active style
+            if (lastActiveLink) {
+              lastActiveLink.classList.remove('active');
+            }
+            menuLink.classList.add('active');
+
+            setLastActiveLink(menuLink);
+          }
+        }
+      }
+    };
+
+    document.addEventListener('scroll', highlightMenu);
+    document.addEventListener('resize', highlightMenu);
+
+    highlightMenu();
+
+    return () => {
+      document.removeEventListener('scroll', highlightMenu);
+      document.removeEventListener('resize', highlightMenu);
+    };
+  });
+};
