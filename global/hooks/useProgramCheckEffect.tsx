725809d6ab1648c47efe4c9f9c07c67ef33c1972
Wajiha Shah <wshah@oicr.on.ca>
2020-02-20 10:06:38 -0500

    add sleep() to loading modal to prevent flashes

--- global/hooks/useProgramCheckEffect.tsx
+++ global/hooks/useProgramCheckEffect.tsx
@@ -1,15 +1,14 @@
 import { usePageQuery } from 'global/hooks/usePageContext';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { ERROR_STATUS_KEY } from 'pages/_error';
 import PROGRAM_SHORTNAME from './PROGRAM_SHORTNAME.gql';
 import { useGlobalLoadingState } from 'components/ApplicationRoot';
+import { sleep } from 'global/utils/common';
 
 export const useProgramCheckEffect = () => {
   const { shortName } = usePageQuery<{ shortName: string }>();
-  const [programNotExist, setProgramNotExist] = useState(false);
-
-  const { loading, data: { program = undefined } = {} } = useQuery<{
+  const { loading: loadingQuery, data: { program = undefined } = {} } = useQuery<{
     program: { name: string; shortName: string };
   }>(PROGRAM_SHORTNAME, {
     variables: {
@@ -17,16 +16,26 @@ export const useProgramCheckEffect = () => {
     },
   });
 
-  const { setLoading } = useGlobalLoadingState();
-
+  const { setLoading, isLoading } = useGlobalLoadingState();
   useEffect(() => {
-    setLoading(loading);
-    setProgramNotExist(loading ? false : !program);
-  }, [program, loading]);
+    // Generate a new loading modal ONLY if the program's existence is unknown
+    if (!program && !isLoading) {
+      setLoading(true);
+    }
+    // Check if the query to check for program existence has finished
+    if (!loadingQuery) {
+      // Now that loading is finished, the program must not exist. Break and send to 404.
+      if (!program) {
+        const err = new Error('Program not found') as Error & { statusCode?: number };
+        err[ERROR_STATUS_KEY] = 404;
+        throw err;
+      }
 
-  if (programNotExist) {
-    const err = new Error('Program not found') as Error & { statusCode?: number };
-    err[ERROR_STATUS_KEY] = 404;
-    throw err;
-  }
+      // Otherwise, the query result is back and the program does exist.
+      // Wait for a small buffer time so the loading modal animation has enough time.
+      else if (isLoading) {
+        sleep(1200).then(() => setLoading(false));
+      }
+    }
+  }, [program, loadingQuery]);
 };
