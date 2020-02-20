aa2fe7cbed9a8a5f406a2f6d67fe7690aec71056
wajiha-oicr <59613607+wajiha-oicr@users.noreply.github.com>
2020-02-20 10:44:58 -0500

    Add sleep() to loading modal to prevent flashes (#1101)
    * add sleep() to loading modal to prevent flashes

--- global/hooks/useProgramCheckEffect.tsx
+++ global/hooks/useProgramCheckEffect.tsx
@@ -1,15 +1,14 @@
 import { usePageQuery } from 'global/hooks/usePageContext';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { ERROR_STATUS_KEY } from 'pages/_error';
 import PROGRAM_SHORTNAME from './PROGRAM_SHORTNAME.gql';
 import { useGlobalLoadingState } from 'components/ApplicationRoot';
+import { sleep } from 'global/utils/common';
 
 export const useProgramCheckEffect = () => {
   const { shortName } = usePageQuery<{ shortName: string }>();
-  const [programNotExist, setProgramNotExist] = useState(false);
-
-  const { loading, data: { program = undefined } = {} } = useQuery<{
+  const { loading: loadingQuery, data: { program = undefined } = {} } = useQuery<{
     program: { name: string; shortName: string };
   }>(PROGRAM_SHORTNAME, {
     variables: {
@@ -17,16 +16,19 @@ export const useProgramCheckEffect = () => {
     },
   });
 
-  const { setLoading } = useGlobalLoadingState();
-
+  const { setLoading: setLoaderShown, isLoading: isLoaderShown } = useGlobalLoadingState();
   useEffect(() => {
-    setLoading(loading);
-    setProgramNotExist(loading ? false : !program);
-  }, [program, loading]);
-
-  if (programNotExist) {
-    const err = new Error('Program not found') as Error & { statusCode?: number };
-    err[ERROR_STATUS_KEY] = 404;
-    throw err;
-  }
+    if (!program && !isLoaderShown) {
+      setLoaderShown(true);
+    }
+    if (!loadingQuery) {
+      if (!program) {
+        const err = new Error('Program not found') as Error & { statusCode?: number };
+        err[ERROR_STATUS_KEY] = 404;
+        throw err;
+      } else if (isLoaderShown) {
+        sleep(1200).then(() => setLoaderShown(false));
+      }
+    }
+  }, [program, loadingQuery]);
 };
