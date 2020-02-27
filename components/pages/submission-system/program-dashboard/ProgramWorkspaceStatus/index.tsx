5b172f18258a4881120a14b828db0cf82c420bc8
Wajiha Shah <wshah@oicr.on.ca>
2020-02-27 15:18:35 -0500

    made wrapper function for links

--- components/pages/submission-system/program-dashboard/ProgramWorkspaceStatus/index.tsx
+++ components/pages/submission-system/program-dashboard/ProgramWorkspaceStatus/index.tsx
@@ -13,84 +13,115 @@ import {
   PROGRAM_CLINICAL_SUBMISSION_PATH,
 } from 'global/constants/pages';
 import { DashboardCard } from '../common';
+import { isCollaborator } from 'global/utils/egoJwt';
+import { ReactNode } from 'react';
+import useAuthContext from 'global/hooks/useAuthContext';
+
+type accessStatus = {
+  jwt: string;
+  programId: string;
+};
+const LinkWrapper: React.ComponentType<{
+  access: accessStatus;
+  wrapper: Function;
+  children: ReactNode;
+}> = ({ wrapper, access, children }) => {
+  const canViewLinks = !isCollaborator({ egoJwt: access.jwt, programId: access.programId });
+  return canViewLinks ? wrapper(children) : children;
+};
 
 export default function ProgramWorkplaceStatus() {
   const { shortName: programShortName } = usePageQuery<{ shortName: string }>();
+  const { token } = useAuthContext();
+  const currentUserStatus: accessStatus = {
+    jwt: token,
+    programId: programShortName,
+  };
   return (
     <DashboardCard cardHeight="170px">
-     <Typography variant="default" component="span">
-          Program Workplace Status
-        </Typography>
+      <Typography variant="default" component="span">
+        Program Workplace Status
+      </Typography>
 
+      <div
+        css={css`
+          height: 50px;
+          display: flex;
+          flex-direction: row;
+          padding-top: 10px;
+        `}
+      >
         <div
           css={css`
+            margin-top: 7px;
+            width: 175px;
             height: 50px;
-            display: flex;
-            flex-direction: row;
-            padding-top: 10px;
           `}
         >
-          <div
-            css={css`
-              margin-top: 7px;
-              width: 175px;
-              height: 50px;
-            `}
+          <LinkWrapper
+            access={currentUserStatus}
+            wrapper={(children: ReactNode) => (
+              <Link
+                href={PROGRAM_SAMPLE_REGISTRATION_PATH}
+                as={PROGRAM_SAMPLE_REGISTRATION_PATH.replace(
+                  PROGRAM_SHORT_NAME_PATH,
+                  programShortName as string,
+                )}
+              >
+                <Hyperlink>{children}</Hyperlink>
+              </Link>
+            )}
           >
-            <Link
-              href={PROGRAM_SAMPLE_REGISTRATION_PATH}
-              as={PROGRAM_SAMPLE_REGISTRATION_PATH.replace(
-                PROGRAM_SHORT_NAME_PATH,
-                programShortName as string,
-              )}
-            >
-              <Hyperlink>
-                <Typography variant="label">Sample Registration</Typography>
-              </Hyperlink>
-            </Link>
-          </div>
-          <div
-            css={css`
-              height: 50px;
-            `}
-          >
-            <SampleRegistrationProgressBar programShortName={programShortName} />
-          </div>
+            <Typography variant="label">Sample Registration</Typography>
+          </LinkWrapper>
         </div>
         <div
           css={css`
             height: 50px;
-            display: flex;
-            flex-direction: row;
           `}
         >
-          <div
-            css={css`
-              margin-top: 7px;
-              width: 175px;
-              height: 50px;
-            `}
-          >
-            <Link
-              href={PROGRAM_CLINICAL_SUBMISSION_PATH}
-              as={PROGRAM_CLINICAL_SUBMISSION_PATH.replace(
-                PROGRAM_SHORT_NAME_PATH,
-                programShortName as string,
-              )}
-            >
-              <Hyperlink>
-                <Typography variant="label">Clinical Submission</Typography>
-              </Hyperlink>
-            </Link>
-          </div>
-          <div
-            css={css`
-              height: 50px;
-            `}
-          >
-            <ClinicalSubmissionProgressBar programShortName={programShortName} />
-          </div>
+          <SampleRegistrationProgressBar programShortName={programShortName} />
         </div>
+      </div>
+      <div
+        css={css`
+          height: 50px;
+          display: flex;
+          flex-direction: row;
+        `}
+      >
+        <div
+          css={css`
+            margin-top: 7px;
+            width: 175px;
+            height: 50px;
+          `}
+        >
+          <LinkWrapper
+            access={currentUserStatus}
+            wrapper={(children: ReactNode) => (
+              <Link
+                href={PROGRAM_CLINICAL_SUBMISSION_PATH}
+                as={PROGRAM_CLINICAL_SUBMISSION_PATH.replace(
+                  PROGRAM_SHORT_NAME_PATH,
+                  programShortName as string,
+                )}
+              >
+                <Hyperlink>{children}</Hyperlink>
+              </Link>
+            )}
+          >
+            <Typography variant="label">Clinical Submission</Typography>
+          </LinkWrapper>
+        </div>
+        <div
+          css={css`
+            height: 50px;
+          `}
+        >
+          <ClinicalSubmissionProgressBar programShortName={programShortName} />
+        </div>
+      </div>
     </DashboardCard>
   );
 }
