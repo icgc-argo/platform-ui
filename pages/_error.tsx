7d234e97676b10f41269705c2e126d406a67f646
Ann Catton <ann.catton@oicr.on.ca>
2020-03-18 10:30:24 -0400

    Remove 401 page logic

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import get from 'lodash/get';
 import Page404 from 'components/pages/404';
-import Page401 from 'components/pages/401';
 import Page500 from 'components/pages/500';
 import Page403 from 'components/pages/403';
 import ClientError from 'components/pages/ClientError';
@@ -10,11 +9,6 @@ export const ERROR_STATUS_KEY = 'statusCode';
 
 class Error extends React.Component {
   static getInitialProps({ res, err }) {
-    if (get(err, ERROR_STATUS_KEY) === 401) {
-      if (res) {
-        res[ERROR_STATUS_KEY] = 401;
-      }
-    }
     if (get(err, ERROR_STATUS_KEY) === 403) {
       if (res) {
         res[ERROR_STATUS_KEY] = 403;
@@ -32,8 +26,6 @@ class Error extends React.Component {
         return <Page404 />;
       case 403:
         return <Page403 />;
-      case 401:
-        return <Page401 />;
       case 500:
         return <Page500 />;
       default:
