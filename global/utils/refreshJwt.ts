4949203df1a09d062a64dd630d470ba2d67b7316
Ann Catton <ann.catton@oicr.on.ca>
2020-04-06 18:26:41 -0400

    🐛 Create queue for refresh request to prevent parallel refreshes

--- global/utils/refreshJwt.ts
+++ global/utils/refreshJwt.ts
@@ -1,17 +1,34 @@
 import urlJoin from 'url-join';
 import { getConfig } from 'global/config';
+import Cookies from 'js-cookie';
+import { EGO_JWT_KEY } from 'global/constants';
+import { isValidJwt } from './egoJwt';
+import Queue from 'promise-queue';
 
 const { EGO_API_ROOT, EGO_CLIENT_ID } = getConfig();
 
-// pass client_id to get ego api to set correct response headers
-const refreshUrl = urlJoin(EGO_API_ROOT, `/api/oauth/refresh?client_id=${EGO_CLIENT_ID}`);
+var maxConcurrent = 1;
+var maxQueue = Infinity;
+var queue = new Queue(maxConcurrent, maxQueue);
 
-export default egoJwt =>
-  fetch(refreshUrl, {
-    credentials: 'include',
-    headers: {
-      accept: '*/*',
-      authorization: egoJwt || '',
-    },
-    method: 'POST',
-  }).then(res => res.text());
+// pass client_id to get ego api to set correct response headers
+const refreshUrl = urlJoin(EGO_API_ROOT, `/oauth/refresh?client_id=${EGO_CLIENT_ID}`);
+
+export default () =>
+  queue.add(() => {
+    return fetch(refreshUrl, {
+      credentials: 'include',
+      headers: {
+        accept: '*/*',
+        authorization: Cookies.get(EGO_JWT_KEY) || '',
+      },
+      method: 'POST',
+    })
+      .then(res => res.text())
+      .then(newJwt => {
+        if (isValidJwt(newJwt)) {
+          Cookies.set(EGO_JWT_KEY, newJwt);
+        }
+        return newJwt;
+      });
+  });
