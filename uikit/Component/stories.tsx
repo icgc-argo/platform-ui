9cfe5f2a2c850274c1c644cbb2829020f4e6df2b
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-04-14 14:32:49 -0400

    add story

---
+++ uikit/Component/stories.tsx
@@ -0,0 +1,9 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import Component from '.';
+
+const ComponentStories = storiesOf(`${__dirname}`, module).add('Basic', () => (
+  <Component>Skeleton</Component>
+));
+
+export default ComponentStories;
