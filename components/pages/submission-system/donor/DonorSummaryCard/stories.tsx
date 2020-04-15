550a714883b68c059347d581235c46b6994ea99d
Priyonto Saha <psaha@ml3839-psaha.local>
2020-04-15 11:28:18 -0400

    🚚 Renaming for clarity

--- components/pages/submission-system/donor/DonorSummaryCard/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-import DonorSummaryCard from '.';
-
-const DonorSummaryCardStories = storiesOf(`${__dirname}`, module).add('Basic', () => {
-  return <DonorSummaryCard />;
-});
-
-export default DonorSummaryCardStories;
