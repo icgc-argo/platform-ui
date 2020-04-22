9dd0af5c4f954e3af6691c2b2623e2e8f7823176
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-04-22 08:57:31 -0400

    add entity count

--- uikit/Component/index.tsx
+++ uikit/Component/index.tsx
@@ -9,35 +9,28 @@ import { withTheme } from 'emotion-theming';
 import random from 'lodash/random';
 import colors from '../theme/defaultTheme/colors';
 
-export enum Entities {
-  PRIMARY_DIAGNOSIS = 'Primary Diagnosis',
-  SPECIMEN = 'Speciment',
-  TREATMENT = 'Treatment',
-  FOLLOW_UP = 'Follow Up',
-}
-
-const TIMELINE_TYPES = {
-  PRIMARY_DIAGNOSIS: {
+const TIMELINE_TYPES = Object.freeze({
+  primary_diagnosis: {
     title: 'Primary Diagnosis',
     borderColor: colors.secondary_1,
     backgroundColor: colors.secondary_4,
   },
-  SPECIMEN: {
+  specimen: {
     title: 'Specimen',
     borderColor: colors.accent3,
     backgroundColor: colors.accent3_4,
   },
-  TREATMENT: {
+  treatment: {
     title: 'Treatment',
     borderColor: colors.accent4_1,
     backgroundColor: colors.accent4_4,
   },
-  FOLLOW_UP: {
+  follow_up: {
     title: 'Follow Up',
     borderColor: colors.accent2_1,
     backgroundColor: colors.accent2_4,
   },
-};
+});
 
 const mock = [
   {
@@ -55,145 +48,83 @@ const mock = [
   { type: 'follow_up', id: 'FOLLOW UP FO2123', description: 'Relapse' },
 ];
 
-const DayCount = ({ days }) => (
-  <Typography
-    variant="data"
-    css={css`
-      margin-right: 12px;
-      text-align: right;
-    `}
-  >
-    {days}
-  </Typography>
-);
-
-const TimelineItem = withTheme(({ id, description, index, type, theme }) => {
-  const { backgroundColor, borderColor } = TIMELINE_TYPES[type];
+const Header = ({ entityCounts }) => {
+  const theme = useTheme();
   return (
     <div
       css={css`
         display: flex;
-        align-items: center;
-        margin: 0 -1px;
-        border: 1px solid ${theme.colors.grey_1};
-        border-top: none;
-        border-bottom: none;
-        ${index === 2
-          ? css`
-              border: 1px solid ${borderColor};
-            `
-          : null};
+        justify-content: space-between;
+        border-bottom: 1px solid ${theme.colors.grey_2};
+        padding-bottom: 9px;
+        margin-bottom: 15px;
       `}
     >
-      <VerticalTabs.Item
-        tabStyle={{ border: borderColor, background: backgroundColor }} // border and background, then we can match it, no logic so dont pass Triangle component
+      <Typography variant="default">Clinical Timeline</Typography>
+      <div
         css={css`
-          width: 100%;
-          border: 0;
-          color: black;
+          display: flex;
+
+          > div {
+            margin-left: 18px;
+          }
+
+          div:first-child {
+            margin-left: 6px;
+          }
+
+          ${css(theme.typography.data as any)};
         `}
-        active={index === 2}
       >
-        <div>
-          <Typography variant="caption" as="div">
-            {id}
-          </Typography>
-          <Typography variant="data" as="div" bold>
-            {description}
-          </Typography>
-        </div>
-      </VerticalTabs.Item>
-    </div>
-  );
-});
+        Show:
+        {Object.keys(entityCounts).map(entityKey => {
+          const { title, color } = TIMELINE_TYPES[entityKey];
+          const count = entityCounts[entityKey];
 
-const TimelineTabs = styled(VerticalTabs)``;
-
-const Timeline = withTheme(({ entities, theme }) => (
-  <div
-    css={css`
-      flex: 1;
-      border: 1px solid ${theme.colors.grey_1};
-    `}
-  >
-    {entities.map(({ id, description, type }, i) => (
-      <>
-        <div className="day">{random(99, 123)}</div>
-        <TimelineItem type={type} id={id} description={description} index={i} />
-      </>
-    ))}
-  </div>
-));
-
-const Header = withTheme(({ entities, theme }) => (
-  <div
-    css={css`
-      display: flex;
-      justify-content: space-between;
-      border-bottom: 1px solid ${theme.colors.grey_2};
-      padding-bottom: 9px;
-      margin-bottom: 15px;
-    `}
-  >
-    <Typography variant="default">Clinical Timeline</Typography>
-    <div
-      css={css`
-        display: flex;
-        ${css(theme.typography.data as any)};
-      `}
-    >
-      Show:
-      {mock.map(({ type }) => {
-        const { title, color } = TIMELINE_TYPES[type];
-
-        return (
-          <div
-            css={css`
-              margin-left: 3px;
-              display: flex;
-            `}
-          >
-            <Checkbox
-              value={title}
-              checked={activeEntities.includes(type)}
-              onChange={() =>
-                setActiveEntities(
-                  activeEntities.includes(type)
-                    ? activeEntities.filter(e => e !== type)
-                    : [...activeEntities, type],
-                )
-              }
-              aria-label={title}
-              color={color}
-            />
-            <label
+          return (
+            <div
               css={css`
-                margin-left: 8px;
+                margin-left: 3px;
+                display: flex;
               `}
             >
-              {title}
-            </label>
-          </div>
-        );
-      })}
+              <Checkbox
+                value={title}
+                checked
+                onChange={x => x}
+                aria-label={title}
+                disabled={count <= 0}
+              />
+
+              <label
+                css={css`
+                  margin-left: 8px;
+                `}
+              >
+                {`${count} ${title}`}
+              </label>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  </div>
-));
+  );
+};
 
 type ComponentProps = {};
 const Component: React.ComponentType<ComponentProps> = () => {
   const [activeEntities, setActiveEntities] = React.useState([]);
 
-  const headerData = mock.reduce(
+  const entityCounts = mock.reduce(
     (acc, entity) => {
       const { type } = entity;
       console.log('type', type);
-      acc[type] + 1;
+      acc[type]++;
       return acc;
     },
-    { 'Primary Diagnosis': 0, Specimen: 0, Treatment: 0, 'Follow Up': 0 },
+    { primary_diagnosis: 0, specimen: 0, treatment: 0, follow_up: 0 },
   );
-  console.log('header data', headerData);
+
   return (
     <Container
       css={css`
@@ -202,7 +133,9 @@ const Component: React.ComponentType<ComponentProps> = () => {
         display: flex;
         flex-direction: column;
       `}
-    />
+    >
+      <Header entityCounts={entityCounts} />
+    </Container>
   );
 };
 
