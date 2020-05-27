cac03b4f8cc8ba64db2d3757ed27d216c4989323
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2020-05-27 19:46:57 -0400

    Feat/#1400 dcc team page (#1496)
    * add footer link
    
    * add team page
    
    * add member cols
    
    * styling
    
    * fix some responsiveness
    
    * fix nesting elements

---
+++ components/pages/team.tsx
@@ -0,0 +1,167 @@
+import React from 'react';
+import { css } from 'uikit';
+import DefaultLayout from './DefaultLayout';
+import Typography from 'uikit/Typography';
+import TitleBorder from 'uikit/TitleBorder';
+import { ThemeColorNames } from 'uikit/theme/types';
+import { Row, Col } from 'react-grid-system';
+import chunk from 'lodash/chunk';
+import get from 'lodash/get';
+
+type Member = { name: string; title?: string };
+type Team = { title: string; members: Array<Member>; color: keyof ThemeColorNames };
+
+const teamData: Array<Team> = [
+  {
+    title: 'Leaders',
+    members: [
+      { name: 'Christina Yung', title: 'Director of Genome Informatics, OICR' },
+      { name: 'Lincoln Stein', title: 'Head of Adaptive Oncology, OICR' },
+    ],
+    color: 'accent1',
+  },
+  {
+    title: 'Bioinformatics and Data Curation',
+    members: [
+      { name: 'Junjun Zhang', title: 'Lead Bioinformatician' },
+      { name: 'Hardeep Nahal' },
+      { name: 'Linda Xiang' },
+    ],
+    color: 'accent3',
+  },
+  {
+    title: 'Software Engineering',
+    members: [
+      { name: 'Atul Kachru', title: 'Senior Project Manager' },
+      { name: 'Dusan Andric', title: 'Software Architect, Team Lead' },
+      { name: 'Kim Cullion', title: 'Senior UX/UI Designer' },
+      { name: 'Rosi Bajari', title: 'Senior Technical Business Analyst' },
+      { name: 'Alex Lepsa' },
+      { name: 'Alexis Li' },
+      { name: 'Ann Catton' },
+      { name: 'Bashar Allabadi' },
+      { name: 'Ciaran Schutte' },
+      { name: 'Henrich Feher' },
+      { name: 'Jared Baker' },
+      { name: 'Jaser Uddin' },
+      { name: 'Jon Eubank' },
+      { name: 'Kevin Hartmann' },
+      { name: 'Minh Ha' },
+      { name: 'Robert Tisma' },
+      { name: 'Wajiha Shah' },
+    ],
+    color: 'accent4',
+  },
+  {
+    title: 'Research IT',
+    members: [
+      { name: 'David Sutton', title: 'Director IT and Information Security Officer, OICR' },
+      { name: 'Bob Gibson' },
+      { name: 'Gino Yearwood' },
+      { name: 'Miki Wong' },
+    ],
+    color: 'secondary',
+  },
+  {
+    title: 'Alumni',
+    members: [{ name: 'Aleks Pejovic' }, { name: 'Priyonto Saha' }, { name: 'Xu Deng' }],
+    color: 'accent3',
+  },
+];
+
+const SectionTitle = props => (
+  <Typography
+    css={css`
+      font-size: 20px;
+      margin-top: 30px;
+      margin-bottom: 15px;
+    `}
+    variant="subtitle"
+    {...props}
+  />
+);
+
+export default function TeamPage() {
+  return (
+    <DefaultLayout>
+      <div
+        css={css`
+          height: 140px;
+          display: flex;
+          align-items: center;
+          background-image: url('/static/icgc-galaxy-bg.jpg');
+          background-position: center;
+          background-size: cover;
+          margin-bottom: 36px;
+          padding: 0 10vw;
+        `}
+      >
+        <Typography variant="hero" color="white">
+          {' '}
+          The ICGC ARGO DCC Team
+        </Typography>
+      </div>
+
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+          padding: 0 10vw;
+          margin-bottom: 25px;
+        `}
+      >
+        {teamData.map((team, i) => (
+          <div key={i}>
+            <SectionTitle>{team.title}</SectionTitle>
+            <TitleBorder color={team.color} width="86px" />
+
+            <Row>
+              {chunk(team.members, 6).map((col, i, arr) => (
+                <Col
+                  key={i}
+                  css={css`
+                    max-width: 330px;
+                  `}
+                  sm={12}
+                  md={arr.length === 1 ? 12 : 12 / arr.length}
+                >
+                  <Typography
+                    variant="paragraph"
+                    css={css`
+                      line-height: 1.71;
+                    `}
+                  >
+                    {col.map((member, i) => {
+                      const title = get(member, 'title', false);
+                      return (
+                        <span
+                          key={i}
+                          css={css`
+                            display: flex;
+                            flex-wrap: wrap;
+                          `}
+                        >
+                          <Typography variant="paragraph2" bold>
+                            {`${member.name}${title ? ',\u00A0' : ''}`}
+                          </Typography>
+                          <span
+                            css={css`
+                              flex-grow: 1;
+                            `}
+                          >
+                            {title ? `${member.title}` : null}
+                          </span>
+                        </span>
+                      );
+                    })}
+                  </Typography>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        ))}
+      </div>
+    </DefaultLayout>
+  );
+}
