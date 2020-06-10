5da2f6f9db615532125c1e0a3cb32adc60365520
Ann Catton <ann.catton@oicr.on.ca>
2020-06-10 15:13:54 -0400

    correct story keys

--- components/pages/file-repository/FileTable/dummyData.tsx
+++ components/pages/file-repository/FileTable/dummyData.tsx
@@ -23,32 +23,32 @@ import { sample } from 'lodash';
 const idmaker = () => `FL${Math.floor(1000 + Math.random() * 9000)}`;
 export const dummyExamples: Array<FileRepositoryRecord> = [
   {
-    fileID: 'FL9951',
-    donorID: 'DO9980',
-    program: { shortName: 'BRCA-MX', fullName: 'Brain Cancer Mexico' },
+    objectId: 'FL9951',
+    donorId: 'DO9980',
+    programId: 'BRCA-MX',
     dataType: 'Aligned Reads',
-    strategy: 'WXS',
-    format: 'FASTQ',
+    experimentalStrategy: 'WXS',
+    fileType: 'FASTQ',
     size: 129580000,
     isDownloadable: true,
   },
   {
-    fileID: 'FL9981',
-    donorID: 'DO9981',
-    program: { shortName: 'BRCA-US', fullName: 'Brain Cancer USA' },
+    objectId: 'FL9981',
+    donorId: 'DO9981',
+    programId: 'BRCA-US',
     dataType: 'SSM',
-    strategy: 'WGS',
-    format: 'FASTQ',
+    experimentalStrategy: 'WGS',
+    fileType: 'FASTQ',
     size: 7695800000,
     isDownloadable: false,
   },
   {
-    fileID: 'FL9980',
-    donorID: 'DO9982',
-    program: { shortName: 'CESC-US', fullName: 'center for eccentric scientific children' },
+    objectId: 'FL9980',
+    donorId: 'DO9982',
+    programId: 'CESC-US',
     dataType: 'Aligned Reads',
-    strategy: 'WXS',
-    format: 'FASTQ',
+    experimentalStrategy: 'WXS',
+    fileType: 'FASTQ',
     size: 121130000,
     isDownloadable: true,
   },
@@ -56,5 +56,5 @@ export const dummyExamples: Array<FileRepositoryRecord> = [
 
 export let dummyData = [];
 for (let i = 0; i < 108; i++) {
-  dummyData.push({ ...sample(dummyExamples), fileID: `FL-${i + 1}` });
+  dummyData.push({ ...sample(dummyExamples), objectId: `FL-${i + 1}` });
 }
