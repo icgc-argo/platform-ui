1aa0aedeeb0af57a181a6de1bc7bbb52ac7eb621
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-01-12 12:59:08 -0500

    add constant for tooltip and displayname content

---
+++ components/pages/file-repository/utils/constants.tsx
@@ -0,0 +1,26 @@
+export const tooltipContent: { [key: string]: React.ReactNode } = {
+  'Release Stage': (
+    <div>
+      Indicates what stage a file is in during the release cycle:
+      <ul>
+        <li>A file is first accessible to its program members.</li>
+        <li>After 12 months: accessible to full membership programs. </li>
+        <li>After 6 months: accessible to associate membership programs. </li>
+        <li>After 6 months: queued for public release (~ every 4 months).</li>
+      </ul>
+    </div>
+  ),
+};
+
+enum ReleaseStageDisplayNames {
+  OWN_PROGRAM = 'My Program Access',
+  FULL_PROGRAMS = 'Full Member Access',
+  ASSOCIATE_PROGRAMS = 'Associate Member Access',
+  PUBLIC_QUEUE = 'Queued for Release',
+  PUBLIC = 'Released to the Public',
+}
+
+// enums are real objects at runtime
+export const facetDisplayNames: { [key: string]: {} } = {
+  'Release Stage': ReleaseStageDisplayNames,
+};
