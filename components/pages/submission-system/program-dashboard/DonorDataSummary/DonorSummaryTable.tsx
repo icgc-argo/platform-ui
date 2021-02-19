544095b3e2cc55deebc6dac3e291babe9f77996d
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2021-02-19 13:35:09 -0500

    Feat/#1819 dashboard UI (#1828)
    * remove extended fields display column and data from query
    
    * remove processing status table display
    
    * comment out stats we dont need
    
    * delete commented stats

--- components/pages/submission-system/program-dashboard/DonorDataSummary/DonorSummaryTable.tsx
+++ components/pages/submission-system/program-dashboard/DonorDataSummary/DonorSummaryTable.tsx
@@ -221,13 +221,7 @@ export default ({
             <PercentageCell original={original} fieldName="submittedCoreDataPercent" />
           ),
         },
-        {
-          Header: 'Extended Fields',
-          accessor: 'submittedExtendedDataPercent',
-          Cell: ({ original }) => (
-            <PercentageCell original={original} fieldName="submittedExtendedDataPercent" />
-          ),
-        },
+
         {
           Header: 'Samples',
           id: REGISTERD_SAMPLE_COLUMN_ID,
@@ -283,20 +277,11 @@ export default ({
       ],
     },
     {
-      columns: [
-        {
-          Header: 'Processing Status',
-          accessor: 'processingStatus',
-          Cell: ProcessingCell,
-        },
-        {
-          Header: 'Last Updated',
-          accessor: 'updatedAt',
-          Cell: ({ original }: { original: DonorSummaryRecord }) => {
-            return <div>{displayDate(original.updatedAt)}</div>;
-          },
-        },
-      ],
+      Header: 'Last Updated',
+      accessor: 'updatedAt',
+      Cell: ({ original }: { original: DonorSummaryRecord }) => {
+        return <div>{displayDate(original.updatedAt)}</div>;
+      },
     },
   ];
 
