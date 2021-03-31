fdb103e07452a7b5695a73adada1f29d57b04bc3
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-03-31 15:09:15 -0400

    update error text

--- components/pages/submission-system/program-dashboard/DonorDataSummary/InvalidDonorsNotification.tsx
+++ components/pages/submission-system/program-dashboard/DonorDataSummary/InvalidDonorsNotification.tsx
@@ -41,10 +41,15 @@ export const InvalidDonorsNotification = ({ numInvalidDonors }: { numInvalidDono
           dictionary
         </Link>
       )}{' '}
-      was released and has made some donors invalid. All invalid donors, along with their released
-      files, will be revoked in the next release.
+      was released and has made some donors invalid against the latest dictionary. Please correct
+      the clinical data for the highlighted donors.
     </div>
   );
+
+  const errorTitle = `${pluralize('donor', numInvalidDonors, true)} ${pluralize(
+    'is',
+    numInvalidDonors,
+  )} invalid and will be revoked if not corrected.`;
 
   return numInvalidDonors > 0 ? (
     <Notification
@@ -53,10 +58,7 @@ export const InvalidDonorsNotification = ({ numInvalidDonors }: { numInvalidDono
       `}
       size="MD"
       variant="ERROR"
-      title={`${pluralize('donor', numInvalidDonors, true)} ${pluralize(
-        'is',
-        numInvalidDonors,
-      )} invalid and will be revoked if not corrected.`}
+      title={errorTitle}
       content={errorBody}
       interactionType={'NONE'}
     />
