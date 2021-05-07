a87634722aed0917ca1e0ca4c76d2122d6402c70
Sam Rich <samrichca@users.noreply.github.com>
2021-05-07 13:47:21 -0400

    add commas to bar chart tooltips

--- components/pages/file-repository/charts/SimpleBarChart/index.tsx
+++ components/pages/file-repository/charts/SimpleBarChart/index.tsx
@@ -204,7 +204,7 @@ const SimpleBarChart: React.ComponentType<SimpleBarChartProps> = ({
                   <div>
                     <span>{capitalize(category)}</span>
                     <br />
-                    <span>{`${count} ${pluralize('file', count)}`}</span>
+                    <span>{`${count.toLocaleString()} ${pluralize('file', count)}`}</span>
                   </div>
                 }
               >
