2e669a2d089975bef3d372b48b86674e167ffc3d
Rakesh Mistry <mistryrn@mcmaster.ca>
2021-07-06 11:40:53 -0400

    🍱 Add Filter Icon (#1983) * Adds a new "Filter" icon to the UIKit icon collection

---
+++ uikit/Icon/icons/collection/filter.tsx
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { css } from '@emotion/core';
+
+export default {
+  title: 'Filter',
+  viewBox: '0 0 10 10',
+  path:
+    'm.227 0c-.125 0-.227.102-.227.227 0 .048.015.095.044.134l3.702 5.07v2.708c0 .069.032.134.086.177l2.051 1.634c.098.079.241.063.32-.035.032-.04.05-.09.05-.142v-4.343l3.702-5.07c.074-.1.052-.243-.05-.316-.036-.029-.083-.044-.131-.044z',
+  css: css`
+    width: 10px;
+    height: 10px;
+  `,
+  defaultFill: '#0774D3',
+};
