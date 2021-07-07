33de9729530e911e76c15572cbb0a4893b882274
Jon Eubank <joneubank@gmail.com>
2021-07-07 13:47:37 -0400

    Ability to download with auth header

--- global/hooks/useAuthContext.tsx
+++ global/hooks/useAuthContext.tsx
@@ -34,6 +34,7 @@ type T_AuthContext = {
   updateToken: () => Promise<string | void>;
   data: ReturnType<typeof decodeToken> | null;
   fetchWithEgoToken: typeof fetch;
+  downloadFileWithEgoToken: (input: RequestInfo, init?: RequestInit) => Promise<void>;
   permissions: string[];
   isLoggingOut: boolean;
 };
@@ -44,6 +45,7 @@ const AuthContext = React.createContext<T_AuthContext>({
   updateToken: async () => {},
   data: null,
   fetchWithEgoToken: fetch,
+  downloadFileWithEgoToken: async () => {},
   permissions: [],
   isLoggingOut: false,
 });
@@ -110,6 +112,49 @@ export function AuthProvider({
     }
   };
 
+  const downloadFileWithEgoToken: T_AuthContext['downloadFileWithEgoToken'] = async (
+    uri,
+    options,
+  ) => {
+    const response = await fetchWithEgoToken(uri, options);
+    const data = await response.blob();
+
+    const contentDispositionHeader = response.headers.get('content-disposition');
+    const filename = contentDispositionHeader ? contentDispositionHeader.split('filename=')[1] : '';
+
+    const blob = new Blob([data], { type: data.type || 'application/octet-stream' });
+    if (typeof window.navigator.msSaveBlob !== 'undefined') {
+      // IE doesn't allow using a blob object directly as link href.
+      // Workaround for "HTML7007: One or more blob URLs were
+      // revoked by closing the blob for which they were created.
+      // These URLs will no longer resolve as the data backing
+      // the URL has been freed."
+      window.navigator.msSaveBlob(blob, filename);
+      return;
+    }
+    // Other browsers
+    // Create a link pointing to the ObjectURL containing the blob
+    const blobURL = window.URL.createObjectURL(blob);
+    const tempLink = document.createElement('a');
+    tempLink.style.display = 'none';
+    tempLink.href = blobURL;
+    tempLink.setAttribute('download', filename);
+    // Safari thinks _blank anchor are pop ups. We only want to set _blank
+    // target if the browser does not support the HTML5 download attribute.
+    // This allows you to download files in desktop safari if pop up blocking
+    // is enabled.
+    if (typeof tempLink.download === 'undefined') {
+      tempLink.setAttribute('target', '_blank');
+    }
+    document.body.appendChild(tempLink);
+    tempLink.click();
+    document.body.removeChild(tempLink);
+    setTimeout(() => {
+      // For Firefox it is necessary to delay revoking the ObjectURL
+      window.URL.revokeObjectURL(blobURL);
+    }, 100);
+  };
+
   if (egoJwt && !isValidJwt(egoJwt)) {
     logOut();
   }
@@ -139,6 +184,7 @@ export function AuthProvider({
     data: egoJwt ? decodeToken(egoJwt || '') : null,
     updateToken,
     fetchWithEgoToken,
+    downloadFileWithEgoToken,
     permissions,
     isLoggingOut,
   };
