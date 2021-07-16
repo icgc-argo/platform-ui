aa9d25535bf4897a0697db2e38ecf72c29f89391
Yelizar Alturmessov <65674408+yalturmes@users.noreply.github.com>
2021-07-16 13:34:27 -0400

    Update Jenkinsfile.groovy
    Set explicit UIDs for containers.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -45,9 +45,12 @@ spec:
     image: docker:18.06-dind
     securityContext:
       privileged: true
+      runAsUser: 0
     volumeMounts:
     - name: docker-graph-storage
       mountPath: /var/lib/docker
+  securityContext:
+    runAsUser: 1000
   volumes:
   - name: docker-graph-storage
     emptyDir: {}
