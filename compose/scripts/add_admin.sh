41f363c85281e9085a930ebc4c8fb65115efc79c
Minh <hlminh2000@gmail.com>
2021-10-01 14:26:18 -0400

    🔥 removes platform folders

--- compose/scripts/add_admin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PGPASSWORD=password
-echo $1
-docker exec $(docker ps | grep ego-db | awk '{print $NF}') psql -U postgres -d ego -h localhost -c "DELETE FROM egouser WHERE egouser.name='$1'"
-docker exec $(docker ps | grep ego-db | awk '{print $NF}') psql -U postgres -d ego -h localhost -c "INSERT INTO egouser (id, name, email, type, firstname, lastname, createdat, lastlogin, status, preferredlanguage) VALUES ('57b8f58e-cf8d-4a7f-ac49-51129e156f33', '$1', '$1', 'ADMIN', 'Foo', 'Bar', NOW(), NOW(), 'APPROVED', 'ENGLISH');"
-docker exec $(docker ps | grep ego-db | awk '{print $NF}') psql -U postgres -d ego -h localhost -c "INSERT INTO userpermission (id, policy_id, user_id, access_level) VALUES ('b69740f2-c9c9-413e-a682-d62b002b54a7', '27b08a5b-5328-4223-8ddc-c9e6dcaa48c3', '57b8f58e-cf8d-4a7f-ac49-51129e156f33', 'WRITE');"
-
-docker exec $(docker ps | grep ego-db | awk '{print $NF}') psql -U postgres -d ego -h localhost -c "INSERT INTO userpermission (id, policy_id, user_id, access_level) VALUES ('b69740f2-c9c9-413e-a682-d62b002b54a8', '27b08a5b-5328-4223-8ddc-c9e6dcaa48c4', '57b8f58e-cf8d-4a7f-ac49-51129e156f33', 'WRITE');"
-docker exec $(docker ps | grep ego-db | awk '{print $NF}') psql -U postgres -d ego -h localhost -c "INSERT INTO userpermission (id, policy_id, user_id, access_level) VALUES ('b69740f2-c9c9-413e-a682-d62b002b54a9', '27b08a5b-5328-4223-8ddc-c9e6dcaa48c5', '57b8f58e-cf8d-4a7f-ac49-51129e156f33', 'WRITE');"
--- global/constants/gatewayApiPaths.tsx
+++
@@ -1,8 +0,0 @@
-export const MANIFEST_DOWNLOAD_PATH = '/file-centric-tsv/score-manifest';
-export const FILE_TABLE_DOWNLOAD_PATH = '/file-centric-tsv/file-table';
-export const GRAPHQL_PATH = '/graphql';
-export const STATUS_PATH = '/status';
-export const CLINICAL_TEMPLATE_PATH = '/clinical/template';
-
-// Donor Aggregator
-export const DONOR_AGGREGATOR_SYNC_PROGRAM = '/donor-aggregator/sync';
