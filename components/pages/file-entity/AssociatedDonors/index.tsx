0d01073b73f350d66dd692ad12c9e3b53c43f791
Dan DeMaria <ddemaria@oicr.on.ca>
2022-02-16 09:53:41 -0500

    Revert commented import

--- components/pages/file-entity/AssociatedDonors/index.tsx
+++ components/pages/file-entity/AssociatedDonors/index.tsx
@@ -19,7 +19,7 @@
 
 import { FileCard, TableDiv } from '../common';
 import { css } from '@emotion/core';
-// import VerticalTabs from 'uikit/VerticalTabs';
+import VerticalTabs from 'uikit/VerticalTabs';
 import { Col, Row } from 'react-grid-system';
 import { DonorRecord } from '../types';
 import React from 'react';
