1c226480fb5e62bd482cc90cac980613008b996f
Sam Rich <samrichca@users.noreply.github.com>
2022-06-20 12:43:48 -0400

    update jira ticket query

--- components/pages/contact/index.tsx
+++ components/pages/contact/index.tsx
@@ -35,7 +35,7 @@ import { getConfig } from 'global/config';
 import urljoin from 'url-join';
 import ReCAPTCHA from 'react-google-recaptcha';
 import yup from 'global/utils/validations';
-import CREATE_JIRA_TICKET from './CREATE_JIRA_TICKET.gql';
+import CREATE_JIRA_TICKET_QUERY from './gql/CREATE_JIRA_TICKET_QUERY';
 import { firstName, lastName, email } from 'global/utils/form/validations';
 import useFormHook from 'global/hooks/useFormHook';
 import FormHelperText from '@icgc-argo/uikit/form/FormHelperText';
@@ -103,7 +103,7 @@ export default function ContactPage() {
     reCaptcha: reCaptchaError,
   } = errors as typeof contactFormSchema;
 
-  const [createTicket] = useMutation(CREATE_JIRA_TICKET);
+  const [createTicket] = useMutation(CREATE_JIRA_TICKET_QUERY);
   const toaster = useToaster();
   const reCaptchaRef = React.useRef(null);
   const [requestLoader, setRequestLoader] = React.useState(false);
