bb2e51c3a56aaefa03cc7832113649ba4b56b99d
Daniel Lu <dllu@wl6330-mkostiuk.ad.oicr.on.ca>
2022-11-10 15:31:11 -0500

    remove comment

--- components/pages/submission-system/program-submitted-data/SearchBar/index.tsx
+++ components/pages/submission-system/program-submitted-data/SearchBar/index.tsx
@@ -199,7 +199,6 @@ export default function SearchBar({
             onChange={handleOnEnter}
             getOverrideCss={() => searchInputFieldStyle}
           />
-          {/* </form> */}
 
           {keyword && keyword.length >= 1 && searchOpen ? (
             <>
