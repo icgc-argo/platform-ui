076c5d736eec37d0725ec3c64f94e30b60d130f5
Daniel <79996555+daniel-cy-lu@users.noreply.github.com>
2023-02-13 16:37:49 -0500

    #2526 Build a Static Program Entity Page (#2570)
    * add ProgramTitleBar
    
    * commit before branch switch
    
    * UI complete
    
    * spelling mistakes and css update
    
    * edit name Program to ProgramEntity
    
    * added comment regarding variables being mock data
    
    * edit circleFill and iconFill type to keyof ThemeColorNames
    
    * update copyright disclaim year to 2023
    
    * comment on the loading variable in ProgramEntity
    
    * remove loading variable since it's not needed for this ticket's purpose. If it's needed in the future, we will add it
    
    * move mock data into a separate file
    
    * remove all unused imports with shift + opt + o
    
    ---------
    
    Co-authored-by: Daniel Lu <dllu@ML6460-DLU.ht.home>
    Co-authored-by: Daniel Lu <dllu@wl6895-kfarnc.ad.oicr.on.ca>
    Co-authored-by: Daniel Lu <dllu@ml6460-dlu.ad.oicr.on.ca>
    Co-authored-by: Daniel Lu <dllu@ML6460-DLU.local>

---
+++ components/pages/program-entity/ProgramCardsLayout/ProgramSummaryLinkContatiner/ProgramSummaryLink.tsx
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2023 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { css, Icon, ThemeColorNames, Typography, UikitIconNames } from '@icgc-argo/uikit';
+import { CircleContainer } from 'components/pages/Homepage/common';
+
+const ProgramSummaryLink = ({
+  circleFill,
+  iconFill,
+  iconName,
+  totalNum,
+  subtitle,
+}: {
+  circleFill: keyof ThemeColorNames;
+  iconFill: keyof ThemeColorNames;
+  iconName: UikitIconNames;
+  totalNum: number;
+  subtitle: string;
+}) => {
+  return (
+    <div
+      css={css`
+        display: flex;
+      `}
+    >
+      <CircleContainer fill={circleFill}>
+        <Icon name={iconName} fill={iconFill} />
+      </CircleContainer>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          width: 70%;
+        `}
+      >
+        <Typography
+          variant="subtitle2"
+          as="div"
+          css={css`
+            margin-bottom: 4px;
+            margin-left: 10px;
+          `}
+        >
+          {totalNum}
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          as="div"
+          css={css`
+            margin-bottom: 4px;
+            margin-left: 10px;
+          `}
+        >
+          {subtitle}
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default ProgramSummaryLink;
