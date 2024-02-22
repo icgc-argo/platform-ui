a48a379963dfbd70e10ff0b1480fc69fb67cdeaa
Dan DeMaria <ddemaria@oicr.on.ca>
2024-02-22 14:29:22 -0500

    DataCenter placeholder - current value for manage program, default string for create

--- components/pages/submission-system/program-form/ProgramForm.tsx
+++ components/pages/submission-system/program-form/ProgramForm.tsx
@@ -422,7 +422,7 @@ export default function CreateProgramForm({
                   }))}
                   onChange={(val) => setData({ key: 'dataCenter', val: val || '' })}
                   onBlur={handleInputBlur('dataCenter')}
-                  placeholder={form.dataCenter}
+                  placeholder={isEditing ? form.dataCenter : undefined}
                   value={form.dataCenter || ''}
                   size="lg"
                 />
