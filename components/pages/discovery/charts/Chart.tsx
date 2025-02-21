09a4bf9a25997f038cc0c278eca73611d651e9ef
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2025-02-20 23:32:40 -0500

    fix scroll

--- components/pages/discovery/charts/Chart.tsx
+++ components/pages/discovery/charts/Chart.tsx
@@ -21,7 +21,11 @@ import { css } from '@emotion/react';
 import { PropsWithChildren } from 'react';
 
 export const ChartContainer = ({ children, height }: PropsWithChildren<{ height?: any }>) => (
-  <div css={css({ flex: 1, overflow: 'scroll' })}>
-    <div css={css({ width: '100%', height: height || '100%' })}>{children}</div>
+  <div css={css({ flex: 1, position: 'relative' })}>
+    <div
+      css={css({ position: 'absolute', top: 0, left: 0, width: '100%', height: height || '100%' })}
+    >
+      {children}
+    </div>
   </div>
 );
