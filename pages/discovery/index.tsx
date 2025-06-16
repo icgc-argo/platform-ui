f310f96f3ec4809e1e286d774fad6ac3b9769b02
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2025-06-16 13:25:25 -0400

    add route auth check

--- pages/discovery/index.tsx
+++ pages/discovery/index.tsx
@@ -19,10 +19,12 @@
 
 import DiscoveryPage from 'components/pages/discovery';
 import { getConfig } from 'global/config';
+import { isDccMember } from 'global/utils/egoJwt';
 import { createPage } from 'global/utils/pages';
 
 export default createPage<{ egoJwt: string }>({
-  isPublic: true,
+  isPublic: false,
+  isAccessible: async ({ initialPermissions }) => isDccMember(initialPermissions),
 })(() => {
   const { FEATURE_DATA_DISCOVERY_ENABLED } = getConfig();
   return FEATURE_DATA_DISCOVERY_ENABLED ? <DiscoveryPage /> : null;
