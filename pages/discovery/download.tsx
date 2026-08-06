76953f2aa7f3555c5d76399cb4f44e36e7f7968e
Jon Eubank <joneubank@gmail.com>
2026-08-06 10:38:15 -0400

    Discovery download page content

--- pages/discovery/download.tsx
+++ pages/discovery/download.tsx
@@ -17,15 +17,15 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import DiscoveryPage from 'components/pages/discovery';
+import DownloadPage from 'components/pages/discovery/DownloadPage';
 import { getConfig } from 'global/config';
-import { hasDacoAccess } from 'global/utils/egoJwt';
 import { createPage } from 'global/utils/pages';
 
-export default createPage<{ egoJwt: string }>({
-  isPublic: false,
-  isAccessible: async ({ initialPermissions }) => hasDacoAccess(initialPermissions),
+const DiscoveryDownloadPage = createPage<{ egoJwt: string }>({
+  isPublic: true,
 })(() => {
   const { FEATURE_DATA_DISCOVERY_ENABLED } = getConfig();
-  return FEATURE_DATA_DISCOVERY_ENABLED ? <DiscoveryPage /> : null;
+  return FEATURE_DATA_DISCOVERY_ENABLED ? <DownloadPage /> : null;
 });
+
+export default DiscoveryDownloadPage;
